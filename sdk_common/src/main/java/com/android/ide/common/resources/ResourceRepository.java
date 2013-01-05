39d51e35d8174eb6af2e4c88a833bc89ae35aa7a
Tor Norbye <tnorbye@google.com>
2013-01-04 16:57:08 -0800

    Handle theme resources in ResourceRepository.hasResourceItem
    This adds the same URL rewriting logic as is already used
    in parseResource in the same class
    
    Change-Id: I67f362cacdb17ab93affcb89bd88e47a2bbe89ad

--- sdk_common/src/main/java/com/android/ide/common/resources/ResourceRepository.java
+++ sdk_common/src/main/java/com/android/ide/common/resources/ResourceRepository.java
@@ -242,6 +242,35 @@ public abstract class ResourceRepository {
      * @return true if the resource is known
      */
     public boolean hasResourceItem(@NonNull String url) {
+        // Handle theme references
+        if (url.startsWith(PREFIX_THEME_REF)) {
+            String remainder = url.substring(PREFIX_THEME_REF.length());
+            if (url.startsWith(ATTR_REF_PREFIX)) {
+                url = PREFIX_RESOURCE_REF + url.substring(PREFIX_THEME_REF.length());
+                return hasResourceItem(url);
+            }
+            int colon = url.indexOf(':');
+            if (colon != -1) {
+                // Convert from ?android:progressBarStyleBig to ?android:attr/progressBarStyleBig
+                if (remainder.indexOf('/', colon) == -1) {
+                    remainder = remainder.substring(0, colon) + RESOURCE_CLZ_ATTR + '/'
+                            + remainder.substring(colon);
+                }
+                url = PREFIX_RESOURCE_REF + remainder;
+                return hasResourceItem(url);
+            } else {
+                int slash = url.indexOf('/');
+                if (slash == -1) {
+                    url = PREFIX_RESOURCE_REF + RESOURCE_CLZ_ATTR + '/' + remainder;
+                    return hasResourceItem(url);
+                }
+            }
+        }
+
+        if (!url.startsWith(PREFIX_RESOURCE_REF)) {
+            return false;
+        }
+
         assert url.startsWith("@") || url.startsWith("?") : url;
 
         ensureInitialized();
@@ -881,7 +910,7 @@ public abstract class ResourceRepository {
         if (url.startsWith(PREFIX_THEME_REF)) {
             String remainder = url.substring(PREFIX_THEME_REF.length());
             if (url.startsWith(ATTR_REF_PREFIX)) {
-                url = PREFIX_RESOURCE_REF + url.substring(1);
+                url = PREFIX_RESOURCE_REF + url.substring(PREFIX_THEME_REF.length());
                 return parseResource(url);
             }
             int colon = url.indexOf(':');
