6b94e0c2275ef5ab67e290e1048872e3308bdc54
Siva Velusamy <vsiva@google.com>
2013-01-28 16:38:20 -0800

    Add support for invoking methods on views.
    This CL adds support for invoking methods on a Android View
    object. To invoke a view, the method name along with all its
    arguments are passed to the device via the DDM channel.
    
    The existing commands to layout a view and invalidate a view
    are now replaced by just calling the generic invoke view method
    operation with appropriate parameters.
    
    Change-Id: I1fe345f9c6d7db0569656256feb091c6f1b664b6

--- ddmlib/src/main/java/com/android/ddmlib/HandleViewDebug.java
+++ ddmlib/src/main/java/com/android/ddmlib/HandleViewDebug.java
@@ -52,20 +52,31 @@ final public class HandleViewDebug extends ChunkHandler {
     /** Obtain the Display List corresponding to the view. */
     private static final int VUOP_DUMP_DISPLAYLIST = 2;
 
-    /** Invalidate View. */
-    private static final int VUOP_INVALIDATE_VIEW = 3;
-
-    /** Re-layout given view. */
-    private static final int VUOP_LAYOUT_VIEW = 4;
-
     /** Profile a view. */
-    private static final int VUOP_PROFILE_VIEW = 5;
+    private static final int VUOP_PROFILE_VIEW = 3;
+
+    /** Invoke a method on the view. */
+    private static final int VUOP_INVOKE_VIEW_METHOD = 4;
+
+    /** Set layout parameter. */
+    private static final int VUOP_SET_LAYOUT_PARAMETER = 5;
+
+    private static final String TAG = "ddmlib"; //$NON-NLS-1$
+
+    private static final HandleViewDebug sInstance = new HandleViewDebug();
+
+    private static final ViewDumpHandler sViewOpNullChunkHandler =
+            new NullChunkHandler(CHUNK_VUOP);
 
     private HandleViewDebug() {}
 
     public static void register(MonitorThread mt) {
         // TODO: add chunk type for auto window updates
         // and register here
+        mt.registerChunkHandler(CHUNK_VUGL, sInstance);
+        mt.registerChunkHandler(CHUNK_VULW, sInstance);
+        mt.registerChunkHandler(CHUNK_VUOP, sInstance);
+        mt.registerChunkHandler(CHUNK_VURT, sInstance);
     }
 
     @Override
@@ -161,10 +172,15 @@ final public class HandleViewDebug extends ChunkHandler {
     }
 
     private static void sendViewOpPacket(@NonNull Client client, int op, @NonNull String viewRoot,
-            @NonNull String view, @Nullable ViewDumpHandler handler) throws IOException {
+            @NonNull String view, @Nullable byte[] extra, @Nullable ViewDumpHandler handler)
+                    throws IOException {
         int bufLen = 4 +                        // opcode
                 4 + viewRoot.length() * 2 +     // view root strlen + view root
                 4 + view.length() * 2;          // view strlen + view
+
+        if (extra != null) {
+            bufLen += extra.length;
+        }
 
         ByteBuffer buf = allocBuffer(bufLen);
         JdwpPacket packet = new JdwpPacket(buf);
@@ -177,6 +193,10 @@ final public class HandleViewDebug extends ChunkHandler {
         chunkBuf.putInt(view.length());
         putString(chunkBuf, view);
 
+        if (extra != null) {
+            chunkBuf.put(extra);
+        }
+
         finishChunkPacket(packet, CHUNK_VUOP, chunkBuf.position());
         if (handler != null) {
             client.sendAndConsume(packet, handler);
@@ -187,27 +207,110 @@ final public class HandleViewDebug extends ChunkHandler {
 
     public static void profileView(@NonNull Client client, @NonNull String viewRoot,
             @NonNull String view, @NonNull ViewDumpHandler handler) throws IOException {
-        sendViewOpPacket(client, VUOP_PROFILE_VIEW, viewRoot, view, handler);
+        sendViewOpPacket(client, VUOP_PROFILE_VIEW, viewRoot, view, null, handler);
     }
 
     public static void captureView(@NonNull Client client, @NonNull String viewRoot,
             @NonNull String view, @NonNull ViewDumpHandler handler) throws IOException {
-        sendViewOpPacket(client, VUOP_CAPTURE_VIEW, viewRoot, view, handler);
+        sendViewOpPacket(client, VUOP_CAPTURE_VIEW, viewRoot, view, null, handler);
     }
 
     public static void invalidateView(@NonNull Client client, @NonNull String viewRoot,
             @NonNull String view) throws IOException {
-        sendViewOpPacket(client, VUOP_INVALIDATE_VIEW, viewRoot, view, null);
+        invokeMethod(client, viewRoot, view, "invalidate");
     }
 
     public static void requestLayout(@NonNull Client client, @NonNull String viewRoot,
             @NonNull String view) throws IOException {
-        sendViewOpPacket(client, VUOP_LAYOUT_VIEW, viewRoot, view, null);
+        invokeMethod(client, viewRoot, view, "requestLayout");
     }
 
     public static void dumpDisplayList(@NonNull Client client, @NonNull String viewRoot,
             @NonNull String view) throws IOException {
-        sendViewOpPacket(client, VUOP_DUMP_DISPLAYLIST, viewRoot, view, null);
+        sendViewOpPacket(client, VUOP_DUMP_DISPLAYLIST, viewRoot, view, null,
+                sViewOpNullChunkHandler);
+    }
+
+    /** A {@link ViewDumpHandler} to use when no response is expected. */
+    private static class NullChunkHandler extends ViewDumpHandler {
+        public NullChunkHandler(int chunkType) {
+            super(chunkType);
+        }
+
+        @Override
+        protected void handleViewDebugResult(ByteBuffer data) {
+        }
+    }
+
+    public static void invokeMethod(@NonNull Client client, @NonNull String viewRoot,
+            @NonNull String view, @NonNull String method, Object... args) throws IOException {
+        int len = 4 + method.length() * 2;
+        if (args != null) {
+            // # of args
+            len += 4;
+
+            // for each argument, we send a char type specifier (2 bytes) and
+            // the arg value (max primitive size = sizeof(double) = 8
+            len += 10 * args.length;
+        }
+
+        byte[] extra = new byte[len];
+        ByteBuffer b = ByteBuffer.wrap(extra);
+
+        b.putInt(method.length());
+        putString(b, method);
+
+        if (args != null) {
+            b.putInt(args.length);
+
+            for (int i = 0; i < args.length; i++) {
+                Object arg = args[i];
+                if (arg instanceof Boolean) {
+                    b.putChar('Z');
+                    b.put((byte) (((Boolean) arg).booleanValue() ? 1 : 0));
+                } else if (arg instanceof Byte) {
+                    b.putChar('B');
+                    b.put(((Byte) arg).byteValue());
+                } else if (arg instanceof Character) {
+                    b.putChar('C');
+                    b.putChar(((Character) arg).charValue());
+                } else if (arg instanceof Short) {
+                    b.putChar('S');
+                    b.putShort(((Short) arg).shortValue());
+                } else if (arg instanceof Integer) {
+                    b.putChar('I');
+                    b.putInt(((Integer) arg).intValue());
+                } else if (arg instanceof Long) {
+                    b.putChar('J');
+                    b.putLong(((Long) arg).longValue());
+                } else if (arg instanceof Float) {
+                    b.putChar('F');
+                    b.putFloat(((Float) arg).floatValue());
+                } else if (arg instanceof Double) {
+                    b.putChar('D');
+                    b.putDouble(((Double) arg).doubleValue());
+                } else {
+                    Log.e(TAG, "View method invocation only supports primitive arguments, supplied: " + arg);
+                    return;
+                }
+            }
+        }
+
+        sendViewOpPacket(client, VUOP_INVOKE_VIEW_METHOD, viewRoot, view, extra,
+                sViewOpNullChunkHandler );
+    }
+
+    public static void setLayoutParameter(@NonNull Client client, @NonNull String viewRoot,
+            @NonNull String view, @NonNull String parameter, int value) throws IOException {
+        int len = 4 + parameter.length() * 2 + 4;
+        byte[] extra = new byte[len];
+        ByteBuffer b = ByteBuffer.wrap(extra);
+
+        b.putInt(parameter.length());
+        putString(b, parameter);
+        b.putInt(value);
+        sendViewOpPacket(client, VUOP_SET_LAYOUT_PARAMETER, viewRoot, view, extra,
+                sViewOpNullChunkHandler);
     }
 
     @Override
