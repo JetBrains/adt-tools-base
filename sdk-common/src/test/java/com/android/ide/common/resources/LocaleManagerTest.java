1cc4685af45822f90fbb289eb06a1a37893f2a52
Tor Norbye <tnorbye@google.com>
2013-05-22 11:02:21 -0700

    LocaleManager tweaks
    Change-Id: I9bf10bcb57040fbd466fd20cea087e71dd9589be

--- sdk-common/src/test/java/com/android/ide/common/resources/LocaleManagerTest.java
+++ sdk-common/src/test/java/com/android/ide/common/resources/LocaleManagerTest.java
@@ -18,14 +18,10 @@ package com.android.ide.common.resources;
 
 import com.google.common.collect.Sets;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import junit.framework.TestCase;
+
 import java.util.Locale;
 import java.util.Map;
-import java.util.Set;
-
-import junit.framework.TestCase;
 
 @SuppressWarnings({"javadoc", "SizeReplaceableByIsEmpty"})
 public class LocaleManagerTest extends TestCase {
@@ -95,64 +91,6 @@ public class LocaleManagerTest extends TestCase {
         assertEquals("Norway", LocaleManager.getRegionName("NO"));
         assertEquals("France", LocaleManager.getRegionName("FR"));
         assertEquals("India", LocaleManager.getRegionName("IN"));
-    }
-
-    public void testAvailableIcons() {
-        // Icons we have in WindowBuilder
-        String[] icons = new String[] {
-                "ad", "ae", "af", "ag", "ai", "al", "am", "ao", "ar", "as", "at", "au", "aw", "ax",
-                "az", "ba", "bb", "bd", "be", "bf", "bg", "bh", "bi", "bj", "bm", "bn", "bo", "br",
-                "bs", "bt", "bv", "bw", "by", "bz", "ca", "cc", "cd", "cf", "cg", "ch", "ci", "ck",
-                "cl", "cm", "cn", "co", "cr", "cu", "cv", "cx", "cy", "cz", "de", "dj", "dk", "dm",
-                "do", "dz", "ec", "ee", "eg", "eh", "er", "es", "et", "fi", "fj", "fk", "fm", "fo",
-                "fr", "ga", "gb", "gd", "ge", "gf", "gh", "gi", "gl", "gm", "gn", "gp", "gq", "gr",
-                "gs", "gt", "gu", "gw", "gy", "hk", "hm", "hn", "hr", "ht", "hu", "id", "ie", "il",
-                "in", "io", "iq", "ir", "is", "it", "jm", "jo", "jp", "ke", "kg", "kh", "ki", "km",
-                "kn", "kp", "kr", "kw", "ky", "kz", "la", "lb", "lc", "li", "lk", "lr", "ls", "lt",
-                "lu", "lv", "ly", "ma", "mc", "md", "me", "mg", "mh", "mk", "ml", "mm", "mn", "mo",
-                "mp", "mq", "mr", "ms", "mt", "mu", "mv", "mw", "mx", "my", "mz", "na", "nc", "ne",
-                "nf", "ng", "ni", "nl", "no", "np", "nr", "nu", "nz", "om", "pa", "pe", "pf", "pg",
-                "ph", "pk", "pl", "pm", "pn", "pr", "ps", "pt", "pw", "py", "qa", "re", "ro", "rs",
-                "ru", "rw", "sa", "sb", "sc", "sd", "se", "sg", "sh", "si", "sj", "sk", "sl", "sm",
-                "sn", "so", "sr", "st", "sv", "sy", "sz", "tc", "td", "tf", "tg", "th", "tj", "tk",
-                "tl", "tm", "tn", "to", "tr", "tt", "tv", "tw", "tz", "ua", "ug", "um", "us", "uy",
-                "uz", "va", "vc", "ve", "vg", "vi", "vn", "vu", "wf", "ws", "ye", "yt", "za", "zm",
-                "zw",
-        };
-        Set<String> sIcons = new HashSet<String>(100);
-        Map<String, String> regionNames = LocaleManager.getRegionNamesMap();
-        Map<String, String> languageToCountry = LocaleManager.getLanguageToCountryMap();
-        Map<String, String> languageNames = LocaleManager.getLanguageNamesMap();
-        List<String> unused = new ArrayList<String>();
-        for (String code : icons) {
-            code = code.toUpperCase(Locale.US);
-            sIcons.add(code);
-
-            String country = regionNames.get(code);
-            if (country == null) {
-                System.out.println("No region name found for region code " + code);
-            }
-
-            if (!languageToCountry.values().contains(code)) {
-                unused.add(code.toLowerCase() + ".png");
-            }
-        }
-        if (!unused.isEmpty()) {
-            System.out.println("The following icons are not referenced by any of the " +
-                    "language to country bindings: " + unused);
-        }
-
-        // Make sure all our language bindings are languages we have maps for
-        for (Map.Entry<String, String> entry : languageToCountry.entrySet()) {
-            String language = entry.getKey();
-            String region = entry.getValue();
-
-            if (!sIcons.contains(region)) {
-                System.out.println("No icon found for region " + region + "  "
-                        + LocaleManager.getRegionName(region) + " (used for language "
-                        + language + "(" + languageNames.get(language) + "))");
-            }
-        }
     }
 
     /* Utility useful for identifying strings which must be using \\u in the string names
