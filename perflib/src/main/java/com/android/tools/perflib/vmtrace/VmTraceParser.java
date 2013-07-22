f3eea84f9f01e7446f4ded29d74a48372f152583
Siva Velusamy <vsiva@google.com>
2013-07-22 14:12:27 -0700

    Add parser for traceview
    This CL creates a new library perflib that is intended to contain
    memory and performance analyzers that might be shared across
    Eclipse and IntelliJ.
    
    The initial commit contains a parser for dalvik trace. The parser
    can currently parse through the dalvik method trace file and
    reconstruct call stacks.
    
    Change-Id: I02fd961bb2c02a705f149a713498c8365d78ffde

---
+++ perflib/src/main/java/com/android/tools/perflib/vmtrace/VmTraceParser.java
@@ -0,0 +1,374 @@
+package com.android.tools.perflib.vmtrace;
+
+import com.android.annotations.VisibleForTesting;
+import com.google.common.base.Charsets;
+import com.google.common.io.Closeables;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.ByteOrder;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class VmTraceParser {
+    private static final int TRACE_MAGIC = 0x574f4c53; // 'SLOW'
+
+    private static final String HEADER_SECTION_VERSION = "*version";
+    private static final String HEADER_SECTION_THREADS = "*threads";
+    private static final String HEADER_SECTION_METHODS = "*methods";
+    private static final String HEADER_END = "*end";
+
+    private static final String KEY_CLOCK = "clock";
+    private static final String KEY_DATA_OVERFLOW = "data-file-overflow";
+    private static final String KEY_VM = "vm";
+
+    private final File mTraceFile;
+
+    private final VmTraceData.Builder mTraceDataBuilder;
+    private VmTraceData mTraceData;
+
+    public VmTraceParser(File traceFile) {
+        if (!traceFile.exists()) {
+            throw new IllegalArgumentException(
+                    "Trace file " + traceFile.getAbsolutePath() + " does not exist.");
+        }
+        mTraceFile = traceFile;
+        mTraceDataBuilder = new VmTraceData.Builder();
+    }
+
+    public void parse() throws IOException {
+        long headerLength = parseHeader(mTraceFile);
+        MappedByteBuffer buffer = mapFile(mTraceFile, headerLength);
+        parseData(buffer);
+    }
+
+    public VmTraceData getTraceData() {
+        if (mTraceData == null) {
+            mTraceData = mTraceDataBuilder.build();
+        }
+
+        return mTraceData;
+    }
+
+    static final int PARSE_VERSION = 0;
+    static final int PARSE_THREADS = 1;
+    static final int PARSE_METHODS = 2;
+    static final int PARSE_OPTIONS = 4;
+
+    /** Parses the trace file header and returns the offset in the file where the header ends. */
+    @VisibleForTesting(visibility = VisibleForTesting.Visibility.PRIVATE)
+    long parseHeader(File f) throws IOException {
+        long offset = 0;
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(new FileInputStream(f), Charsets.US_ASCII));
+
+            int mode = PARSE_VERSION;
+            String line;
+            while (true) {
+                line = in.readLine();
+                if (line == null) {
+                    throw new IOException("Key section does not have an *end marker");
+                }
+
+                // Calculate how much we have read from the file so far.  The
+                // extra byte is for the line ending not included by readLine().
+                offset += line.length() + 1;
+
+                if (line.startsWith("*")) {
+                    if (line.equals(HEADER_SECTION_VERSION)) {
+                        mode = PARSE_VERSION;
+                        continue;
+                    }
+                    if (line.equals(HEADER_SECTION_THREADS)) {
+                        mode = PARSE_THREADS;
+                        continue;
+                    }
+                    if (line.equals(HEADER_SECTION_METHODS)) {
+                        mode = PARSE_METHODS;
+                        continue;
+                    }
+                    if (line.equals(HEADER_END)) {
+                        break;
+                    }
+                }
+
+                switch (mode) {
+                    case PARSE_VERSION:
+                        mTraceDataBuilder.setVersion(Integer.decode(line));
+                        mode = PARSE_OPTIONS;
+                        break;
+                    case PARSE_THREADS:
+                        parseThread(line);
+                        break;
+                    case PARSE_METHODS:
+                        parseMethod(line);
+                        break;
+                    case PARSE_OPTIONS:
+                        parseOption(line);
+                        break;
+                }
+            }
+        } finally {
+            if (in != null) {
+                Closeables.closeQuietly(in);
+            }
+        }
+
+        return offset;
+    }
+
+    /** Parses trace option formatted as a key value pair. */
+    private void parseOption(String line) {
+        String[] tokens = line.split("=");
+        if (tokens.length == 2) {
+            String key = tokens[0];
+            String value = tokens[1];
+
+            if (key.equals(KEY_CLOCK)) {
+                if (value.equals("thread-cpu")) {
+                    mTraceDataBuilder.setClockType(VmTraceData.ClockType.THREAD_CPU);
+                } else if (value.equals("wall")) {
+                    mTraceDataBuilder.setClockType(VmTraceData.ClockType.WALL);
+                } else if (value.equals("dual")) {
+                    mTraceDataBuilder.setClockType(VmTraceData.ClockType.DUAL);
+                }
+            } else if (key.equals(KEY_DATA_OVERFLOW)) {
+                mTraceDataBuilder.setDataFileOverflow(Boolean.parseBoolean(value));
+            } else if (key.equals(KEY_VM)) {
+                mTraceDataBuilder.setVm(value);
+            } else {
+                mTraceDataBuilder.setProperty(key, value);
+            }
+        }
+    }
+
+    /** Parses thread information comprising an integer id and the thread name */
+    private void parseThread(String line) {
+        int index = line.indexOf('\t');
+        if (index < 0) {
+            return;
+        }
+
+        try {
+            int id = Integer.decode(line.substring(0, index));
+            String name = line.substring(index).trim();
+            mTraceDataBuilder.addThread(id, name);
+        } catch (NumberFormatException e) {
+            return;
+        }
+    }
+
+    void parseMethod(String line) {
+        String[] tokens = line.split("\t");
+        long id;
+        try {
+            id = Long.decode(tokens[0]);
+        } catch (NumberFormatException e) {
+            return;
+        }
+
+        String className = tokens[1];
+        String methodName = null;
+        String signature = null;
+        String pathname = null;
+        int lineNumber = -1;
+        if (tokens.length == 6) {
+            methodName = tokens[2];
+            signature = tokens[3];
+            pathname = tokens[4];
+            lineNumber = Integer.decode(tokens[5]);
+            pathname = constructPathname(className, pathname);
+        } else if (tokens.length > 2) {
+            if (tokens[3].startsWith("(")) {
+                methodName = tokens[2];
+                signature = tokens[3];
+            } else {
+                pathname = tokens[2];
+                lineNumber = Integer.decode(tokens[3]);
+            }
+        }
+
+        mTraceDataBuilder.addMethod(id, new MethodInfo(id, className, methodName, signature,
+                pathname, lineNumber));
+    }
+
+    private String constructPathname(String className, String pathname) {
+        int index = className.lastIndexOf('/');
+        if (index > 0 && index < className.length() - 1 && pathname.endsWith(".java")) {
+            pathname = className.substring(0, index + 1) + pathname;
+        }
+        return pathname;
+    }
+
+    /**
+     * Parses the data section of the trace. The data section comprises of a header followed
+     * by a list of records.
+     *
+     * All values are stored in little-endian order.
+     */
+    private void parseData(MappedByteBuffer buffer) {
+        int recordSize = readDataFileHeader(buffer);
+        parseMethodTraceData(buffer, recordSize);
+    }
+
+    /**
+     * Parses the list of records corresponding to each trace event (method entry, exit, ...)
+     *  Record format v1:
+     *  u1  thread ID
+     *  u4  method ID | method action
+     *  u4  time delta since start, in usec
+     *
+     * Record format v2:
+     *  u2  thread ID
+     *  u4  method ID | method action
+     *  u4  time delta since start, in usec
+     *
+     * Record format v3:
+     *  u2  thread ID
+     *  u4  method ID | method action
+     *  u4  time delta since start, in usec
+     *  u4  wall time since start, in usec (when clock == "dual" only)
+     *
+     * 32 bits of microseconds is 70 minutes.
+     */
+    private void parseMethodTraceData(MappedByteBuffer buffer, int recordSize) {
+        int methodId;
+        int threadId;
+        int version = mTraceDataBuilder.getVersion();
+        VmTraceData.ClockType clockType = mTraceDataBuilder.getClockType();
+        while (buffer.hasRemaining()) {
+            int threadTime;
+            int globalTime;
+
+            int positionStart = buffer.position();
+
+            threadId = version == 1 ? buffer.getInt() : buffer.getShort();
+            methodId = buffer.getInt();
+
+            switch (clockType) {
+                case WALL:
+                    threadTime = 0;
+                    globalTime = buffer.getInt();
+                    break;
+                case DUAL:
+                    threadTime = buffer.getInt();
+                    globalTime = buffer.getInt();
+                    break;
+                case THREAD_CPU:
+                default:
+                    threadTime = buffer.getInt();
+                    globalTime = 0;
+                    break;
+            }
+
+            int positionEnd = buffer.position();
+            int bytesRead = positionEnd - positionStart;
+            if (bytesRead < recordSize) {
+                buffer.position(positionEnd + (recordSize - bytesRead));
+            }
+
+            int action = methodId & 0x03;
+            TraceAction methodAction;
+            switch (action) {
+                case 0:
+                    methodAction = TraceAction.METHOD_ENTER;
+                    break;
+                case 1:
+                    methodAction = TraceAction.METHOD_EXIT;
+                    break;
+                case 2:
+                    methodAction = TraceAction.METHOD_EXIT_UNROLL;
+                    break;
+                default:
+                    throw new RuntimeException(
+                            "Invalid trace action, expected one of method entry, exit or unroll.");
+            }
+            methodId = methodId & ~0x03;
+
+            mTraceDataBuilder.addMethodAction(threadId, methodId, methodAction, threadTime, globalTime);
+        }
+    }
+
+    /**
+     *  Parses the data header with the following format:
+     *  u4  magic ('SLOW')
+     *  u2  version
+     *  u2  offset to data
+     *  u8  start date/time in usec
+     *  u2  record size in bytes (version >= 2 only)
+     *  ... padding to 32 bytes
+
+     * @param buffer byte buffer pointing to the header
+     * @return record size for each data entry following the header
+     */
+    private int readDataFileHeader(MappedByteBuffer buffer) {
+        int magic = buffer.getInt();
+        if (magic != TRACE_MAGIC) {
+            String msg = String.format("Error: magic number mismatch; got 0x%x, expected 0x%x\n",
+                    magic, TRACE_MAGIC);
+            throw new RuntimeException(msg);
+        }
+
+        // read version
+        int version = buffer.getShort();
+        if (version != mTraceDataBuilder.getVersion()) {
+            String msg = String.format(
+                    "Error: version number mismatch; got %d in data header but %d in options\n",
+                    version, mTraceData.getVersion());
+            throw new RuntimeException(msg);
+        }
+        if (version < 1 || version > 3) {
+            String msg = String.format(
+                    "Error: unsupported trace version number %d.  "
+                            + "Please use a newer version of TraceView to read this file.",
+                    version);
+            throw new RuntimeException(msg);
+        }
+
+        // read offset
+        int offsetToData = buffer.getShort() - 16;
+
+        // read startWhen
+        buffer.getLong();
+
+        // read record size
+        int recordSize;
+        switch (version) {
+            case 1:
+                recordSize = 9;
+                break;
+            case 2:
+                recordSize = 10;
+                break;
+            default:
+                recordSize = buffer.getShort();
+                offsetToData -= 2;
+                break;
+        }
+
+        // Skip over offsetToData bytes
+        while (offsetToData-- > 0) {
+            buffer.get();
+        }
+
+        return recordSize;
+    }
+
+    private MappedByteBuffer mapFile(File f, long offset) throws IOException {
+        FileInputStream dataFile = new FileInputStream(f);
+        try {
+            FileChannel fc = dataFile.getChannel();
+            MappedByteBuffer buffer = fc.map(FileChannel.MapMode.READ_ONLY, offset,
+                    f.length() - offset);
+            buffer.order(ByteOrder.LITTLE_ENDIAN);
+            return buffer;
+        } finally {
+            dataFile.close(); // this *also* closes the associated channel, fc
+        }
+    }
+}
---
+++ perflib/src/test/java/com/android/tools/perflib/vmtrace/VmTraceParserTest.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tools.perflib.vmtrace;
+
+import com.android.annotations.NonNull;
+import com.android.utils.SparseArray;
+import com.google.common.base.Joiner;
+
+import junit.framework.TestCase;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+public class VmTraceParserTest extends TestCase {
+    public void testParseHeader() throws IOException {
+        File f = getFile("/header.trace");
+        VmTraceParser parser = new VmTraceParser(f);
+        parser.parseHeader(f);
+        VmTraceData traceData = parser.getTraceData();
+
+        assertEquals(3, traceData.getVersion());
+        assertTrue(traceData.isDataFileOverflow());
+        assertEquals(VmTraceData.ClockType.DUAL, traceData.getClockType());
+        assertEquals("dalvik", traceData.getVm());
+
+        SparseArray<String> threads = traceData.getThreads();
+        assertEquals(2, threads.size());
+        assertEquals("main", threads.get(1));
+        assertEquals("AsyncTask #1", threads.get(11));
+
+        Map<Long, MethodInfo> methods = traceData.getMethods();
+        assertEquals(4, methods.size());
+
+        MethodInfo info = traceData.getMethod(0x62830738);
+        assertNotNull(info);
+        assertEquals("android/graphics/Bitmap", info.className);
+        assertEquals("access$100", info.methodName);
+        assertEquals("(I)V", info.signature);
+        assertEquals("android/graphics/BitmapF.java", info.srcPath);
+        assertEquals(29, info.srcLineNumber);
+
+        info = traceData.getMethod(0x6282b4b0);
+        assertNotNull(info);
+        assertEquals(-1, info.srcLineNumber);
+    }
+
+    private class CallFormatter implements Call.Formatter {
+        private final Map<Long, MethodInfo> mMethodInfo;
+
+        public CallFormatter(Map<Long, MethodInfo> methodInfo) {
+            mMethodInfo = methodInfo;
+        }
+
+        @Override
+        public String format(Call c) {
+            MethodInfo info = mMethodInfo.get(c.getMethodId());
+            return info == null ? Long.toString(c.getMethodId()) : info.getName();
+        }
+    }
+
+    private void testTrace(String traceName, String threadName, String expectedCallSequence) throws IOException {
+        VmTraceData traceData = getVmTraceData(traceName);
+
+        int threadId = findThreadIdFromName(threadName, traceData.getThreads());
+        assertTrue(String.format("Thread %s was not found in the trace", threadName), threadId > 0);
+
+        List<Call> calls = traceData.getCalls(threadId);
+        String actual = formatCallStacks(calls, new CallFormatter(traceData.getMethods()));
+        assertEquals(expectedCallSequence, actual);
+    }
+
+    public void testBasicTrace() throws IOException {
+        String expected =
+                " -> android/os/Debug.startMethodTracing: (Ljava/lang/String;)V -> android/os/Debug.startMethodTracing: (Ljava/lang/String;II)V -> dalvik/system/VMDebug.startMethodTracing: (Ljava/lang/String;II)V\n"
+                        + " -> com/test/android/traceview/Basic.foo: ()V -> com/test/android/traceview/Basic.bar: ()I\n"
+                        + " -> android/os/Debug.stopMethodTracing: ()V -> dalvik/system/VMDebug.stopMethodTracing: ()V";
+        testTrace("/basic.trace", "AsyncTask #1", expected);
+    }
+
+    public void testMisMatchedTrace() throws IOException {
+        String expected =
+                " -> com/test/android/traceview/MisMatched.foo: ()V -> com/test/android/traceview/MisMatched.bar: ()V -> android/os/Debug.startMethodTracing: (Ljava/lang/String;)V -> android/os/Debug.startMethodTracing: (Ljava/lang/String;II)V -> dalvik/system/VMDebug.startMethodTracing: (Ljava/lang/String;II)V\n"
+                + "                                                                                                     -> com/test/android/traceview/MisMatched.baz: ()I\n"
+                + " -> android/os/Debug.stopMethodTracing: ()V -> dalvik/system/VMDebug.stopMethodTracing: ()V";
+        testTrace("/mismatched.trace", "AsyncTask #1", expected);
+    }
+
+    public void testExceptionTrace() throws IOException {
+        String expected =
+                " -> android/os/Debug.startMethodTracing: (Ljava/lang/String;)V -> android/os/Debug.startMethodTracing: (Ljava/lang/String;II)V -> dalvik/system/VMDebug.startMethodTracing: (Ljava/lang/String;II)V\n"
+                + " -> com/test/android/traceview/Exceptions.foo: ()V -> com/test/android/traceview/Exceptions.bar: ()V -> com/test/android/traceview/Exceptions.baz: ()V -> java/lang/RuntimeException.<init>: ()V -> java/lang/Exception.<init>: ()V -> java/lang/Throwable.<init>: ()V -> java/util/Collections.emptyList: ()Ljava/util/List;\n"
+                + "                                                                                                                                                                                                                                                                       -> java/lang/Throwable.fillInStackTrace: ()Ljava/lang/Throwable; -> java/lang/Throwable.nativeFillInStackTrace: ()Ljava/lang/Object;\n"
+                + " -> android/os/Debug.stopMethodTracing: ()V -> dalvik/system/VMDebug.stopMethodTracing: ()V";
+        testTrace("/exception.trace", "AsyncTask #1", expected);
+    }
+
+    private int findThreadIdFromName(@NonNull String threadName,
+            @NonNull SparseArray<String> threads) {
+        for (int i = 0; i < threads.size(); i++) {
+            int id = threads.keyAt(i);
+            String name = threads.valueAt(i);
+            if (threadName.equals(name)) {
+                return id;
+            }
+        }
+
+        return -1;
+    }
+
+    private String formatCallStacks(List<Call> calls, Call.Formatter formatter) {
+        if (calls == null) return "<none>";
+        List<String> callStacks = new ArrayList<String>(calls.size());
+
+        for (Call c : calls) {
+            callStacks.add(c.format(formatter));
+        }
+
+        return Joiner.on('\n').join(callStacks);
+    }
+
+    private VmTraceData getVmTraceData(String traceFilePath) throws IOException {
+        VmTraceParser parser = new VmTraceParser(getFile(traceFilePath));
+        parser.parse();
+        return parser.getTraceData();
+    }
+
+    private File getFile(String path) {
+        URL resource = getClass().getResource(path);
+        // Note: When running from an IntelliJ, make sure the IntelliJ compiler settings treats
+        // *.trace files as resources, otherwise they are excluded from compiler output
+        // resulting in a NPE.
+        assertNotNull(path + " not found", resource);
+        return new File(resource.getFile());
+    }
+}
