c22c1c15ff47aceb5449920b6809bed0c190fbbd
Tor Norbye <tnorbye@google.com>
2013-09-02 16:45:41 -0700

    Warn about custom lint jars that do not contain the expected keys
    Change-Id: I673b21ceb2748a43efb91072519679c08d7694d9

--- lint/cli/src/test/java/com/android/tools/lint/client/api/JarFileIssueRegistryTest.java
+++ lint/cli/src/test/java/com/android/tools/lint/client/api/JarFileIssueRegistryTest.java
@@ -15,17 +15,21 @@
  */
 package com.android.tools.lint.client.api;
 
+import com.android.annotations.NonNull;
+import com.android.annotations.Nullable;
 import com.android.tools.lint.checks.AbstractCheckTest;
 import com.android.tools.lint.detector.api.Detector;
+import com.android.tools.lint.detector.api.Severity;
 import com.google.common.io.Files;
 
 import java.io.File;
+import java.io.StringWriter;
 
 @SuppressWarnings("SpellCheckingInspection")
 public class JarFileIssueRegistryTest extends AbstractCheckTest {
     public void testError() {
         try {
-            JarFileIssueRegistry.get(new File("bogus"));
+            JarFileIssueRegistry.get(new TestLintClient(), new File("bogus"));
             fail("Expected exception for bogus path");
         } catch (Throwable t) {
             // pass
@@ -37,14 +41,31 @@ public class JarFileIssueRegistryTest extends AbstractCheckTest {
         File file1 = getTestfile(targetDir, "rules/appcompat.jar.data");
         File file2 = getTestfile(targetDir, "apicheck/unsupported.jar.data");
         assertTrue(file1.getPath(), file1.exists());
-        IssueRegistry registry1 = JarFileIssueRegistry.get(file1);
-        IssueRegistry registry2 = JarFileIssueRegistry.get(new File(file1.getPath()));
+        final StringWriter mLoggedWarnings = new StringWriter();
+        TestLintClient client = new TestLintClient() {
+            @Override
+            public void log(@NonNull Severity severity, @Nullable Throwable exception,
+                    @Nullable String format, @Nullable Object... args) {
+                if (format != null) {
+                    mLoggedWarnings.append(String.format(format, args));
+                }
+            }
+
+        };
+        IssueRegistry registry1 = JarFileIssueRegistry.get(client, file1);
+        IssueRegistry registry2 = JarFileIssueRegistry.get(client, new File(file1.getPath()));
         assertSame(registry1, registry2);
-        IssueRegistry registry3 = JarFileIssueRegistry.get(file2);
+        IssueRegistry registry3 = JarFileIssueRegistry.get(client, file2);
         assertNotSame(registry1, registry3);
 
         assertEquals(1, registry1.getIssues().size());
         assertEquals("AppCompatMethod", registry1.getIssues().get(0).getId());
+
+        assertEquals(
+                "Custom lint rule jar " + file2.getPath() + " does not contain a valid "
+                        + "registry manifest key (Lint-Registry).\n"
+                        + "Either the custom jar is invalid, or it uses an outdated API not "
+                        + "supported this lint client", mLoggedWarnings.toString());
     }
 
     @Override
