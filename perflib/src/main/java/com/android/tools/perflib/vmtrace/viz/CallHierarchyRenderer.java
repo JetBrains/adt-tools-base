9eaaa96a498a1bc60e08d1afeef7066546ad5488
Siva Velusamy <vsiva@google.com>
2013-09-05 18:07:38 -0700

    Fix percentage calculation.
    A previous commit enabled rendering based on thread or wall clock.
    This implies that the percentages should be computed based on
    the same clock used for rendering. (The current code mistakenly
    always used the thread clock).
    
    Change-Id: I7648ccc0c7adee1e34eb36b258ad24d7d7054bcf

--- perflib/src/main/java/com/android/tools/perflib/vmtrace/viz/CallHierarchyRenderer.java
+++ perflib/src/main/java/com/android/tools/perflib/vmtrace/viz/CallHierarchyRenderer.java
@@ -206,7 +206,8 @@ public class CallHierarchyRenderer {
         long methodTime = selector.get(info, mThreadName);
 
         // Always use inclusive time of top level to compute percentages.
-        long topLevelTime = getMethodInfo(mTopCall).getInclusiveTime(mThreadName, THREAD);
+        MethodInfo topMethod = mTraceData.getMethod(mTopCall.getMethodId());
+        long topLevelTime = INCLUSIVE_TIME_SELECTOR.get(topMethod, mThreadName);
 
         return (double)methodTime/topLevelTime * 100;
     }
