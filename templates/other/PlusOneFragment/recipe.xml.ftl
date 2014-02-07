tadfisher/platform_tools_base

Add new PlusOneFragment template
Change-Id: I2d7e00856707395ddd49064063f6e683147948d0

templates/other/PlusOneFragment/recipe.xml.ftl
@@ -1,0 +1,22 @@
+<?xml version="1.0"?>
+<recipe>
+	
+	<dependency mavenUrl="com.android.support:appcompat-v7:+"/> 
+	<dependency mavenUrl="com.google.android.gms:play-services:4.0.+"/>
+
+    <merge from="AndroidManifest.xml.ftl"
+             to="${escapeXmlAttribute(manifestOut)}/AndroidManifest.xml" />
+
+    <merge from="res/values/strings.xml" to="${escapeXmlAttribute(resOut)}/values/strings.xml" />
+
+        <instantiate from="res/layout/fragment_plus_one.xml.ftl"
+                       to="${escapeXmlAttribute(resOut)}/layout/fragment_${classToResource(className)}.xml" />
+
+        <open file="${escapeXmlAttribute(resOut)}/layout/fragment_${classToResource(className)}.xml" />
+
+    <open file="${escapeXmlAttribute(srcOut)}/${className}.java" />
+
+    <instantiate from="src/app_package/PlusOneFragment.java.ftl"
+                   to="${escapeXmlAttribute(srcOut)}/${className}.java" />
+
+</recipe>

JetBrains/adt-tools-base

templates/other/PlusOneFragment/recipe.xml.ftl
@@ -1,0 +1,22 @@
+<?xml version="1.0"?>
+<recipe>
+
+    <dependency mavenUrl="com.android.support:support-v4:+"/>
+    <dependency mavenUrl="com.google.android.gms:play-services:4.0.+"/>
+
+    <merge from="AndroidManifest.xml.ftl"
+             to="${escapeXmlAttribute(manifestOut)}/AndroidManifest.xml" />
+
+    <merge from="res/values/strings.xml" to="${escapeXmlAttribute(resOut)}/values/strings.xml" />
+
+    <instantiate from="res/layout/fragment_plus_one.xml.ftl"
+                   to="${escapeXmlAttribute(resOut)}/layout/fragment_${classToResource(className)}.xml" />
+
+    <open file="${escapeXmlAttribute(resOut)}/layout/fragment_${classToResource(className)}.xml" />
+
+    <open file="${escapeXmlAttribute(srcOut)}/${className}.java" />
+
+    <instantiate from="src/app_package/PlusOneFragment.java.ftl"
+                   to="${escapeXmlAttribute(srcOut)}/${className}.java" />
+
+</recipe>