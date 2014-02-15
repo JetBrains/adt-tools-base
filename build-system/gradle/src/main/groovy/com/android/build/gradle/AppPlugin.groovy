ccde919cfa2b2766721c9344211354c3d0a37eed
Xavier Ducrohet <xav@google.com>
2014-02-14 17:41:18 -0800

    Refactor common code to basePlugin
    Change-Id: Id848708c34a5120faef22d6b59fdb22f95464c87

--- build-system/gradle/src/main/groovy/com/android/build/gradle/AppPlugin.groovy
+++ build-system/gradle/src/main/groovy/com/android/build/gradle/AppPlugin.groovy
@@ -16,34 +16,18 @@
 
 package com.android.build.gradle
 
-import com.android.build.gradle.internal.VariantManager
-import com.android.build.gradle.internal.dsl.BuildTypeDsl
-import com.android.build.gradle.internal.dsl.BuildTypeFactory
-import com.android.build.gradle.internal.dsl.GroupableProductFlavorDsl
-import com.android.build.gradle.internal.dsl.GroupableProductFlavorFactory
-import com.android.build.gradle.internal.dsl.SigningConfigDsl
-import com.android.build.gradle.internal.dsl.SigningConfigFactory
 import com.android.build.gradle.internal.test.PluginHolder
-import com.android.builder.DefaultBuildType
-import com.android.builder.model.SigningConfig
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.internal.project.ProjectInternal
 import org.gradle.internal.reflect.Instantiator
 import org.gradle.tooling.provider.model.ToolingModelBuilderRegistry
 
 import javax.inject.Inject
-
-import static com.android.builder.BuilderConstants.DEBUG
-import static com.android.builder.BuilderConstants.RELEASE
 /**
  * Gradle plugin class for 'application' projects.
  */
 class AppPlugin extends BasePlugin implements Plugin<Project> {
     static PluginHolder pluginHolder;
-
-    AppExtension extension
-    VariantManager variantManager
 
     @Inject
     public AppPlugin(Instantiator instantiator, ToolingModelBuilderRegistry registry) {
@@ -51,13 +35,8 @@ class AppPlugin extends BasePlugin implements Plugin<Project> {
     }
 
     @Override
-    public AppExtension getExtension() {
-        return extension
-    }
-
-    @Override
-    public VariantManager getVariantManager() {
-        return variantManager
+    protected Class<? extends BaseExtension> getExtensionClass() {
+        return AppExtension.class
     }
 
     @Override
@@ -67,49 +46,6 @@ class AppPlugin extends BasePlugin implements Plugin<Project> {
         // This is for testing.
         if (pluginHolder != null) {
             pluginHolder.plugin = this;
-        }
-
-        def buildTypeContainer = project.container(DefaultBuildType,
-                new BuildTypeFactory(instantiator,  project.fileResolver, project.getLogger()))
-        def productFlavorContainer = project.container(GroupableProductFlavorDsl,
-                new GroupableProductFlavorFactory(instantiator, project.fileResolver, project.getLogger()))
-        def signingConfigContainer = project.container(SigningConfig,
-                new SigningConfigFactory(instantiator))
-
-        extension = project.extensions.create('android', AppExtension,
-                this, (ProjectInternal) project, instantiator,
-                buildTypeContainer, productFlavorContainer, signingConfigContainer)
-        setBaseExtension(extension)
-
-        variantManager = new VariantManager(project, this, extension)
-
-        // map the whenObjectAdded callbacks on the containers.
-        signingConfigContainer.whenObjectAdded { SigningConfig signingConfig ->
-            variantManager.addSigningConfig((SigningConfigDsl) signingConfig)
-        }
-
-        buildTypeContainer.whenObjectAdded { DefaultBuildType buildType ->
-            variantManager.addBuildType((BuildTypeDsl) buildType)
-        }
-
-        productFlavorContainer.whenObjectAdded { GroupableProductFlavorDsl productFlavor ->
-            variantManager.addProductFlavor(productFlavor)
-        }
-
-        // create default Objects, signingConfig first as its used by the BuildTypes.
-        signingConfigContainer.create(DEBUG)
-        buildTypeContainer.create(DEBUG)
-        buildTypeContainer.create(RELEASE)
-
-        // map whenObjectRemoved on the containers to throw an exception.
-        signingConfigContainer.whenObjectRemoved {
-            throw new UnsupportedOperationException("Removing signingConfigs is not supported.")
-        }
-        buildTypeContainer.whenObjectRemoved {
-            throw new UnsupportedOperationException("Removing build types is not supported.")
-        }
-        productFlavorContainer.whenObjectRemoved {
-            throw new UnsupportedOperationException("Removing product flavors is not supported.")
         }
     }
 
