tadfisher/platform_tools_base

Update locations of output files.
Three main folders:
- generated: for generated source and resources
- outputs: for main output files + reports
- intermediates: for all other files.

Change-Id: Ic6ce0b151b9d112a743168608d2ae0bc847aaf17

build-system/gradle/src/build-test/groovy/com/android/build/gradle/ManualBuildTest.java
@@ -16,6 +16,8 @@
 
 package com.android.build.gradle;
 
+import static com.android.build.gradle.BasePlugin.FD_I;
+
 import com.google.common.base.Charsets;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
@@ -47,7 +49,7 @@ public class ManualBuildTest extends BuildTest {
 
     public void testOverlay1Content() throws Exception {
         File project = buildProject("overlay1", BasePlugin.GRADLE_MIN_VERSION);
-        File drawableOutput = new File(project, "build/res/all/debug/drawable");
+        File drawableOutput = new File(project, "build/" + FD_I + "/res/debug/drawable");
 
         checkImageColor(drawableOutput, "no_overlay.png", GREEN);
         checkImageColor(drawableOutput, "type_overlay.png", GREEN);
@@ -55,7 +57,7 @@ public class ManualBuildTest extends BuildTest {
 
     public void testOverlay2Content() throws Exception {
         File project = buildProject("overlay2", BasePlugin.GRADLE_MIN_VERSION);
-        File drawableOutput = new File(project, "build/res/all/one/debug/drawable");
+        File drawableOutput = new File(project, "build/" + FD_I + "/res/one/debug/drawable");
 
         checkImageColor(drawableOutput, "no_overlay.png", GREEN);
         checkImageColor(drawableOutput, "type_overlay.png", GREEN);
@@ -66,7 +68,7 @@ public class ManualBuildTest extends BuildTest {
 
     public void testOverlay3Content() throws Exception {
         File project = buildProject("overlay3", BasePlugin.GRADLE_MIN_VERSION);
-        File drawableOutput = new File(project, "build/res/all/freebeta/debug/drawable");
+        File drawableOutput = new File(project, "build/" + FD_I + "/res/freebeta/debug/drawable");
 
         checkImageColor(drawableOutput, "no_overlay.png", GREEN);
         checkImageColor(drawableOutput, "debug_overlay.png", GREEN);
@@ -76,7 +78,7 @@ public class ManualBuildTest extends BuildTest {
         checkImageColor(drawableOutput, "free_beta_debug_overlay.png", GREEN);
         checkImageColor(drawableOutput, "free_normal_overlay.png", RED);
 
-        drawableOutput = new File(project, "build/res/all/freenormal/debug/drawable");
+        drawableOutput = new File(project, "build/" + FD_I + "/res/freenormal/debug/drawable");
 
         checkImageColor(drawableOutput, "no_overlay.png", GREEN);
         checkImageColor(drawableOutput, "debug_overlay.png", GREEN);
@@ -86,7 +88,7 @@ public class ManualBuildTest extends BuildTest {
         checkImageColor(drawableOutput, "free_beta_debug_overlay.png", RED);
         checkImageColor(drawableOutput, "free_normal_overlay.png", GREEN);
 
-        drawableOutput = new File(project, "build/res/all/paidbeta/debug/drawable");
+        drawableOutput = new File(project, "build/" + FD_I + "/res/paidbeta/debug/drawable");
 
         checkImageColor(drawableOutput, "no_overlay.png", GREEN);
         checkImageColor(drawableOutput, "debug_overlay.png", GREEN);
@@ -118,7 +120,7 @@ public class ManualBuildTest extends BuildTest {
 
     public void testLibsManifestMerging() throws Exception {
         File project = new File(testDir, "libsTest");
-        File fileOutput = new File(project, "libapp/build/bundles/release/AndroidManifest.xml");
+        File fileOutput = new File(project, "libapp/build/" + FD_I + "/bundles/release/AndroidManifest.xml");
 
         runGradleTasks(sdkDir, ndkDir, BasePlugin.GRADLE_MIN_VERSION,
                 project, "clean", "build");
@@ -128,7 +130,7 @@ public class ManualBuildTest extends BuildTest {
     // test whether a library project has its fields ProGuarded
     public void testLibProguard() throws Exception {
         File project = new File(testDir, "libProguard");
-        File fileOutput = new File(project, "build/proguard/release");
+        File fileOutput = new File(project, "build/" + FD_I + "/proguard/release");
 
         runGradleTasks(sdkDir, ndkDir, BasePlugin.GRADLE_MIN_VERSION,
           project, "clean", "build");
@@ -139,8 +141,8 @@ public class ManualBuildTest extends BuildTest {
     // test whether proguard.txt has been correctly merged
     public void testLibProguardConsumerFile() throws Exception {
         File project = new File(testDir, "libProguardConsumerFiles");
-        File debugFileOutput = new File(project, "build/bundles/debug");
-        File releaseFileOutput = new File(project, "build/bundles/release");
+        File debugFileOutput = new File(project, "build/" + FD_I + "/bundles/debug");
+        File releaseFileOutput = new File(project, "build/" + FD_I + "/bundles/release");
 
         runGradleTasks(sdkDir, ndkDir, BasePlugin.GRADLE_MIN_VERSION,
             project, "clean", "build");
@@ -150,7 +152,7 @@ public class ManualBuildTest extends BuildTest {
 
     public void testAnnotations() throws Exception {
         File project = new File(testDir, "extractAnnotations");
-        File debugFileOutput = new File(project, "build/bundles/debug");
+        File debugFileOutput = new File(project, "build/" + FD_I + "/bundles/debug");
 
         runGradleTasks(sdkDir, ndkDir, BasePlugin.GRADLE_MIN_VERSION,
                 project, "clean", "extractDebugAnnotations");

build-system/gradle/src/build-test/groovy/com/android/build/gradle/PackagingBuildTest.java
@@ -16,6 +16,8 @@
 
 package com.android.build.gradle;
 
+import static com.android.build.gradle.BasePlugin.FD_O;
+
 import com.android.annotations.NonNull;
 
 import junit.framework.Test;
@@ -73,7 +75,7 @@ public class PackagingBuildTest extends BuildTest {
         File projectFolder = buildProject(projectName, BasePlugin.GRADLE_MIN_VERSION);
 
         // TODO replace with model access.
-        File apkFolder = new File(projectFolder, "build/apk");
+        File apkFolder = new File(projectFolder, "build/" + FD_O + "/apk");
 
         File apk = new File(apkFolder, projectName + "-debug-unaligned.apk");
 

build-system/gradle/src/main/groovy/com/android/build/gradle/internal/variant/LibraryVariantFactory.groovy
@@ -45,6 +45,9 @@ import org.gradle.tooling.BuildException
 import static com.android.SdkConstants.FN_ANNOTATIONS_ZIP
 import static com.android.SdkConstants.LIBS_FOLDER
 import static com.android.build.gradle.BasePlugin.DIR_BUNDLES
+import static com.android.build.gradle.BasePlugin.FD_I
+import static com.android.build.gradle.BasePlugin.FD_O
+
 /**
  */
 public class LibraryVariantFactory implements VariantFactory {
@@ -110,7 +113,7 @@ public class LibraryVariantFactory implements VariantFactory {
         // the dependencies. This is what gets packaged in the aar.
         MergeResources packageRes = basePlugin.basicCreateMergeResourcesTask(variantData,
                 "package",
-                "$project.buildDir/$DIR_BUNDLES/${dirName}/res",
+                "$project.buildDir/${FD_I}/$DIR_BUNDLES/${dirName}/res",
                 false /*includeDependencies*/,
                 false /*process9Patch*/)
 
@@ -127,7 +130,7 @@ public class LibraryVariantFactory implements VariantFactory {
 
         // Add a task to merge the assets folders
         basePlugin.createMergeAssetsTask(variantData,
-                "$project.buildDir/$DIR_BUNDLES/${dirName}/assets",
+                "$project.buildDir/${FD_I}/$DIR_BUNDLES/${dirName}/assets",
                 false /*includeDependencies*/)
 
         // Add a task to create the BuildConfig class
@@ -136,7 +139,7 @@ public class LibraryVariantFactory implements VariantFactory {
         // Add a task to generate resource source files, directing the location
         // of the r.txt file to be directly in the bundle.
         basePlugin.createProcessResTask(variantData,
-                "$project.buildDir/$DIR_BUNDLES/${dirName}",
+                "$project.buildDir/${FD_I}/$DIR_BUNDLES/${dirName}",
                 false /*generateResourcePackage*/,
                 )
 
@@ -144,7 +147,7 @@ public class LibraryVariantFactory implements VariantFactory {
         basePlugin.createProcessJavaResTask(variantData)
 
         basePlugin.createAidlTask(variantData, basePlugin.project.file(
-                "$basePlugin.project.buildDir/$DIR_BUNDLES/${dirName}/$SdkConstants.FD_AIDL"))
+                "$basePlugin.project.buildDir/${FD_I}/$DIR_BUNDLES/${dirName}/$SdkConstants.FD_AIDL"))
 
         // Add a compile task
         basePlugin.createCompileTask(variantData, null/*testedVariant*/)
@@ -161,7 +164,7 @@ public class LibraryVariantFactory implements VariantFactory {
         packageJniLibs.from(variantConfig.jniLibsList).include("**/*.so")
         packageJniLibs.from(variantData.ndkCompileTask.soFolder).include("**/*.so")
         packageJniLibs.into(project.file(
-                "$project.buildDir/$DIR_BUNDLES/${dirName}/jni"))
+                "$project.buildDir/${FD_I}/$DIR_BUNDLES/${dirName}/jni"))
 
         // package the renderscript header files files into the bundle folder
         Sync packageRenderscript = project.tasks.create(
@@ -170,7 +173,7 @@ public class LibraryVariantFactory implements VariantFactory {
         // package from 3 sources. the order is important to make sure the override works well.
         packageRenderscript.from(variantConfig.renderscriptSourceList).include("**/*.rsh")
         packageRenderscript.into(project.file(
-                "$project.buildDir/$DIR_BUNDLES/${dirName}/$SdkConstants.FD_RENDERSCRIPT"))
+                "$project.buildDir/${FD_I}/$DIR_BUNDLES/${dirName}/$SdkConstants.FD_RENDERSCRIPT"))
 
         // merge consumer proguard files from different build types and flavors
         MergeFileTask mergeProGuardFileTask = project.tasks.create(
@@ -180,7 +183,7 @@ public class LibraryVariantFactory implements VariantFactory {
             project.files(variantConfig.getConsumerProguardFiles()).files }
         mergeProGuardFileTask.conventionMapping.outputFile = {
             project.file(
-                    "$project.buildDir/$DIR_BUNDLES/${dirName}/$LibraryBundle.FN_PROGUARD_TXT")
+                    "$project.buildDir/${FD_I}/$DIR_BUNDLES/${dirName}/$LibraryBundle.FN_PROGUARD_TXT")
         }
 
         // copy lint.jar into the bundle folder
@@ -188,8 +191,8 @@ public class LibraryVariantFactory implements VariantFactory {
                 "copy${fullName.capitalize()}Lint",
                 Copy)
         lintCopy.dependsOn basePlugin.lintCompile
-        lintCopy.from("$project.buildDir/lint/lint.jar")
-        lintCopy.into("$project.buildDir/$DIR_BUNDLES/$dirName")
+        lintCopy.from("$project.buildDir/${FD_I}/lint/lint.jar")
+        lintCopy.into("$project.buildDir/${FD_I}/$DIR_BUNDLES/$dirName")
 
         Zip bundle = project.tasks.create(
                 "bundle${fullName.capitalize()}",
@@ -222,7 +225,7 @@ public class LibraryVariantFactory implements VariantFactory {
                 jacocoTask.dependsOn variantData.javaCompileTask
                 jacocoTask.conventionMapping.jacocoClasspath = { project.configurations[JacocoPlugin.ANT_CONFIGURATION_NAME] }
                 jacocoTask.conventionMapping.inputDir = { variantData.javaCompileTask.destinationDir }
-                jacocoTask.conventionMapping.outputDir = { project.file("${project.buildDir}/coverage-instrumented-classes/${variantConfig.dirName}") }
+                jacocoTask.conventionMapping.outputDir = { project.file("${project.buildDir}/${FD_I}/coverage-instrumented-classes/${variantConfig.dirName}") }
 
                 agentTask = basePlugin.getJacocoAgentTask()
             }
@@ -237,7 +240,7 @@ public class LibraryVariantFactory implements VariantFactory {
                 packageLocalJar.from(new File(agentTask.destinationDir, BasePlugin.FILE_JACOCO_AGENT))
             }
             packageLocalJar.into(project.file(
-                    "$project.buildDir/$DIR_BUNDLES/${dirName}/$LIBS_FOLDER"))
+                    "$project.buildDir/${FD_I}/$DIR_BUNDLES/${dirName}/$LIBS_FOLDER"))
 
             // jar the classes.
             Jar jar = project.tasks.create("package${fullName.capitalize()}Jar", Jar);
@@ -251,7 +254,7 @@ public class LibraryVariantFactory implements VariantFactory {
             jar.from(variantData.processJavaResourcesTask.destinationDir)
 
             jar.destinationDir = project.file(
-                    "$project.buildDir/$DIR_BUNDLES/${dirName}")
+                    "$project.buildDir/${FD_I}/$DIR_BUNDLES/${dirName}")
             jar.archiveName = "classes.jar"
 
             String packageName = variantConfig.getPackageFromManifest()
@@ -279,9 +282,9 @@ public class LibraryVariantFactory implements VariantFactory {
         }
 
         bundle.setDescription("Assembles a bundle containing the library in ${fullName.capitalize()}.");
-        bundle.destinationDir = project.file("$project.buildDir/libs")
+        bundle.destinationDir = project.file("$project.buildDir/${FD_O}/aar")
         bundle.extension = BuilderConstants.EXT_LIB_ARCHIVE
-        bundle.from(project.file("$project.buildDir/$DIR_BUNDLES/${dirName}"))
+        bundle.from(project.file("$project.buildDir/${FD_I}/$DIR_BUNDLES/${dirName}"))
 
         libVariantData.packageLibTask = bundle
         variantData.outputFile = bundle.archivePath
@@ -312,7 +315,7 @@ public class LibraryVariantFactory implements VariantFactory {
         // configure the variant to be testable.
         variantConfig.output = new LibraryBundle(
                 bundle.archivePath,
-                project.file("$project.buildDir/$DIR_BUNDLES/${dirName}"),
+                project.file("$project.buildDir/${FD_I}/$DIR_BUNDLES/${dirName}"),
                 variantData.getName()) {
 
             @Override
@@ -360,9 +363,9 @@ public class LibraryVariantFactory implements VariantFactory {
         task.group = org.gradle.api.plugins.BasePlugin.BUILD_GROUP
         task.plugin = basePlugin
         task.variant = variantData
-        task.destinationDir = project.file("$project.buildDir/$DIR_BUNDLES/${dirName}")
+        task.destinationDir = project.file("$project.buildDir/${FD_I}/$DIR_BUNDLES/${dirName}")
         task.output = new File(task.destinationDir, FN_ANNOTATIONS_ZIP)
-        task.classDir = project.file("$project.buildDir/classes/${variantData.variantConfiguration.dirName}")
+        task.classDir = project.file("$project.buildDir/${FD_I}/classes/${variantData.variantConfiguration.dirName}")
         task.source = variantData.getJavaSources()
         task.encoding = extension.compileOptions.encoding
         task.sourceCompatibility = extension.compileOptions.sourceCompatibility

JetBrains/adt-tools-base

build-system/gradle/src/build-test/groovy/com/android/build/gradle/ManualBuildTest.java
@@ -16,6 +16,9 @@
 
 package com.android.build.gradle;
 
+import static com.android.build.gradle.BasePlugin.FD_INTERMEDIATES;
+import static com.android.build.gradle.BasePlugin.FD_OUTPUTS;
+
 import com.google.common.base.Charsets;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
@@ -47,7 +50,7 @@ public class ManualBuildTest extends BuildTest {
 
     public void testOverlay1Content() throws Exception {
         File project = buildProject("overlay1", BasePlugin.GRADLE_MIN_VERSION);
-        File drawableOutput = new File(project, "build/res/all/debug/drawable");
+        File drawableOutput = new File(project, "build/" + FD_INTERMEDIATES + "/res/debug/drawable");
 
         checkImageColor(drawableOutput, "no_overlay.png", GREEN);
         checkImageColor(drawableOutput, "type_overlay.png", GREEN);
@@ -55,7 +58,7 @@ public class ManualBuildTest extends BuildTest {
 
     public void testOverlay2Content() throws Exception {
         File project = buildProject("overlay2", BasePlugin.GRADLE_MIN_VERSION);
-        File drawableOutput = new File(project, "build/res/all/one/debug/drawable");
+        File drawableOutput = new File(project, "build/" + FD_INTERMEDIATES + "/res/one/debug/drawable");
 
         checkImageColor(drawableOutput, "no_overlay.png", GREEN);
         checkImageColor(drawableOutput, "type_overlay.png", GREEN);
@@ -66,7 +69,7 @@ public class ManualBuildTest extends BuildTest {
 
     public void testOverlay3Content() throws Exception {
         File project = buildProject("overlay3", BasePlugin.GRADLE_MIN_VERSION);
-        File drawableOutput = new File(project, "build/res/all/freebeta/debug/drawable");
+        File drawableOutput = new File(project, "build/" + FD_INTERMEDIATES + "/res/freebeta/debug/drawable");
 
         checkImageColor(drawableOutput, "no_overlay.png", GREEN);
         checkImageColor(drawableOutput, "debug_overlay.png", GREEN);
@@ -76,7 +79,7 @@ public class ManualBuildTest extends BuildTest {
         checkImageColor(drawableOutput, "free_beta_debug_overlay.png", GREEN);
         checkImageColor(drawableOutput, "free_normal_overlay.png", RED);
 
-        drawableOutput = new File(project, "build/res/all/freenormal/debug/drawable");
+        drawableOutput = new File(project, "build/" + FD_INTERMEDIATES + "/res/freenormal/debug/drawable");
 
         checkImageColor(drawableOutput, "no_overlay.png", GREEN);
         checkImageColor(drawableOutput, "debug_overlay.png", GREEN);
@@ -86,7 +89,7 @@ public class ManualBuildTest extends BuildTest {
         checkImageColor(drawableOutput, "free_beta_debug_overlay.png", RED);
         checkImageColor(drawableOutput, "free_normal_overlay.png", GREEN);
 
-        drawableOutput = new File(project, "build/res/all/paidbeta/debug/drawable");
+        drawableOutput = new File(project, "build/" + FD_INTERMEDIATES + "/res/paidbeta/debug/drawable");
 
         checkImageColor(drawableOutput, "no_overlay.png", GREEN);
         checkImageColor(drawableOutput, "debug_overlay.png", GREEN);
@@ -118,7 +121,7 @@ public class ManualBuildTest extends BuildTest {
 
     public void testLibsManifestMerging() throws Exception {
         File project = new File(testDir, "libsTest");
-        File fileOutput = new File(project, "libapp/build/bundles/release/AndroidManifest.xml");
+        File fileOutput = new File(project, "libapp/build/" + FD_INTERMEDIATES + "/bundles/release/AndroidManifest.xml");
 
         runGradleTasks(sdkDir, ndkDir, BasePlugin.GRADLE_MIN_VERSION,
                 project, "clean", "build");
@@ -128,7 +131,7 @@ public class ManualBuildTest extends BuildTest {
     // test whether a library project has its fields ProGuarded
     public void testLibProguard() throws Exception {
         File project = new File(testDir, "libProguard");
-        File fileOutput = new File(project, "build/proguard/release");
+        File fileOutput = new File(project, "build/" + FD_OUTPUTS + "/proguard/release");
 
         runGradleTasks(sdkDir, ndkDir, BasePlugin.GRADLE_MIN_VERSION,
           project, "clean", "build");
@@ -139,8 +142,8 @@ public class ManualBuildTest extends BuildTest {
     // test whether proguard.txt has been correctly merged
     public void testLibProguardConsumerFile() throws Exception {
         File project = new File(testDir, "libProguardConsumerFiles");
-        File debugFileOutput = new File(project, "build/bundles/debug");
-        File releaseFileOutput = new File(project, "build/bundles/release");
+        File debugFileOutput = new File(project, "build/" + FD_INTERMEDIATES + "/bundles/debug");
+        File releaseFileOutput = new File(project, "build/" + FD_INTERMEDIATES + "/bundles/release");
 
         runGradleTasks(sdkDir, ndkDir, BasePlugin.GRADLE_MIN_VERSION,
             project, "clean", "build");
@@ -150,7 +153,7 @@ public class ManualBuildTest extends BuildTest {
 
     public void testAnnotations() throws Exception {
         File project = new File(testDir, "extractAnnotations");
-        File debugFileOutput = new File(project, "build/bundles/debug");
+        File debugFileOutput = new File(project, "build/" + FD_INTERMEDIATES + "/bundles/debug");
 
         runGradleTasks(sdkDir, ndkDir, BasePlugin.GRADLE_MIN_VERSION,
                 project, "clean", "extractDebugAnnotations");

build-system/gradle/src/build-test/groovy/com/android/build/gradle/PackagingBuildTest.java
@@ -16,6 +16,8 @@
 
 package com.android.build.gradle;
 
+import static com.android.build.gradle.BasePlugin.FD_OUTPUTS;
+
 import com.android.annotations.NonNull;
 
 import junit.framework.Test;
@@ -73,7 +75,7 @@ public class PackagingBuildTest extends BuildTest {
         File projectFolder = buildProject(projectName, BasePlugin.GRADLE_MIN_VERSION);
 
         // TODO replace with model access.
-        File apkFolder = new File(projectFolder, "build/apk");
+        File apkFolder = new File(projectFolder, "build/" + FD_OUTPUTS + "/apk");
 
         File apk = new File(apkFolder, projectName + "-debug-unaligned.apk");
 

build-system/gradle/src/main/groovy/com/android/build/gradle/internal/variant/LibraryVariantFactory.groovy
@@ -45,6 +45,9 @@ import org.gradle.tooling.BuildException
 import static com.android.SdkConstants.FN_ANNOTATIONS_ZIP
 import static com.android.SdkConstants.LIBS_FOLDER
 import static com.android.build.gradle.BasePlugin.DIR_BUNDLES
+import static com.android.build.gradle.BasePlugin.FD_INTERMEDIATES
+import static com.android.build.gradle.BasePlugin.FD_OUTPUTS
+
 /**
  */
 public class LibraryVariantFactory implements VariantFactory {
@@ -110,7 +113,7 @@ public class LibraryVariantFactory implements VariantFactory {
         // the dependencies. This is what gets packaged in the aar.
         MergeResources packageRes = basePlugin.basicCreateMergeResourcesTask(variantData,
                 "package",
-                "$project.buildDir/$DIR_BUNDLES/${dirName}/res",
+                "$project.buildDir/${FD_INTERMEDIATES}/$DIR_BUNDLES/${dirName}/res",
                 false /*includeDependencies*/,
                 false /*process9Patch*/)
 
@@ -127,7 +130,7 @@ public class LibraryVariantFactory implements VariantFactory {
 
         // Add a task to merge the assets folders
         basePlugin.createMergeAssetsTask(variantData,
-                "$project.buildDir/$DIR_BUNDLES/${dirName}/assets",
+                "$project.buildDir/${FD_INTERMEDIATES}/$DIR_BUNDLES/${dirName}/assets",
                 false /*includeDependencies*/)
 
         // Add a task to create the BuildConfig class
@@ -136,7 +139,7 @@ public class LibraryVariantFactory implements VariantFactory {
         // Add a task to generate resource source files, directing the location
         // of the r.txt file to be directly in the bundle.
         basePlugin.createProcessResTask(variantData,
-                "$project.buildDir/$DIR_BUNDLES/${dirName}",
+                "$project.buildDir/${FD_INTERMEDIATES}/$DIR_BUNDLES/${dirName}",
                 false /*generateResourcePackage*/,
                 )
 
@@ -144,7 +147,7 @@ public class LibraryVariantFactory implements VariantFactory {
         basePlugin.createProcessJavaResTask(variantData)
 
         basePlugin.createAidlTask(variantData, basePlugin.project.file(
-                "$basePlugin.project.buildDir/$DIR_BUNDLES/${dirName}/$SdkConstants.FD_AIDL"))
+                "$basePlugin.project.buildDir/${FD_INTERMEDIATES}/$DIR_BUNDLES/${dirName}/$SdkConstants.FD_AIDL"))
 
         // Add a compile task
         basePlugin.createCompileTask(variantData, null/*testedVariant*/)
@@ -161,7 +164,7 @@ public class LibraryVariantFactory implements VariantFactory {
         packageJniLibs.from(variantConfig.jniLibsList).include("**/*.so")
         packageJniLibs.from(variantData.ndkCompileTask.soFolder).include("**/*.so")
         packageJniLibs.into(project.file(
-                "$project.buildDir/$DIR_BUNDLES/${dirName}/jni"))
+                "$project.buildDir/${FD_INTERMEDIATES}/$DIR_BUNDLES/${dirName}/jni"))
 
         // package the renderscript header files files into the bundle folder
         Sync packageRenderscript = project.tasks.create(
@@ -170,7 +173,7 @@ public class LibraryVariantFactory implements VariantFactory {
         // package from 3 sources. the order is important to make sure the override works well.
         packageRenderscript.from(variantConfig.renderscriptSourceList).include("**/*.rsh")
         packageRenderscript.into(project.file(
-                "$project.buildDir/$DIR_BUNDLES/${dirName}/$SdkConstants.FD_RENDERSCRIPT"))
+                "$project.buildDir/${FD_INTERMEDIATES}/$DIR_BUNDLES/${dirName}/$SdkConstants.FD_RENDERSCRIPT"))
 
         // merge consumer proguard files from different build types and flavors
         MergeFileTask mergeProGuardFileTask = project.tasks.create(
@@ -180,7 +183,7 @@ public class LibraryVariantFactory implements VariantFactory {
             project.files(variantConfig.getConsumerProguardFiles()).files }
         mergeProGuardFileTask.conventionMapping.outputFile = {
             project.file(
-                    "$project.buildDir/$DIR_BUNDLES/${dirName}/$LibraryBundle.FN_PROGUARD_TXT")
+                    "$project.buildDir/${FD_INTERMEDIATES}/$DIR_BUNDLES/${dirName}/$LibraryBundle.FN_PROGUARD_TXT")
         }
 
         // copy lint.jar into the bundle folder
@@ -188,8 +191,8 @@ public class LibraryVariantFactory implements VariantFactory {
                 "copy${fullName.capitalize()}Lint",
                 Copy)
         lintCopy.dependsOn basePlugin.lintCompile
-        lintCopy.from("$project.buildDir/lint/lint.jar")
-        lintCopy.into("$project.buildDir/$DIR_BUNDLES/$dirName")
+        lintCopy.from("$project.buildDir/${FD_INTERMEDIATES}/lint/lint.jar")
+        lintCopy.into("$project.buildDir/${FD_INTERMEDIATES}/$DIR_BUNDLES/$dirName")
 
         Zip bundle = project.tasks.create(
                 "bundle${fullName.capitalize()}",
@@ -222,7 +225,7 @@ public class LibraryVariantFactory implements VariantFactory {
                 jacocoTask.dependsOn variantData.javaCompileTask
                 jacocoTask.conventionMapping.jacocoClasspath = { project.configurations[JacocoPlugin.ANT_CONFIGURATION_NAME] }
                 jacocoTask.conventionMapping.inputDir = { variantData.javaCompileTask.destinationDir }
-                jacocoTask.conventionMapping.outputDir = { project.file("${project.buildDir}/coverage-instrumented-classes/${variantConfig.dirName}") }
+                jacocoTask.conventionMapping.outputDir = { project.file("${project.buildDir}/${FD_INTERMEDIATES}/coverage-instrumented-classes/${variantConfig.dirName}") }
 
                 agentTask = basePlugin.getJacocoAgentTask()
             }
@@ -237,7 +240,7 @@ public class LibraryVariantFactory implements VariantFactory {
                 packageLocalJar.from(new File(agentTask.destinationDir, BasePlugin.FILE_JACOCO_AGENT))
             }
             packageLocalJar.into(project.file(
-                    "$project.buildDir/$DIR_BUNDLES/${dirName}/$LIBS_FOLDER"))
+                    "$project.buildDir/${FD_INTERMEDIATES}/$DIR_BUNDLES/${dirName}/$LIBS_FOLDER"))
 
             // jar the classes.
             Jar jar = project.tasks.create("package${fullName.capitalize()}Jar", Jar);
@@ -251,7 +254,7 @@ public class LibraryVariantFactory implements VariantFactory {
             jar.from(variantData.processJavaResourcesTask.destinationDir)
 
             jar.destinationDir = project.file(
-                    "$project.buildDir/$DIR_BUNDLES/${dirName}")
+                    "$project.buildDir/${FD_INTERMEDIATES}/$DIR_BUNDLES/${dirName}")
             jar.archiveName = "classes.jar"
 
             String packageName = variantConfig.getPackageFromManifest()
@@ -279,9 +282,9 @@ public class LibraryVariantFactory implements VariantFactory {
         }
 
         bundle.setDescription("Assembles a bundle containing the library in ${fullName.capitalize()}.");
-        bundle.destinationDir = project.file("$project.buildDir/libs")
+        bundle.destinationDir = project.file("$project.buildDir/${FD_OUTPUTS}/aar")
         bundle.extension = BuilderConstants.EXT_LIB_ARCHIVE
-        bundle.from(project.file("$project.buildDir/$DIR_BUNDLES/${dirName}"))
+        bundle.from(project.file("$project.buildDir/${FD_INTERMEDIATES}/$DIR_BUNDLES/${dirName}"))
 
         libVariantData.packageLibTask = bundle
         variantData.outputFile = bundle.archivePath
@@ -312,7 +315,7 @@ public class LibraryVariantFactory implements VariantFactory {
         // configure the variant to be testable.
         variantConfig.output = new LibraryBundle(
                 bundle.archivePath,
-                project.file("$project.buildDir/$DIR_BUNDLES/${dirName}"),
+                project.file("$project.buildDir/${FD_INTERMEDIATES}/$DIR_BUNDLES/${dirName}"),
                 variantData.getName()) {
 
             @Override
@@ -360,9 +363,9 @@ public class LibraryVariantFactory implements VariantFactory {
         task.group = org.gradle.api.plugins.BasePlugin.BUILD_GROUP
         task.plugin = basePlugin
         task.variant = variantData
-        task.destinationDir = project.file("$project.buildDir/$DIR_BUNDLES/${dirName}")
+        task.destinationDir = project.file("$project.buildDir/${FD_INTERMEDIATES}/$DIR_BUNDLES/${dirName}")
         task.output = new File(task.destinationDir, FN_ANNOTATIONS_ZIP)
-        task.classDir = project.file("$project.buildDir/classes/${variantData.variantConfiguration.dirName}")
+        task.classDir = project.file("$project.buildDir/${FD_INTERMEDIATES}/classes/${variantData.variantConfiguration.dirName}")
         task.source = variantData.getJavaSources()
         task.encoding = extension.compileOptions.encoding
         task.sourceCompatibility = extension.compileOptions.sourceCompatibility