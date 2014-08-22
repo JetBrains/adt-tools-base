tadfisher/platform_tools_base

Add STL support in NDK gradle.
Change-Id: I05920de3c28802c538a926c89ffabb878e197987

build-system/tests/ndkStl/src/androidTest/java/com/example/hellojni/HelloJniTest.java
@@ -1,0 +1,29 @@
+package com.example.hellojni;
+
+import android.test.ActivityInstrumentationTestCase;
+
+/**
+ * This is a simple framework for a test of an Application.  See
+ * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
+ * how to write and extend Application tests.
+ * <p/>
+ * To run this test, you can type:
+ * adb shell am instrument -w \
+ * -e class com.example.hellojni.HelloJniTest \
+ * com.example.hellojni.tests/android.test.InstrumentationTestRunner
+ */
+public class HelloJniTest extends ActivityInstrumentationTestCase<HelloJni> {
+
+    public HelloJniTest() {
+        super("com.example.hellojni", HelloJni.class);
+    }
+
+
+    public void testJniName() {
+        final HelloJni a = getActivity();
+        // ensure a valid handle to the activity has been returned
+        assertNotNull(a);
+
+        assertTrue("HELLO WORLD!".equals(a.stringFromJNI()));
+    }
+}

JetBrains/adt-tools-base

build-system/tests/ndkStl/src/androidTest/java/com/example/hellojni/HelloJniTest.java
@@ -1,0 +1,29 @@
+package com.example.hellojni;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * This is a simple framework for a test of an Application.  See
+ * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
+ * how to write and extend Application tests.
+ * <p/>
+ * To run this test, you can type:
+ * adb shell am instrument -w \
+ * -e class com.example.hellojni.HelloJniTest \
+ * com.example.hellojni.tests/android.test.InstrumentationTestRunner
+ */
+public class HelloJniTest extends ActivityInstrumentationTestCase2<HelloJni> {
+
+    public HelloJniTest() {
+        super(HelloJni.class);
+    }
+
+
+    public void testJniName() {
+        final HelloJni a = getActivity();
+        // ensure a valid handle to the activity has been returned
+        assertNotNull(a);
+
+        assertTrue("HELLO WORLD!".equals(a.stringFromJNI()));
+    }
+}