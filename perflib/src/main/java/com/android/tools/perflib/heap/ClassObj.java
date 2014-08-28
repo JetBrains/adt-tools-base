tadfisher/platform_tools_base

Improve hprof parsing performance.
Stats for parsing a 25Mb hprof:
- before: 5s, 860Mb in the heap
- after: 671ms, 92Mb in the heap

Optimizations included:
- using Trove collections instead of hash maps, getting rid of Long boxing.
- lazy, on-demand loading for values. The dominator computation is going
 through them anyway, adding the references.
- PriorityQueue was doing an O(NlogN) heap-sort (10% CPU time), but in reality
 we only need one extra phase to resolve class references at the end.
- Snapshot#findReference is now a hot method, so iterating through Heaps
 should not construct an Iterator object every time.

Other changes:
- added an InMemoryBuffer to help with testing.

Dominators will be optimized in a second CL, there are plenty of hashmaps and
auto-boxing going on there.

Change-Id: Id67bfb6f18e40951b393bbaf1016a7ad0889c788

perflib/src/main/java/com/android/tools/perflib/heap/ClassObj.java
@@ -16,6 +16,8 @@
 
 package com.android.tools.perflib.heap;
 
+import com.android.annotations.VisibleForTesting;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -25,24 +27,26 @@ import java.util.Set;
 
 public class ClassObj extends Instance implements Comparable<ClassObj> {
 
-    String mClassName;
+    final String mClassName;
 
-    ClassObj mSuperClass;
+    private final long mStaticFieldsOffset;
+
+    long mSuperClassId;
 
     Field[] mFields;
 
-    private int mInstanceSize;
+    Field[] mStaticFields;
 
-    Map<Field, Value> mStaticFields = new HashMap<Field, Value>();
+    private int mInstanceSize;
 
     ArrayList<Instance> mInstances = new ArrayList<Instance>();
 
     Set<ClassObj> mSubclasses = new HashSet<ClassObj>();
 
-    public ClassObj(long id, StackTrace stack, String className) {
-        mId = id;
-        mStack = stack;
+    public ClassObj(long id, StackTrace stack, String className, long staticFieldsOffset) {
+        super(id, stack);
         mClassName = className;
+        mStaticFieldsOffset = staticFieldsOffset;
     }
 
     public final void addSubclass(ClassObj subclass) {
@@ -67,9 +71,8 @@ public class ClassObj extends Instance implements Comparable<ClassObj> {
         mInstances.add(instance);
     }
 
-    public final void setSuperClass(ClassObj superClass) {
-        mSuperClass = superClass;
-        superClass.addSubclass(this);
+    public final void setSuperClassId(long superClass) {
+        mSuperClassId = superClass;
     }
 
     public Field[] getFields() {
@@ -80,6 +83,10 @@ public class ClassObj extends Instance implements Comparable<ClassObj> {
         mFields = fields;
     }
 
+    public void setStaticFields(Field[] staticFields) {
+        mStaticFields = staticFields;
+    }
+
     public void setInstanceSize(int size) {
         mInstanceSize = size;
     }
@@ -88,21 +95,39 @@ public class ClassObj extends Instance implements Comparable<ClassObj> {
         return mInstanceSize;
     }
 
+    public Map<Field, Object> getStaticFields() {
+        Map<Field, Object> result = new HashMap<Field, Object>();
+        getBuffer().setPosition(mStaticFieldsOffset);
+
+        int numEntries = readUnsignedShort();
+        for (int i = 0; i < numEntries; i++) {
+            Field f = mStaticFields[i];
+
+            readId();
+            readUnsignedByte();
+
+            Object value = readValue(f.getType());
+            result.put(f, value);
+        }
+        return result;
+    }
+
     public final void dump() {
         System.out.println("+----------  ClassObj dump for: " + mClassName);
 
         System.out.println("+-----  Static fields");
-        for (Field field : mStaticFields.keySet()) {
+        Map<Field, Object> staticFields = getStaticFields();
+        for (Field field : staticFields.keySet()) {
             System.out.println(field.getName() + ": " + field.getType() + " = "
-                    + mStaticFields.get(field));
+                    + staticFields.get(field));
         }
 
         System.out.println("+-----  Instance fields");
         for (Field field : mFields) {
             System.out.println(field.getName() + ": " + field.getType());
         }
-        if (mSuperClass != null) {
-            mSuperClass.dump();
+        if (getSuperClassObj() != null) {
+            getSuperClassObj().dump();
         }
     }
 
@@ -113,9 +138,9 @@ public class ClassObj extends Instance implements Comparable<ClassObj> {
     @Override
     public final void accept(Visitor visitor) {
         if (visitor.visitEnter(this)) {
-            for (Value value : mStaticFields.values()) {
-                if (value.getValue() instanceof Instance) {
-                    ((Instance) value.getValue()).accept(visitor);
+            for (Object value : getStaticFields().values()) {
+                if (value instanceof Instance) {
+                    ((Instance) value).accept(visitor);
                 }
             }
             visitor.visitLeave(this);
@@ -135,17 +160,13 @@ public class ClassObj extends Instance implements Comparable<ClassObj> {
         return 0 == compareTo((ClassObj) o);
     }
 
-    public void addStaticField(Type type, String name, Value value) {
-        // TODO: Do we need to add a root for this objects?
-        mStaticFields.put(new Field(type, name), value);
-    }
-
-    public Value getStaticField(Type type, String name) {
-        return mStaticFields.get(new Field(type, name));
+    @VisibleForTesting
+    Object getStaticField(Type type, String name) {
+        return getStaticFields().get(new Field(type, name));
     }
 
     public ClassObj getSuperClassObj() {
-        return mSuperClass;
+        return mHeap.mSnapshot.findClass(mSuperClassId);
     }
 
     public Collection<Instance> getInstances() {

perflib/src/test/java/com/android/tools/perflib/heap/analysis/VisitorsTest.java
@@ -19,14 +19,19 @@ package com.android.tools.perflib.heap.analysis;
 import com.android.tools.perflib.heap.ArrayInstance;
 import com.android.tools.perflib.heap.ClassInstance;
 import com.android.tools.perflib.heap.ClassObj;
+import com.android.tools.perflib.heap.Field;
 import com.android.tools.perflib.heap.Heap;
 import com.android.tools.perflib.heap.RootObj;
 import com.android.tools.perflib.heap.RootType;
 import com.android.tools.perflib.heap.Snapshot;
 import com.android.tools.perflib.heap.Type;
 import com.android.tools.perflib.heap.Value;
+import com.android.tools.perflib.heap.io.InMemoryBuffer;
+import com.google.common.collect.Maps;
 
 import junit.framework.TestCase;
+
+import java.util.Map;
 
 /**
  * There are two testing scenarios we want to cover here: basic connectivity between different
@@ -35,34 +40,38 @@ import junit.framework.TestCase;
  */
 public class VisitorsTest extends TestCase {
 
-    private final ClassObj mDummyClass = new ClassObj(0, null, "dummy");
+    private final ClassObj mDummyClass = new ClassObj(42, null, "dummy", 0);
 
     private Snapshot mSnapshot;
 
     @Override
     public void setUp() throws Exception {
-        mSnapshot = new Snapshot();
+        mSnapshot = new Snapshot(new InMemoryBuffer(10));
         mSnapshot.setHeapTo(13, "testHeap");
         mDummyClass.setInstanceSize(20);
-        mSnapshot.addClass(0, mDummyClass);
+        mDummyClass.setFields(new Field[0]);
+        mSnapshot.addClass(42, mDummyClass);
     }
 
     public void testSimpleStaticFieldsGraph() {
-        ClassInstance object1 = new ClassInstance(1, null);
-        object1.setClass(mDummyClass);
+        Type.setIdSize(4);
+        final ClassInstance object1 = new ClassInstance(1, null, 0);
+        object1.setClassId(42);
         mSnapshot.addInstance(1, object1);
 
-        ClassInstance object2 = new ClassInstance(2, null);
-        object2.setClass(mDummyClass);
+        final ClassInstance object2 = new ClassInstance(2, null, 0);
+        object2.setClassId(42);
         mSnapshot.addInstance(2, object2);
 
-        ClassObj clazz = new ClassObj(13, null, "FooBar");
-        Value value1 = new Value(clazz);
-        value1.setValue(object1);
-        clazz.addStaticField(Type.OBJECT, "foo", value1);
-        Value value2 = new Value(clazz);
-        value2.setValue(object2);
-        clazz.addStaticField(Type.OBJECT, "bar", value2);
+        ClassObj clazz = new ClassObj(13, null, "FooBar", 0) {
+            @Override
+            public Map<Field, Object> getStaticFields() {
+                Map<Field, Object> result = Maps.newHashMap();
+                result.put(new Field(Type.OBJECT, "foo"), object1);
+                result.put(new Field(Type.OBJECT, "bar"), object2);
+                return result;
+            }
+        };
         clazz.setSize(10);
         mSnapshot.addClass(13, clazz);
 
@@ -75,14 +84,17 @@ public class VisitorsTest extends TestCase {
     }
 
     public void testSimpleArray() {
-        ClassInstance object = new ClassInstance(1, null);
-        object.setClass(mDummyClass);
+        Type.setIdSize(4);
+        final ClassInstance object = new ClassInstance(1, null, 0);
+        object.setClassId(42);
         mSnapshot.addInstance(1, object);
 
-        ArrayInstance array = new ArrayInstance(2, null, Type.OBJECT);
-        Value value = new Value(array);
-        value.setValue(object);
-        array.setValues(new Value[] {value, value, value});
+        ArrayInstance array = new ArrayInstance(2, null, Type.OBJECT, 3, 0) {
+            @Override
+            public Object[] getValues() {
+                return new Object[] {object, object, object};
+            }
+        };
         mSnapshot.addInstance(2, array);
 
         mSnapshot.setToDefaultHeap();
@@ -95,10 +107,9 @@ public class VisitorsTest extends TestCase {
 
     public void testBasicDiamond() {
         Snapshot snapshot = new SnapshotBuilder(4)
-                .addReference(1, 2)
-                .addReference(1, 3)
-                .addReference(2, 4)
-                .addReference(3, 4)
+                .addReferences(1, 2, 3)
+                .addReferences(2, 4)
+                .addReferences(3, 4)
                 .addRoot(1)
                 .getSnapshot();
 
@@ -110,9 +121,9 @@ public class VisitorsTest extends TestCase {
 
     public void testBasicCycle() {
         Snapshot snapshot = new SnapshotBuilder(3)
-                .addReference(1, 2)
-                .addReference(2, 3)
-                .addReference(3, 1)
+                .addReferences(1, 2)
+                .addReferences(2, 3)
+                .addReferences(3, 1)
                 .addRoot(1)
                 .getSnapshot();
 

JetBrains/adt-tools-base

perflib/src/main/java/com/android/tools/perflib/heap/ClassObj.java
@@ -16,6 +16,8 @@
 
 package com.android.tools.perflib.heap;
 
+import com.android.annotations.VisibleForTesting;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -25,24 +27,26 @@ import java.util.Set;
 
 public class ClassObj extends Instance implements Comparable<ClassObj> {
 
-    String mClassName;
+    final String mClassName;
 
-    ClassObj mSuperClass;
+    private final long mStaticFieldsOffset;
+
+    long mSuperClassId;
 
     Field[] mFields;
 
-    private int mInstanceSize;
+    Field[] mStaticFields;
 
-    Map<Field, Value> mStaticFields = new HashMap<Field, Value>();
+    private int mInstanceSize;
 
     ArrayList<Instance> mInstances = new ArrayList<Instance>();
 
     Set<ClassObj> mSubclasses = new HashSet<ClassObj>();
 
-    public ClassObj(long id, StackTrace stack, String className) {
-        mId = id;
-        mStack = stack;
+    public ClassObj(long id, StackTrace stack, String className, long staticFieldsOffset) {
+        super(id, stack);
         mClassName = className;
+        mStaticFieldsOffset = staticFieldsOffset;
     }
 
     public final void addSubclass(ClassObj subclass) {
@@ -67,9 +71,8 @@ public class ClassObj extends Instance implements Comparable<ClassObj> {
         mInstances.add(instance);
     }
 
-    public final void setSuperClass(ClassObj superClass) {
-        mSuperClass = superClass;
-        superClass.addSubclass(this);
+    public final void setSuperClassId(long superClass) {
+        mSuperClassId = superClass;
     }
 
     public Field[] getFields() {
@@ -80,6 +83,10 @@ public class ClassObj extends Instance implements Comparable<ClassObj> {
         mFields = fields;
     }
 
+    public void setStaticFields(Field[] staticFields) {
+        mStaticFields = staticFields;
+    }
+
     public void setInstanceSize(int size) {
         mInstanceSize = size;
     }
@@ -88,21 +95,39 @@ public class ClassObj extends Instance implements Comparable<ClassObj> {
         return mInstanceSize;
     }
 
+    public Map<Field, Object> getStaticFieldValues() {
+        Map<Field, Object> result = new HashMap<Field, Object>();
+        getBuffer().setPosition(mStaticFieldsOffset);
+
+        int numEntries = readUnsignedShort();
+        for (int i = 0; i < numEntries; i++) {
+            Field f = mStaticFields[i];
+
+            readId();
+            readUnsignedByte();
+
+            Object value = readValue(f.getType());
+            result.put(f, value);
+        }
+        return result;
+    }
+
     public final void dump() {
         System.out.println("+----------  ClassObj dump for: " + mClassName);
 
         System.out.println("+-----  Static fields");
-        for (Field field : mStaticFields.keySet()) {
+        Map<Field, Object> staticFields = getStaticFieldValues();
+        for (Field field : staticFields.keySet()) {
             System.out.println(field.getName() + ": " + field.getType() + " = "
-                    + mStaticFields.get(field));
+                    + staticFields.get(field));
         }
 
         System.out.println("+-----  Instance fields");
         for (Field field : mFields) {
             System.out.println(field.getName() + ": " + field.getType());
         }
-        if (mSuperClass != null) {
-            mSuperClass.dump();
+        if (getSuperClassObj() != null) {
+            getSuperClassObj().dump();
         }
     }
 
@@ -113,9 +138,9 @@ public class ClassObj extends Instance implements Comparable<ClassObj> {
     @Override
     public final void accept(Visitor visitor) {
         if (visitor.visitEnter(this)) {
-            for (Value value : mStaticFields.values()) {
-                if (value.getValue() instanceof Instance) {
-                    ((Instance) value.getValue()).accept(visitor);
+            for (Object value : getStaticFieldValues().values()) {
+                if (value instanceof Instance) {
+                    ((Instance) value).accept(visitor);
                 }
             }
             visitor.visitLeave(this);
@@ -135,17 +160,13 @@ public class ClassObj extends Instance implements Comparable<ClassObj> {
         return 0 == compareTo((ClassObj) o);
     }
 
-    public void addStaticField(Type type, String name, Value value) {
-        // TODO: Do we need to add a root for this objects?
-        mStaticFields.put(new Field(type, name), value);
-    }
-
-    public Value getStaticField(Type type, String name) {
-        return mStaticFields.get(new Field(type, name));
+    @VisibleForTesting
+    Object getStaticField(Type type, String name) {
+        return getStaticFieldValues().get(new Field(type, name));
     }
 
     public ClassObj getSuperClassObj() {
-        return mSuperClass;
+        return mHeap.mSnapshot.findClass(mSuperClassId);
     }
 
     public Collection<Instance> getInstances() {

perflib/src/test/java/com/android/tools/perflib/heap/analysis/VisitorsTest.java
@@ -19,14 +19,19 @@ package com.android.tools.perflib.heap.analysis;
 import com.android.tools.perflib.heap.ArrayInstance;
 import com.android.tools.perflib.heap.ClassInstance;
 import com.android.tools.perflib.heap.ClassObj;
+import com.android.tools.perflib.heap.Field;
 import com.android.tools.perflib.heap.Heap;
 import com.android.tools.perflib.heap.RootObj;
 import com.android.tools.perflib.heap.RootType;
 import com.android.tools.perflib.heap.Snapshot;
 import com.android.tools.perflib.heap.Type;
 import com.android.tools.perflib.heap.Value;
+import com.android.tools.perflib.heap.io.InMemoryBuffer;
+import com.google.common.collect.Maps;
 
 import junit.framework.TestCase;
+
+import java.util.Map;
 
 /**
  * There are two testing scenarios we want to cover here: basic connectivity between different
@@ -35,34 +40,38 @@ import junit.framework.TestCase;
  */
 public class VisitorsTest extends TestCase {
 
-    private final ClassObj mDummyClass = new ClassObj(0, null, "dummy");
+    private final ClassObj mDummyClass = new ClassObj(42, null, "dummy", 0);
 
     private Snapshot mSnapshot;
 
     @Override
     public void setUp() throws Exception {
-        mSnapshot = new Snapshot();
+        mSnapshot = new Snapshot(new InMemoryBuffer(10));
         mSnapshot.setHeapTo(13, "testHeap");
         mDummyClass.setInstanceSize(20);
-        mSnapshot.addClass(0, mDummyClass);
+        mDummyClass.setFields(new Field[0]);
+        mSnapshot.addClass(42, mDummyClass);
     }
 
     public void testSimpleStaticFieldsGraph() {
-        ClassInstance object1 = new ClassInstance(1, null);
-        object1.setClass(mDummyClass);
+        Type.setIdSize(4);
+        final ClassInstance object1 = new ClassInstance(1, null, 0);
+        object1.setClassId(42);
         mSnapshot.addInstance(1, object1);
 
-        ClassInstance object2 = new ClassInstance(2, null);
-        object2.setClass(mDummyClass);
+        final ClassInstance object2 = new ClassInstance(2, null, 0);
+        object2.setClassId(42);
         mSnapshot.addInstance(2, object2);
 
-        ClassObj clazz = new ClassObj(13, null, "FooBar");
-        Value value1 = new Value(clazz);
-        value1.setValue(object1);
-        clazz.addStaticField(Type.OBJECT, "foo", value1);
-        Value value2 = new Value(clazz);
-        value2.setValue(object2);
-        clazz.addStaticField(Type.OBJECT, "bar", value2);
+        ClassObj clazz = new ClassObj(13, null, "FooBar", 0) {
+            @Override
+            public Map<Field, Object> getStaticFieldValues() {
+                Map<Field, Object> result = Maps.newHashMap();
+                result.put(new Field(Type.OBJECT, "foo"), object1);
+                result.put(new Field(Type.OBJECT, "bar"), object2);
+                return result;
+            }
+        };
         clazz.setSize(10);
         mSnapshot.addClass(13, clazz);
 
@@ -75,14 +84,17 @@ public class VisitorsTest extends TestCase {
     }
 
     public void testSimpleArray() {
-        ClassInstance object = new ClassInstance(1, null);
-        object.setClass(mDummyClass);
+        Type.setIdSize(4);
+        final ClassInstance object = new ClassInstance(1, null, 0);
+        object.setClassId(42);
         mSnapshot.addInstance(1, object);
 
-        ArrayInstance array = new ArrayInstance(2, null, Type.OBJECT);
-        Value value = new Value(array);
-        value.setValue(object);
-        array.setValues(new Value[] {value, value, value});
+        ArrayInstance array = new ArrayInstance(2, null, Type.OBJECT, 3, 0) {
+            @Override
+            public Object[] getValues() {
+                return new Object[] {object, object, object};
+            }
+        };
         mSnapshot.addInstance(2, array);
 
         mSnapshot.setToDefaultHeap();
@@ -95,10 +107,9 @@ public class VisitorsTest extends TestCase {
 
     public void testBasicDiamond() {
         Snapshot snapshot = new SnapshotBuilder(4)
-                .addReference(1, 2)
-                .addReference(1, 3)
-                .addReference(2, 4)
-                .addReference(3, 4)
+                .addReferences(1, 2, 3)
+                .addReferences(2, 4)
+                .addReferences(3, 4)
                 .addRoot(1)
                 .getSnapshot();
 
@@ -110,9 +121,9 @@ public class VisitorsTest extends TestCase {
 
     public void testBasicCycle() {
         Snapshot snapshot = new SnapshotBuilder(3)
-                .addReference(1, 2)
-                .addReference(2, 3)
-                .addReference(3, 1)
+                .addReferences(1, 2)
+                .addReferences(2, 3)
+                .addReferences(3, 1)
                 .addRoot(1)
                 .getSnapshot();
 