28002ff5c765993a5d278ff2b6ec474ce0cc5cd1
Christopher Ferris <cferris@google.com>
2014-08-29 16:13:27 -0700

    Add support for 64 bit native dumps.
    Bug: 17319589
    Change-Id: I43643133d5559d0f5176eae2dea4666d44a6b2b8

--- ddmlib/src/main/java/com/android/ddmlib/HandleNativeHeap.java
+++ ddmlib/src/main/java/com/android/ddmlib/HandleNativeHeap.java
@@ -35,6 +35,56 @@ final class HandleNativeHeap extends ChunkHandler {
 
     private static final HandleNativeHeap mInst = new HandleNativeHeap();
 
+    /**
+     * Handle getting different sized size_t and pointer reads.
+     */
+    abstract class NativeBuffer {
+        public NativeBuffer(ByteBuffer buffer) {
+            mBuffer = buffer;
+        }
+
+        public abstract int getSizeT();
+        public abstract long getPtr();
+
+        protected ByteBuffer mBuffer;
+    }
+
+    /**
+     * This class treats size_t and pointer values as 32 bit.
+     */
+    final class NativeBuffer32 extends NativeBuffer {
+        public NativeBuffer32(ByteBuffer buffer) {
+          super(buffer);
+        }
+
+        @Override
+        public int getSizeT() {
+            return mBuffer.getInt();
+        }
+        @Override
+        public long getPtr() {
+            return (long)mBuffer.getInt() & 0x00000000ffffffffL;
+        }
+    }
+
+    /**
+     * This class treats size_t and pointer values as 64 bit.
+     */
+    final class NativeBuffer64 extends NativeBuffer {
+        public NativeBuffer64(ByteBuffer buffer) {
+          super(buffer);
+        }
+
+        @Override
+        public int getSizeT() {
+            return (int)mBuffer.getLong();
+        }
+        @Override
+        public long getPtr() {
+            return mBuffer.getLong();
+        }
+    }
+
     private HandleNativeHeap() {
     }
 
@@ -117,62 +167,94 @@ final class HandleNativeHeap extends ChunkHandler {
      * Handle our native heap data.
      */
     private void handleNHGT(Client client, ByteBuffer data) {
-        ClientData cd = client.getClientData();
+        ClientData clientData = client.getClientData();
 
         Log.d("ddm-nativeheap", "NHGT: " + data.limit() + " bytes");
 
-        // TODO - process incoming data and save in "cd"
-        byte[] copy = new byte[data.limit()];
-        data.get(copy);
+        data.order(ByteOrder.LITTLE_ENDIAN);
+
+        // There are two supported header formats.
+        //
+        // The original version of the header for 32 bit processes:
+        //
+        //   uint32_t mapSize;
+        //   uint32_t mapSize;
+        //   uint32_t allocSize;
+        //   uint32_t allocInfoSize;
+        //   uint32_t totalMemory;
+        //   uint32_t backtrace_size;
+        //
+        // The new header which includes a signature and pointer size:
+        //
+        //   uint32_t signature;   (Which is always 0x812345dd)
+        //   uint16_t version;     (Only version 2 of the new format supported)
+        //   uint16_t pointerSize; (Size in bytes of size_t/pointer values)
+        //   size_t mapSize;
+        //   size_t allocSize;
+        //   size_t allocInfoSize;
+        //   size_t totalMemory;
+        //   size_t backtrace_size;
+        //
+        // If the signature doesn't match, then the code uses the original
+        // header format. If the signature matches, then use the new
+        // header format with variable sizes of size_t and pointers.
+        int signature = data.getInt(0);
+        short pointerSize = 4;
+        if (signature == 0x812345dd) {
+            // Consume signature value.
+            int ignore = data.getInt();
+            short version = data.getShort();
+            if (version != 2) {
+                Log.e("ddms", "Unknown header version: " + version);
+                return;
+            }
+            pointerSize = data.getShort();
+        }
+        NativeBuffer buffer;
+        if (pointerSize == 4) {
+            buffer = new NativeBuffer32(data);
+        } else if (pointerSize == 8) {
+            buffer = new NativeBuffer64(data);
+        } else {
+            Log.e("ddms", "Unknown pointer size: " + pointerSize);
+            return;
+        }
 
         // clear the previous run
-        cd.clearNativeAllocationInfo();
+        clientData.clearNativeAllocationInfo();
 
-        ByteBuffer buffer = ByteBuffer.wrap(copy);
-        buffer.order(ByteOrder.LITTLE_ENDIAN);
-
-//        read the header
-//        typedef struct Header {
-//            uint32_t mapSize;
-//            uint32_t allocSize;
-//            uint32_t allocInfoSize;
-//            uint32_t totalMemory;
-//              uint32_t backtraceSize;
-//        };
-
-        int mapSize = buffer.getInt();
-        int allocSize = buffer.getInt();
-        int allocInfoSize = buffer.getInt();
-        int totalMemory = buffer.getInt();
-        int backtraceSize = buffer.getInt();
+        int mapSize = buffer.getSizeT();
+        int allocSize = buffer.getSizeT();
+        int allocInfoSize = buffer.getSizeT();
+        int totalMemory = buffer.getSizeT();
+        int backtraceSize = buffer.getSizeT();
 
         Log.d("ddms", "mapSize: " + mapSize);
         Log.d("ddms", "allocSize: " + allocSize);
         Log.d("ddms", "allocInfoSize: " + allocInfoSize);
         Log.d("ddms", "totalMemory: " + totalMemory);
 
-        cd.setTotalNativeMemory(totalMemory);
+        clientData.setTotalNativeMemory(totalMemory);
 
         // this means that updates aren't turned on.
-        if (allocInfoSize == 0)
+        if (allocInfoSize == 0) {
           return;
+        }
 
         if (mapSize > 0) {
             byte[] maps = new byte[mapSize];
-            buffer.get(maps, 0, mapSize);
-            parseMaps(cd, maps);
+            data.get(maps, 0, mapSize);
+            parseMaps(clientData, maps);
         }
 
         int iterations = allocSize / allocInfoSize;
-
         for (int i = 0 ; i < iterations ; i++) {
             NativeAllocationInfo info = new NativeAllocationInfo(
-                    buffer.getInt() /* size */,
-                    buffer.getInt() /* allocations */);
+                    buffer.getSizeT() /* size */,
+                    buffer.getSizeT() /* allocations */);
 
             for (int j = 0 ; j < backtraceSize ; j++) {
-                long addr = (buffer.getInt()) & 0x00000000ffffffffL;
-
+                long addr = buffer.getPtr();
                 if (addr == 0x0) {
                     // skip past null addresses
                     continue;
@@ -180,8 +262,7 @@ final class HandleNativeHeap extends ChunkHandler {
 
                 info.addStackCallAddress(addr);
             }
-
-            cd.addNativeAllocation(info);
+            clientData.addNativeAllocation(info);
         }
     }
 
@@ -196,11 +277,6 @@ final class HandleNativeHeap extends ChunkHandler {
             return;
         }
 
-        // WORK IN PROGRESS
-
-//        Log.e("ddm-nativeheap", "NHSG: ----------------------------------");
-//        Log.e("ddm-nativeheap", "NHSG: " + data.limit() + " bytes");
-
         byte[] copy = new byte[data.limit()];
         data.get(copy);
 
@@ -213,85 +289,57 @@ final class HandleNativeHeap extends ChunkHandler {
         int offset = buffer.getInt();
         int allocationUnitCount = buffer.getInt();
 
-//        Log.e("ddm-nativeheap", "id: " + id);
-//        Log.e("ddm-nativeheap", "unitsize: " + unitsize);
-//        Log.e("ddm-nativeheap", "startAddress: 0x" + Long.toHexString(startAddress));
-//        Log.e("ddm-nativeheap", "offset: " + offset);
-//        Log.e("ddm-nativeheap", "allocationUnitCount: " + allocationUnitCount);
-//        Log.e("ddm-nativeheap", "end: 0x" +
-//                Long.toHexString(startAddress + unitsize * allocationUnitCount));
-
         // read the usage
         while (buffer.position() < buffer.limit()) {
             int eState = buffer.get() & 0x000000ff;
             int eLen = (buffer.get() & 0x000000ff) + 1;
-            //Log.e("ddm-nativeheap", "solidity: " + (eState & 0x7) + " - kind: "
-            //        + ((eState >> 3) & 0x7) + " - len: " + eLen);
         }
-
-
-//        count += unitsize * allocationUnitCount;
-//        Log.e("ddm-nativeheap", "count = " + count);
-
     }
 
-    private void parseMaps(ClientData cd, byte[] maps) {
+    private void parseMaps(ClientData clientData, byte[] maps) {
         InputStreamReader input = new InputStreamReader(new ByteArrayInputStream(maps));
         BufferedReader reader = new BufferedReader(input);
 
         String line;
 
         try {
-
-            // most libraries are defined on several lines, so we need to make sure we parse
-            // all the library lines and only add the library at the end
-            long startAddr = 0;
-            long endAddr = 0;
-            String library = null;
-
             while ((line = reader.readLine()) != null) {
                 Log.d("ddms", "line: " + line);
-                if (line.length() < 16) {
+                // Expected format:
+                //   7fe51f2000-7fe5213000 rw-p 00000000 00:00 0      [stack]
+
+                int library_start = line.lastIndexOf(' ');
+                if (library_start == -1) {
                     continue;
                 }
 
+                // Assume that any string that starts with a / is a
+                // shared library or executable that we will try to symbolize.
+                String library = line.substring(library_start+1);
+                if (!library.startsWith("/")) {
+                    continue;
+                }
+
+                // Parse the start and end address range.
+                int dashIndex = line.indexOf('-');
+                int spaceIndex = line.indexOf(' ', dashIndex);
+                if (dashIndex == -1 || spaceIndex == -1) {
+                    continue;
+                }
+
+                long startAddr = 0;
+                long endAddr = 0;
                 try {
-                    long tmpStart = Long.parseLong(line.substring(0, 8), 16);
-                    long tmpEnd = Long.parseLong(line.substring(9, 17), 16);
-
-                    int index = line.indexOf('/');
-
-                    if (index == -1)
-                        continue;
-
-                    String tmpLib = line.substring(index);
-
-                    if (library == null ||
-                            (library != null && !tmpLib.equals(library))) {
-
-                        if (library != null) {
-                            cd.addNativeLibraryMapInfo(startAddr, endAddr, library);
-                            Log.d("ddms", library + "(" + Long.toHexString(startAddr) +
-                                    " - " + Long.toHexString(endAddr) + ")");
-                        }
-
-                        // now init the new library
-                        library = tmpLib;
-                        startAddr = tmpStart;
-                        endAddr = tmpEnd;
-                    } else {
-                        // add the new end
-                        endAddr = tmpEnd;
-                    }
+                    startAddr = Long.parseLong(line.substring(0, dashIndex), 16);
+                    endAddr = Long.parseLong(line.substring(dashIndex+1, spaceIndex), 16);
                 } catch (NumberFormatException e) {
                     e.printStackTrace();
+                    continue;
                 }
-            }
 
-            if (library != null) {
-                cd.addNativeLibraryMapInfo(startAddr, endAddr, library);
+                clientData.addNativeLibraryMapInfo(startAddr, endAddr, library);
                 Log.d("ddms", library + "(" + Long.toHexString(startAddr) +
-                        " - " + Long.toHexString(endAddr) + ")");
+                      " - " + Long.toHexString(endAddr) + ")");
             }
         } catch (IOException e) {
             e.printStackTrace();
