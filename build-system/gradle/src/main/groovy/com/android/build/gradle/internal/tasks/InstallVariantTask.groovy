95a2f0c15bdd0252c53b15528e14a3adea1eea81
Chris Warrington <cmw@google.com>
2014-10-10 18:35:12 -0700

    Gradle install task fails if it finds no compatible device.
    The number of successful installs is displayed, along with the reasons for unsuccessful installs.
    
    Change-Id: I8d1136f142013df3a1e479e7048b6e1306a6662e

--- build-system/gradle/src/main/groovy/com/android/build/gradle/internal/tasks/InstallVariantTask.groovy
+++ build-system/gradle/src/main/groovy/com/android/build/gradle/internal/tasks/InstallVariantTask.groovy
@@ -25,8 +25,11 @@ import com.android.builder.testing.api.DeviceConnector
 import com.android.builder.testing.api.DeviceProvider
 import com.android.ddmlib.IDevice
 import com.android.ide.common.build.SplitOutputMatcher
+import com.google.common.base.Joiner
+import org.gradle.api.GradleException
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.TaskExecutionException
 
 /**
  * Task installing an app variant. It looks at connected device and install the best matching
@@ -39,11 +42,17 @@ public class InstallVariantTask extends BaseTask {
     int timeOut = 0
 
     BaseVariantData<? extends BaseVariantOutputData> variantData
+    InstallVariantTask() {
+        this.getOutputs().upToDateWhen {
+            logger.debug("Install task is always run.");
+            false;
+        }
+    }
 
     @TaskAction
     void install() {
         DeviceProvider deviceProvider = new ConnectedDeviceProvider(getAdbExe())
-        deviceProvider.init();
+        deviceProvider.init()
 
         VariantConfiguration variantConfig = variantData.variantConfiguration
         String variantName = variantConfig.fullName
@@ -51,7 +60,8 @@ public class InstallVariantTask extends BaseTask {
 
         String serial = System.getenv("ANDROID_SERIAL");
 
-        int foundDevice = 0;
+        int successfulInstallCount = 0;
+
         for (DeviceConnector device : deviceProvider.getDevices()) {
             if (serial != null && !serial.equals(device.getSerialNumber())) {
                 continue;
@@ -69,21 +79,33 @@ public class InstallVariantTask extends BaseTask {
                             device.getDensity(), device.getAbis())
 
                     if (output == null) {
-                        System.out.println(
-                                "Skipping device '${device.getName()}' for '${projectName}:${variantName}': No matching output file.");
+                        project.logger.lifecycle(
+                                "Skipping device '${device.getName()}' for '${projectName}:${variantName}': " +
+                                "Could not find build of variant which supports density ${device.getDensity()} " +
+                                "and an ABI in " + Joiner.on(", ").join(device.getAbis()));
                     } else {
-                        System.out.println(
+                        project.logger.lifecycle(
                                 "Installing '${output.baseName}' on '${device.getName()}'.");
                         File apkFile = output.getOutputFile();
                         device.installPackage(apkFile, getTimeOut(), plugin.logger)
-                        foundDevice++
+                        successfulInstallCount++
                     }
-                }
+                } // When InstallUtils.checkDeviceApiLevel returns false, it logs the reason.
+            } else {
+                project.logger.lifecycle(
+                        "Skipping device '${device.getName()}' for '${projectName}:${variantName}': Device not authorized, see http://developer.android.com/tools/help/adb.html#Enabling.");
+
             }
         }
 
-        if (foundDevice == 0) {
-            System.out.println("Found no authorized devices")
+        if (successfulInstallCount == 0) {
+            if (serial != null) {
+                throw new GradleException("Failed to find device with serial '${serial}'. Unset ANDROID_SERIAL to search for any device.")
+            } else {
+                throw new GradleException("Failed to install on any devices.")
+            }
+        } else {
+            project.logger.quiet("Installed on ${successfulInstallCount} ${successfulInstallCount==1?'device':'devices'}.");
         }
     }
 }
