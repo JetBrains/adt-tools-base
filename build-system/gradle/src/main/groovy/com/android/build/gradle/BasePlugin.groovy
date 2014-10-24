981c5b9daba2e4382a8c563d30d215bf070c8b2b
Xavier Ducrohet <xav@google.com>
2014-10-23 19:53:06 -0700

    Fix dex dependencies.
    Change-Id: I841951a1ca573fa861a287d66ac59aad50d2e38d

--- build-system/gradle/src/main/groovy/com/android/build/gradle/BasePlugin.groovy
+++ build-system/gradle/src/main/groovy/com/android/build/gradle/BasePlugin.groovy
@@ -1989,7 +1989,7 @@ public abstract class BasePlugin {
                 preDexTask = project.tasks.create(preDexTaskName, PreDex)
 
                 variantData.preDexTask = preDexTask
-                preDexTask.dependsOn variantData.prepareDependenciesTask
+                preDexTask.dependsOn variantData.variantDependency.packageConfiguration.buildDependencies
                 preDexTask.plugin = this
                 preDexTask.dexOptions = extension.dexOptions
 
@@ -2021,7 +2021,7 @@ public abstract class BasePlugin {
             if (runPreDex) {
                 dexTask.dependsOn preDexTask
             } else {
-                dexTask.dependsOn variantData.prepareDependenciesTask
+                dexTask.dependsOn variantData.variantDependency.packageConfiguration.buildDependencies
             }
 
             dexTask.conventionMapping.inputFiles = {
@@ -2088,7 +2088,7 @@ public abstract class BasePlugin {
                 "jill${config.fullName.capitalize()}PackagedLibraries",
                 JillTask)
 
-        jillPackagedTask.dependsOn variantData.prepareDependenciesTask
+        jillPackagedTask.dependsOn variantData.variantDependency.packageConfiguration.buildDependencies
         jillPackagedTask.plugin = this
         jillPackagedTask.dexOptions = extension.dexOptions
 
