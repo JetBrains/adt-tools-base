tadfisher/platform_tools_base

Introduce new UNIT_TEST variant type.
This commit only declares the new type and makes sure code is ready to handle
both kinds of test. It also renames some output files and tasks, in preparation
for unit testing tasks.

Change-Id: I5656f86b9a9acf977e70b5f11e41df45f7b50552

build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/application/AndroidTestResourcesTest.groovy
@@ -71,7 +71,7 @@ class AndroidTestResourcesTest {
                         super.setUp();
                         final HelloWorld a = getActivity();
                         mTextView = (TextView) a.findViewById(
-                                com.example.helloworld.test.R.id.test_layout_1_textview);
+                                com.example.helloworld.androidTest.R.id.test_layout_1_textview);
                     }
 
                     @MediumTest
@@ -93,7 +93,7 @@ class AndroidTestResourcesTest {
      * Use the test app to create an application and a library project.
      */
     @BeforeClass
-    static void setup() {
+    static void setUp() {
         testApp.writeSources(appProject.testDir)
         appProject.getBuildFile() << """
                 apply plugin: 'com.android.application'
@@ -102,7 +102,7 @@ class AndroidTestResourcesTest {
                     buildToolsVersion "$GradleTestProject.DEFAULT_BUILD_TOOL_VERSION"
                 }
                 """.stripIndent()
-        appProject.execute("assembleDebugTest")
+        appProject.execute("assembleDebugAndroidTest")
 
         testApp.writeSources(libProject.testDir)
         libProject.getBuildFile() << """
@@ -112,7 +112,7 @@ class AndroidTestResourcesTest {
                     buildToolsVersion "$GradleTestProject.DEFAULT_BUILD_TOOL_VERSION"
                 }
                 """.stripIndent()
-        libProject.execute("assembleDebugTest")
+        libProject.execute("assembleDebugAndroidTest")
     }
 
     @AfterClass
@@ -142,7 +142,7 @@ class AndroidTestResourcesTest {
     private void checkLayoutInR(GradleTestProject fixture) {
         def rFile = fixture.file(Joiner.on(File.separatorChar).join(
                 "build", AndroidProject.FD_GENERATED, "source", "r",
-                "test", "debug", "com", "example", "helloworld", "test",  "R.java"))
+                "androidTest", "debug", "com", "example", "helloworld", "androidTest",  "R.java"))
         assertTrue("Should have generated R file", rFile.exists())
         def rFileContents = rFile.getText("UTF-8")
 

JetBrains/adt-tools-base

build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/application/AndroidTestResourcesTest.groovy
@@ -96,7 +96,7 @@ class AndroidTestResourcesTest {
      * Use the test app to create an application and a library project.
      */
     @BeforeClass
-    static void setup() {
+    static void setUp() {
         testApp.writeSources(appProject.testDir)
         appProject.getBuildFile() << """
                 apply plugin: 'com.android.application'
@@ -105,7 +105,7 @@ class AndroidTestResourcesTest {
                     buildToolsVersion "$GradleTestProject.DEFAULT_BUILD_TOOL_VERSION"
                 }
                 """.stripIndent()
-        appProject.execute("assembleDebugTest")
+        appProject.execute("assembleDebugAndroidTest")
 
         testApp.writeSources(libProject.testDir)
         libProject.getBuildFile() << """
@@ -115,7 +115,7 @@ class AndroidTestResourcesTest {
                     buildToolsVersion "$GradleTestProject.DEFAULT_BUILD_TOOL_VERSION"
                 }
                 """.stripIndent()
-        libProject.execute("assembleDebugTest")
+        libProject.execute("assembleDebugAndroidTest")
     }
 
     @AfterClass
@@ -145,7 +145,7 @@ class AndroidTestResourcesTest {
     private void checkLayoutInR(GradleTestProject fixture) {
         def rFile = fixture.file(Joiner.on(File.separatorChar).join(
                 "build", AndroidProject.FD_GENERATED, "source", "r",
-                "test", "debug", "com", "example", "helloworld", "test",  "R.java"))
+                "androidTest", "debug", "com", "example", "helloworld", "test",  "R.java"))
         assertTrue("Should have generated R file", rFile.exists())
         def rFileContents = rFile.getText("UTF-8")
 