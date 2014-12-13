b94beb876b78a9adfc7b68648ec3717560dc51fa
Raymond Chiu <chiur@google.com>
2014-12-13 00:55:08 +0000

    Revert "Integrate Jacoco to provide test coverage."
    This reverts commit 7879486fc65e03456d48db9692567c98c8ecc1c1.
    
    Change-Id: Icf7c6d70ec5bd28c05db55e3b809f9b50cd8e938

--- build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/common/fixture/GradleTestProject.java
+++ build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/common/fixture/GradleTestProject.java
@@ -26,7 +26,6 @@ import com.android.build.gradle.BasePlugin;
 import com.android.build.gradle.integration.common.fixture.app.AbstractAndroidTestApp;
 import com.android.build.gradle.integration.common.fixture.app.AndroidTestApp;
 import com.android.build.gradle.integration.common.fixture.app.TestSourceFile;
-import com.android.build.gradle.integration.common.utils.JacocoAgent;
 import com.android.build.gradle.integration.common.utils.SdkHelper;
 import com.android.builder.model.AndroidProject;
 import com.android.io.StreamException;
@@ -552,8 +551,7 @@ public class GradleTestProject implements TestRule {
         args.addAll(arguments);
 
         BuildLauncher launcher = connection.newBuild().forTasks(tasks)
-                .withArguments(args.toArray(new String[args.size()]))
-                .setJvmArguments(JacocoAgent.getJvmArg());
+                .withArguments(args.toArray(new String[args.size()]));
         if (stdout != null) {
             launcher.setStandardOutput(stdout);
         }
