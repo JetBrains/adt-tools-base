tadfisher/platform_tools_base

New injected properties to control issue reporting.
The new SyncIssue mechanism to have the plugin not
throw on some errors (and instead put the errors in the
SyncIssue list) is now tied to a new properties injected
by the IDE.

This allows older IDEs (basically Studio 1.0) to still
get the old behavior of the sync failing rather than having
sync succeed but not having a display of these new errors.

Change-Id: I15233f0d8ae00a8c7934c8fad12d6744df02b3c4

build-system/gradle/src/main/groovy/com/android/build/gradle/internal/ExtraModelInfo.java
@@ -17,6 +17,7 @@
 package com.android.build.gradle.internal;
 
 import static com.android.builder.model.AndroidProject.PROPERTY_BUILD_MODEL_ONLY;
+import static com.android.builder.model.AndroidProject.PROPERTY_BUILD_MODEL_ONLY_ADVANCED;
 
 import com.android.annotations.NonNull;
 import com.android.annotations.Nullable;
@@ -36,7 +37,6 @@ import com.android.builder.model.SourceProviderContainer;
 import com.android.builder.model.SyncIssue;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.ListMultimap;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 import org.gradle.api.GradleException;
@@ -45,7 +45,6 @@ import org.gradle.api.artifacts.Configuration;
 
 import java.io.File;
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -53,7 +52,11 @@ import java.util.Map;
  */
 public class ExtraModelInfo {
 
-    private boolean buildModelForIde;
+    public static enum ModelQueryMode {
+        STANDARD, IDE, IDE_ADVANCED
+    }
+
+    private ModelQueryMode modelQueryMode;
 
     private final Map<SyncIssueKey, SyncIssue> syncIssues = Maps.newHashMap();
 
@@ -72,23 +75,31 @@ public class ExtraModelInfo {
     private final ListMultimap<String, SourceProviderContainer> extraMultiFlavorSourceProviders = ArrayListMultimap.create();
 
     public ExtraModelInfo(Project project) {
-        buildModelForIde = getBuildModelForIde(project);
+        modelQueryMode = computeModelQueryMode(project);
     }
 
     public Map<SyncIssueKey, SyncIssue> getSyncIssues() {
         return syncIssues;
     }
 
-    public boolean isBuildModelForIde() {
-        return buildModelForIde;
+    public ModelQueryMode getModelQueryMode() {
+        return modelQueryMode;
     }
 
     public void handleSyncError(String data, int type, String msg) {
-        if (!buildModelForIde) {
-            throw new GradleException(msg);
-        } else {
-            SyncIssue syncIssue = new SyncIssueImpl(type, SyncIssue.SEVERITY_ERROR, data, msg);
-            syncIssues.put(SyncIssueKey.from(syncIssue), syncIssue);
+        switch (modelQueryMode) {
+            case STANDARD:
+                throw new GradleException(msg);
+            case IDE:
+                // compat mode for the only issue supported before the addition of SyncIssue
+                // in the model.
+                if (type != SyncIssue.TYPE_UNRESOLVED_DEPENDENCY) {
+                    throw new GradleException(msg);
+                }
+            case IDE_ADVANCED:
+                // new IDE, able to support SyncIssue.
+                SyncIssue syncIssue = new SyncIssueImpl(type, SyncIssue.SEVERITY_ERROR, data, msg);
+                syncIssues.put(SyncIssueKey.from(syncIssue), syncIssue);
         }
     }
 
@@ -198,14 +209,28 @@ public class ExtraModelInfo {
      * means we will attempt to resolve dependencies even if some are broken/unsupported to avoid
      * failing the import in the IDE.
      */
-    private boolean getBuildModelForIde(Project project) {
-        boolean flagValue = false;
-        if (project.hasProperty(PROPERTY_BUILD_MODEL_ONLY)) {
-            Object value = project.getProperties().get(PROPERTY_BUILD_MODEL_ONLY);
+    private static ModelQueryMode computeModelQueryMode(@NonNull Project project) {
+        if (hasTrueProperty(project, PROPERTY_BUILD_MODEL_ONLY_ADVANCED)) {
+            return ModelQueryMode.IDE_ADVANCED;
+        }
+
+        if (hasTrueProperty(project, PROPERTY_BUILD_MODEL_ONLY)) {
+            return ModelQueryMode.IDE;
+        }
+
+        return ModelQueryMode.STANDARD;
+    }
+
+    private static boolean hasTrueProperty(
+            @NonNull Project project,
+            @NonNull String propertyName) {
+        if (project.hasProperty(propertyName)) {
+            Object value = project.getProperties().get(propertyName);
             if (value instanceof String) {
-                flagValue = Boolean.parseBoolean((String) value);
+                return Boolean.parseBoolean((String) value);
             }
         }
-        return flagValue;
+
+        return false;
     }
 }

build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/common/fixture/GradleTestProject.java
@@ -429,11 +429,11 @@ public class GradleTestProject implements TestRule {
      * @param tasks Variadic list of tasks to execute.
      */
     public void execute(String ... tasks) {
-        execute(Collections.<String>emptyList(), false, tasks);
+        execute(Collections.<String>emptyList(), false, false, tasks);
     }
 
     public void execute(@NonNull List<String> arguments, String ... tasks) {
-        execute(arguments, false, tasks);
+        execute(arguments, false, false, tasks);
     }
 
     /**
@@ -445,8 +445,21 @@ public class GradleTestProject implements TestRule {
      */
     @NonNull
     public AndroidProject executeAndReturnModel(String ... tasks) {
+        return executeAndReturnModel(false, tasks);
+    }
+
+    /**
+     * Runs gradle on the project, and returns the project model.  Throws exception on failure.
+     *
+     * @param emulateOlderIde whether to emulate an older IDE (studio 1.0) querying the model.
+     * @param tasks Variadic list of tasks to execute.
+     *
+     * @return the AndroidProject model for the project.
+     */
+    @NonNull
+    public AndroidProject executeAndReturnModel(boolean emulateOlderIde, String ... tasks) {
         //noinspection ConstantConditions
-        return execute(Collections.<String>emptyList(), true, tasks);
+        return execute(Collections.<String>emptyList(), true, emulateOlderIde, tasks);
     }
 
     /**
@@ -459,16 +472,31 @@ public class GradleTestProject implements TestRule {
      */
     @NonNull
     public Map<String, AndroidProject> executeAndReturnMultiModel(String ... tasks) {
+        return executeAndReturnMultiModel(false, tasks);
+    }
+
+    /**
+     * Runs gradle on the project, and returns a project model for each sub-project.
+     * Throws exception on failure.
+     *
+     * @param emulateOlderIde whether to emulate an older IDE (studio 1.0) querying the model.
+     * @param tasks Variadic list of tasks to execute.
+     *
+     * @return the AndroidProject model for the project.
+     */
+    @NonNull
+    public Map<String, AndroidProject> executeAndReturnMultiModel(boolean emulateOlderIde, String ... tasks) {
         ProjectConnection connection = getProjectConnection();
         try {
             executeBuild(Collections.<String>emptyList(), connection, tasks);
 
-            return buildModel(connection);
+            return buildModel(connection, emulateOlderIde);
 
         } finally {
             connection.close();
         }
     }
+
 
     /**
      * Returns the project model without building.
@@ -477,9 +505,21 @@ public class GradleTestProject implements TestRule {
      */
     @NonNull
     public AndroidProject getSingleModel() {
+        return getSingleModel(false);
+    }
+
+    /**
+     * Returns the project model without building.
+     *
+     * This will fail if the project is a multi-project setup.
+     *
+     * @param emulateOlderIde whether to emulate an older IDE (studio 1.0) querying the model.
+     */
+    @NonNull
+    public AndroidProject getSingleModel(boolean emulateOlderIde) {
         ProjectConnection connection = getProjectConnection();
         try {
-            Map<String, AndroidProject> modelMap = buildModel(connection);
+            Map<String, AndroidProject> modelMap = buildModel(connection, emulateOlderIde);
 
             // ensure there was only one project
             assertEquals("Quering GradleTestProject.getModel() with multi-project settings",
@@ -496,9 +536,19 @@ public class GradleTestProject implements TestRule {
      */
     @NonNull
     public Map<String, AndroidProject> getAllModels() {
+        return getAllModels(false);
+    }
+
+    /**
+     * Returns a project model for each sub-project without building.
+     *
+     * @param emulateOlderIde whether to emulate an older IDE (studio 1.0) querying the model.
+     */
+    @NonNull
+    public Map<String, AndroidProject> getAllModels(boolean emulateOlderIde) {
         ProjectConnection connection = getProjectConnection();
         try {
-            return buildModel(connection);
+            return buildModel(connection, emulateOlderIde);
 
         } finally {
             connection.close();
@@ -510,6 +560,7 @@ public class GradleTestProject implements TestRule {
      *
      * @param arguments List of arguments for the gradle command.
      * @param returnModel whether the model should be queried and returned.
+     * @param emulateOlderIde whether to emulate an older IDE (studio 1.0) querying the model.
      * @param tasks Variadic list of tasks to execute.
      *
      * @return the model, if <var>returnModel</var> was true, null otherwise
@@ -518,13 +569,14 @@ public class GradleTestProject implements TestRule {
     private AndroidProject execute(
             @NonNull List<String> arguments,
             boolean returnModel,
+            boolean emulateOlderIde,
             @NonNull String ... tasks) {
         ProjectConnection connection = getProjectConnection();
         try {
             executeBuild(arguments, connection, tasks);
 
             if (returnModel) {
-                Map<String, AndroidProject> modelMap = buildModel(connection);
+                Map<String, AndroidProject> modelMap = buildModel(connection, emulateOlderIde);
 
                 // ensure there was only one project
                 assertEquals("Quering GradleTestProject.getModel() with multi-project settings",
@@ -575,9 +627,14 @@ public class GradleTestProject implements TestRule {
 
     /**
      * Returns a project model for each sub-project without building.
+     *
+     * @param connection the opened ProjectConnection
+     * @param emulateOlderIde whether to emulate an older IDE (studio 1.0) querying the model.
      */
     @NonNull
-    private static Map<String, AndroidProject> buildModel(@NonNull ProjectConnection connection) {
+    private static Map<String, AndroidProject> buildModel(
+            @NonNull ProjectConnection connection,
+            boolean emulateOlderIde) {
         GetModelAction getModelAction = new GetModelAction();
         BuildActionExecuter<Map<String, AndroidProject>> executer
                 = connection.action(getModelAction);
@@ -585,6 +642,10 @@ public class GradleTestProject implements TestRule {
         executer.withArguments(
                 "-P" + AndroidProject.PROPERTY_BUILD_MODEL_ONLY + "=true",
                 "-P" + AndroidProject.PROPERTY_INVOKED_FROM_IDE + "=true");
+        if (!emulateOlderIde) {
+            executer.withArguments(
+                    "-P" + AndroidProject.PROPERTY_BUILD_MODEL_ONLY_ADVANCED + "=true");
+        }
 
         executer.setJvmArguments(Iterables.toArray(getDebugJvmArguments(), String.class));
         return executer.run();

build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/dependencies/AppWithCompileLocalAarFromOlderIdeTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 The Android Open Source Project
+ * Copyright (C) 2015 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,29 +14,29 @@
  * limitations under the License.
  */
 
-package com.android.build.gradle.integration.application
+package com.android.build.gradle.integration.dependencies
 
 import com.android.build.gradle.integration.common.fixture.GradleTestProject
-import com.android.build.gradle.integration.common.fixture.app.HelloWorldApp
-import com.android.builder.model.SyncIssue
 import groovy.transform.CompileStatic
-import org.junit.Before
-import org.junit.Rule
+import org.gradle.tooling.BuildException
+import org.junit.AfterClass
+import org.junit.BeforeClass
+import org.junit.ClassRule
 import org.junit.Test
 
-import static com.android.build.gradle.integration.common.truth.TruthHelper.assertThat
 /**
- * General Model tests
+ * test for package (apk) local aar in app
  */
 @CompileStatic
-class ModelTest {
+class AppWithCompileLocalAarFromOlderIdeTest {
 
-    @Rule
-    public GradleTestProject project = GradleTestProject.builder().create()
+    @ClassRule
+    static public GradleTestProject project = GradleTestProject.builder()
+            .fromTestProject("projectWithLocalDeps")
+            .create()
 
-    @Before
-    public void setUp() {
-        new HelloWorldApp().writeSources(project.testDir)
+    @BeforeClass
+    static void setUp() {
         project.getBuildFile() << """
 apply plugin: 'com.android.application'
 
@@ -44,19 +44,21 @@ android {
     compileSdkVersion $GradleTestProject.DEFAULT_COMPILE_SDK_VERSION
     buildToolsVersion "$GradleTestProject.DEFAULT_BUILD_TOOL_VERSION"
 }
-"""
-    }
 
-    @Test
-    public void unresolvedDependencies() {
-        project.getBuildFile() << """
 dependencies {
-    compile 'foo:bar:1.2.3'
+    compile files('libs/baseLib-1.0.aar')
 }
 """
-        assertThat(project.getSingleModel()).issues().hasSingleIssue(
-                SyncIssue.SEVERITY_ERROR,
-                SyncIssue.TYPE_UNRESOLVED_DEPENDENCY,
-                'foo:bar:1.2.3')
+
+    }
+
+    @AfterClass
+    static void cleanUp() {
+        project = null
+    }
+
+    @Test(expected=BuildException.class)
+    void "check model failed to load"() {
+        project.getSingleModel(true /*emulateOldeIde*/)
     }
 }

JetBrains/adt-tools-base

build-system/gradle/src/main/groovy/com/android/build/gradle/internal/ExtraModelInfo.java
@@ -17,6 +17,7 @@
 package com.android.build.gradle.internal;
 
 import static com.android.builder.model.AndroidProject.PROPERTY_BUILD_MODEL_ONLY;
+import static com.android.builder.model.AndroidProject.PROPERTY_BUILD_MODEL_ONLY_ADVANCED;
 
 import com.android.annotations.NonNull;
 import com.android.annotations.Nullable;
@@ -36,7 +37,6 @@ import com.android.builder.model.SourceProviderContainer;
 import com.android.builder.model.SyncIssue;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.ListMultimap;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 import org.gradle.api.GradleException;
@@ -45,7 +45,6 @@ import org.gradle.api.artifacts.Configuration;
 
 import java.io.File;
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -53,7 +52,11 @@ import java.util.Map;
  */
 public class ExtraModelInfo {
 
-    private boolean buildModelForIde;
+    public static enum ModelQueryMode {
+        STANDARD, IDE, IDE_ADVANCED
+    }
+
+    private ModelQueryMode modelQueryMode;
 
     private final Map<SyncIssueKey, SyncIssue> syncIssues = Maps.newHashMap();
 
@@ -72,23 +75,32 @@ public class ExtraModelInfo {
     private final ListMultimap<String, SourceProviderContainer> extraMultiFlavorSourceProviders = ArrayListMultimap.create();
 
     public ExtraModelInfo(Project project) {
-        buildModelForIde = getBuildModelForIde(project);
+        modelQueryMode = computeModelQueryMode(project);
     }
 
     public Map<SyncIssueKey, SyncIssue> getSyncIssues() {
         return syncIssues;
     }
 
-    public boolean isBuildModelForIde() {
-        return buildModelForIde;
+    public ModelQueryMode getModelQueryMode() {
+        return modelQueryMode;
     }
 
     public void handleSyncError(String data, int type, String msg) {
-        if (!buildModelForIde) {
-            throw new GradleException(msg);
-        } else {
-            SyncIssue syncIssue = new SyncIssueImpl(type, SyncIssue.SEVERITY_ERROR, data, msg);
-            syncIssues.put(SyncIssueKey.from(syncIssue), syncIssue);
+        switch (modelQueryMode) {
+            case STANDARD:
+                throw new GradleException(msg);
+            case IDE:
+                // compat mode for the only issue supported before the addition of SyncIssue
+                // in the model.
+                if (type != SyncIssue.TYPE_UNRESOLVED_DEPENDENCY) {
+                    throw new GradleException(msg);
+                }
+                // intended fall-through
+            case IDE_ADVANCED:
+                // new IDE, able to support SyncIssue.
+                SyncIssue syncIssue = new SyncIssueImpl(type, SyncIssue.SEVERITY_ERROR, data, msg);
+                syncIssues.put(SyncIssueKey.from(syncIssue), syncIssue);
         }
     }
 
@@ -198,14 +210,28 @@ public class ExtraModelInfo {
      * means we will attempt to resolve dependencies even if some are broken/unsupported to avoid
      * failing the import in the IDE.
      */
-    private boolean getBuildModelForIde(Project project) {
-        boolean flagValue = false;
-        if (project.hasProperty(PROPERTY_BUILD_MODEL_ONLY)) {
-            Object value = project.getProperties().get(PROPERTY_BUILD_MODEL_ONLY);
+    private static ModelQueryMode computeModelQueryMode(@NonNull Project project) {
+        if (isPropertyTrue(project, PROPERTY_BUILD_MODEL_ONLY_ADVANCED)) {
+            return ModelQueryMode.IDE_ADVANCED;
+        }
+
+        if (isPropertyTrue(project, PROPERTY_BUILD_MODEL_ONLY)) {
+            return ModelQueryMode.IDE;
+        }
+
+        return ModelQueryMode.STANDARD;
+    }
+
+    private static boolean isPropertyTrue(
+            @NonNull Project project,
+            @NonNull String propertyName) {
+        if (project.hasProperty(propertyName)) {
+            Object value = project.getProperties().get(propertyName);
             if (value instanceof String) {
-                flagValue = Boolean.parseBoolean((String) value);
+                return Boolean.parseBoolean((String) value);
             }
         }
-        return flagValue;
+
+        return false;
     }
 }

build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/common/fixture/GradleTestProject.java
@@ -429,11 +429,11 @@ public class GradleTestProject implements TestRule {
      * @param tasks Variadic list of tasks to execute.
      */
     public void execute(String ... tasks) {
-        execute(Collections.<String>emptyList(), false, tasks);
+        execute(Collections.<String>emptyList(), false, false, tasks);
     }
 
     public void execute(@NonNull List<String> arguments, String ... tasks) {
-        execute(arguments, false, tasks);
+        execute(arguments, false, false, tasks);
     }
 
     /**
@@ -445,8 +445,21 @@ public class GradleTestProject implements TestRule {
      */
     @NonNull
     public AndroidProject executeAndReturnModel(String ... tasks) {
+        return executeAndReturnModel(false, tasks);
+    }
+
+    /**
+     * Runs gradle on the project, and returns the project model.  Throws exception on failure.
+     *
+     * @param emulateStudio_1_0 whether to emulate an older IDE (studio 1.0) querying the model.
+     * @param tasks Variadic list of tasks to execute.
+     *
+     * @return the AndroidProject model for the project.
+     */
+    @NonNull
+    public AndroidProject executeAndReturnModel(boolean emulateStudio_1_0, String ... tasks) {
         //noinspection ConstantConditions
-        return execute(Collections.<String>emptyList(), true, tasks);
+        return execute(Collections.<String>emptyList(), true, emulateStudio_1_0, tasks);
     }
 
     /**
@@ -459,16 +472,31 @@ public class GradleTestProject implements TestRule {
      */
     @NonNull
     public Map<String, AndroidProject> executeAndReturnMultiModel(String ... tasks) {
+        return executeAndReturnMultiModel(false, tasks);
+    }
+
+    /**
+     * Runs gradle on the project, and returns a project model for each sub-project.
+     * Throws exception on failure.
+     *
+     * @param emulateStudio_1_0 whether to emulate an older IDE (studio 1.0) querying the model.
+     * @param tasks Variadic list of tasks to execute.
+     *
+     * @return the AndroidProject model for the project.
+     */
+    @NonNull
+    public Map<String, AndroidProject> executeAndReturnMultiModel(boolean emulateStudio_1_0, String ... tasks) {
         ProjectConnection connection = getProjectConnection();
         try {
             executeBuild(Collections.<String>emptyList(), connection, tasks);
 
-            return buildModel(connection);
+            return buildModel(connection, emulateStudio_1_0);
 
         } finally {
             connection.close();
         }
     }
+
 
     /**
      * Returns the project model without building.
@@ -477,9 +505,21 @@ public class GradleTestProject implements TestRule {
      */
     @NonNull
     public AndroidProject getSingleModel() {
+        return getSingleModel(false);
+    }
+
+    /**
+     * Returns the project model without building.
+     *
+     * This will fail if the project is a multi-project setup.
+     *
+     * @param emulateStudio_1_0 whether to emulate an older IDE (studio 1.0) querying the model.
+     */
+    @NonNull
+    public AndroidProject getSingleModel(boolean emulateStudio_1_0) {
         ProjectConnection connection = getProjectConnection();
         try {
-            Map<String, AndroidProject> modelMap = buildModel(connection);
+            Map<String, AndroidProject> modelMap = buildModel(connection, emulateStudio_1_0);
 
             // ensure there was only one project
             assertEquals("Quering GradleTestProject.getModel() with multi-project settings",
@@ -496,9 +536,19 @@ public class GradleTestProject implements TestRule {
      */
     @NonNull
     public Map<String, AndroidProject> getAllModels() {
+        return getAllModels(false);
+    }
+
+    /**
+     * Returns a project model for each sub-project without building.
+     *
+     * @param emulateStudio_1_0 whether to emulate an older IDE (studio 1.0) querying the model.
+     */
+    @NonNull
+    public Map<String, AndroidProject> getAllModels(boolean emulateStudio_1_0) {
         ProjectConnection connection = getProjectConnection();
         try {
-            return buildModel(connection);
+            return buildModel(connection, emulateStudio_1_0);
 
         } finally {
             connection.close();
@@ -510,6 +560,7 @@ public class GradleTestProject implements TestRule {
      *
      * @param arguments List of arguments for the gradle command.
      * @param returnModel whether the model should be queried and returned.
+     * @param emulateStudio_1_0 whether to emulate an older IDE (studio 1.0) querying the model.
      * @param tasks Variadic list of tasks to execute.
      *
      * @return the model, if <var>returnModel</var> was true, null otherwise
@@ -518,13 +569,14 @@ public class GradleTestProject implements TestRule {
     private AndroidProject execute(
             @NonNull List<String> arguments,
             boolean returnModel,
+            boolean emulateStudio_1_0,
             @NonNull String ... tasks) {
         ProjectConnection connection = getProjectConnection();
         try {
             executeBuild(arguments, connection, tasks);
 
             if (returnModel) {
-                Map<String, AndroidProject> modelMap = buildModel(connection);
+                Map<String, AndroidProject> modelMap = buildModel(connection, emulateStudio_1_0);
 
                 // ensure there was only one project
                 assertEquals("Quering GradleTestProject.getModel() with multi-project settings",
@@ -575,9 +627,14 @@ public class GradleTestProject implements TestRule {
 
     /**
      * Returns a project model for each sub-project without building.
+     *
+     * @param connection the opened ProjectConnection
+     * @param emulateStudio_1_0 whether to emulate an older IDE (studio 1.0) querying the model.
      */
     @NonNull
-    private static Map<String, AndroidProject> buildModel(@NonNull ProjectConnection connection) {
+    private static Map<String, AndroidProject> buildModel(
+            @NonNull ProjectConnection connection,
+            boolean emulateStudio_1_0) {
         GetModelAction getModelAction = new GetModelAction();
         BuildActionExecuter<Map<String, AndroidProject>> executer
                 = connection.action(getModelAction);
@@ -585,6 +642,10 @@ public class GradleTestProject implements TestRule {
         executer.withArguments(
                 "-P" + AndroidProject.PROPERTY_BUILD_MODEL_ONLY + "=true",
                 "-P" + AndroidProject.PROPERTY_INVOKED_FROM_IDE + "=true");
+        if (!emulateStudio_1_0) {
+            executer.withArguments(
+                    "-P" + AndroidProject.PROPERTY_BUILD_MODEL_ONLY_ADVANCED + "=true");
+        }
 
         executer.setJvmArguments(Iterables.toArray(getDebugJvmArguments(), String.class));
         return executer.run();

build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/dependencies/AppWithCompileLocalAarFromOlderIdeTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 The Android Open Source Project
+ * Copyright (C) 2015 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,29 +14,29 @@
  * limitations under the License.
  */
 
-package com.android.build.gradle.integration.application
+package com.android.build.gradle.integration.dependencies
 
 import com.android.build.gradle.integration.common.fixture.GradleTestProject
-import com.android.build.gradle.integration.common.fixture.app.HelloWorldApp
-import com.android.builder.model.SyncIssue
 import groovy.transform.CompileStatic
-import org.junit.Before
-import org.junit.Rule
+import org.gradle.tooling.BuildException
+import org.junit.AfterClass
+import org.junit.BeforeClass
+import org.junit.ClassRule
 import org.junit.Test
 
-import static com.android.build.gradle.integration.common.truth.TruthHelper.assertThat
 /**
- * General Model tests
+ * test for package (apk) local aar in app
  */
 @CompileStatic
-class ModelTest {
+class AppWithCompileLocalAarFromOlderIdeTest {
 
-    @Rule
-    public GradleTestProject project = GradleTestProject.builder().create()
+    @ClassRule
+    static public GradleTestProject project = GradleTestProject.builder()
+            .fromTestProject("projectWithLocalDeps")
+            .create()
 
-    @Before
-    public void setUp() {
-        new HelloWorldApp().writeSources(project.testDir)
+    @BeforeClass
+    static void setUp() {
         project.getBuildFile() << """
 apply plugin: 'com.android.application'
 
@@ -44,19 +44,21 @@ android {
     compileSdkVersion $GradleTestProject.DEFAULT_COMPILE_SDK_VERSION
     buildToolsVersion "$GradleTestProject.DEFAULT_BUILD_TOOL_VERSION"
 }
-"""
-    }
 
-    @Test
-    public void unresolvedDependencies() {
-        project.getBuildFile() << """
 dependencies {
-    compile 'foo:bar:1.2.3'
+    compile files('libs/baseLib-1.0.aar')
 }
 """
-        assertThat(project.getSingleModel()).issues().hasSingleIssue(
-                SyncIssue.SEVERITY_ERROR,
-                SyncIssue.TYPE_UNRESOLVED_DEPENDENCY,
-                'foo:bar:1.2.3')
+
+    }
+
+    @AfterClass
+    static void cleanUp() {
+        project = null
+    }
+
+    @Test(expected=BuildException.class)
+    void "check model failed to load"() {
+        project.getSingleModel(true /*emulateStudio_1_0*/)
     }
 }