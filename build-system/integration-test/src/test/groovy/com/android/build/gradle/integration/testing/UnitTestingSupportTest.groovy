bda4354d8406bb11984d5674909d6144dd2b3629
Michal Bendowski <bendowski@google.com>
2015-02-09 14:48:30 +0000

    Strip final flag from inner classes.
    JVM bytecode always finds a way to surprise me.
    
    Change-Id: I31ee073ae701e6dcd3d050a94864851d3cd51cd0
    (cherry picked from commit 8381eca5c0b3ab47cbbb275b56220f581d3cfd24)

--- build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/testing/UnitTestingSupportTest.groovy
+++ build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/testing/UnitTestingSupportTest.groovy
@@ -41,6 +41,7 @@ class UnitTestingSupportTest {
                 [ "referenceProductionCode",
                   "exceptions",
                   "mockFinalClass",
+                  "mockInnerClass",
                   "mockFinalMethod" ])
 
         checkResults(
