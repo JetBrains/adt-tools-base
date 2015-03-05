fe660e1fa0b1fc871753f813edcf66a6d6b37041
Michal Bendowski <bendowski@google.com>
2015-03-05 13:13:05 +0000

    Don't ignore per-build-type dependencies for unit testing.
    This is mostly for consistency. The testDebugCompile configuration
    exists already, it was just ignored, which is surprising.
    
    I extracted common parts from BuildTypeData and ProductFlavorData into
    VariantDimensionData to avoid code duplication.
    
    Change-Id: I257310919dfbb8b7ad0cd1eb530873d0fc76614f

--- build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/BuildTypeData.groovy
+++ build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/BuildTypeData.groovy
@@ -18,20 +18,13 @@ import com.android.annotations.NonNull
 import com.android.annotations.Nullable
 import com.android.build.gradle.internal.api.DefaultAndroidSourceSet
 import com.android.build.gradle.internal.dsl.BuildType
-import com.android.builder.core.VariantType
 import org.gradle.api.Project
 import org.gradle.api.Task
-import org.gradle.api.artifacts.Configuration
 /**
  * Class containing a BuildType and associated data (Sourceset for instance).
  */
-class BuildTypeData implements ConfigurationProvider {
-
+class BuildTypeData extends VariantDimensionData {
     final BuildType buildType
-    final DefaultAndroidSourceSet sourceSet
-    private final DefaultAndroidSourceSet unitTestSourceSet
-    private final Project project
-
     final Task assembleTask
 
     BuildTypeData(
@@ -39,43 +32,12 @@ class BuildTypeData implements ConfigurationProvider {
             @NonNull  Project project,
             @NonNull  DefaultAndroidSourceSet sourceSet,
             @Nullable DefaultAndroidSourceSet unitTestSourceSet) {
+        super(sourceSet, null, unitTestSourceSet, project)
+
         this.buildType = buildType
-        this.sourceSet = sourceSet
-        this.project = project
-        this.unitTestSourceSet = unitTestSourceSet
 
         assembleTask = project.tasks.create("assemble${buildType.name.capitalize()}")
         assembleTask.description = "Assembles all ${buildType.name.capitalize()} builds."
         assembleTask.group = "Build"
-    }
-
-    @Override
-    @NonNull
-    Configuration getCompileConfiguration() {
-        return project.configurations.getByName(sourceSet.compileConfigurationName)
-    }
-
-    @Override
-    @NonNull
-    Configuration getPackageConfiguration() {
-        return project.configurations.getByName(sourceSet.packageConfigurationName)
-    }
-
-    @NonNull
-    Configuration getProvidedConfiguration() {
-        return project.configurations.getByName(sourceSet.providedConfigurationName)
-    }
-
-    @Nullable
-    DefaultAndroidSourceSet getTestSourceSet(VariantType type) {
-        switch (type) {
-            case VariantType.UNIT_TEST:
-                return unitTestSourceSet
-            case VariantType.ANDROID_TEST:
-                // There are no per-build-type sources for android tests.
-                return null
-            default:
-                throw new IllegalArgumentException("Unknown test variant type $type.")
-        }
     }
 }
