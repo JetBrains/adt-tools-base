tadfisher/platform_tools_base

Rework support for rendering PNGs.
- Changed the name to preprocessDebugResources.
- The whole mechanism is still behind a flag.
- Added a new "stage", build/intermediates/res/preprocessed
  that should be used with aapt.
- A single task that figures out how to handle a given
  resource: copy as-is or replace with a bunch of replacement
  files.
- The new task does not use the DataMerger machinery, as it
  was my first experiment with making it incremental. It will
  have to modified to use it to correctly handle all incremental
  cases.
- Added first integration tests. They don't cover more complicated cases
  where project contains both an xml and PNGs for some densities.

Change-Id: Iee1197944d0f37d85bfbac2ac52d435775f0559f

build-system/gradle-core/src/main/groovy/com/android/build/gradle/tasks/PreprocessResourcesTask.java
@@ -1,0 +1,207 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.build.gradle.tasks;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import com.android.annotations.NonNull;
+import com.android.build.gradle.internal.tasks.IncrementalTask;
+import com.android.builder.png.VectorDrawableRenderer;
+import com.android.ide.common.res2.FileStatus;
+import com.android.resources.Density;
+import com.google.common.base.Charsets;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.SetMultimap;
+import com.google.common.io.Files;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.InputDirectory;
+import org.gradle.api.tasks.OutputDirectory;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * Generates PNGs from an Android vector drawables.
+ */
+public class PreprocessResourcesTask extends IncrementalTask {
+    public static final int MIN_SDK = VectorDrawableRenderer.MIN_SDK_WITH_VECTOR_SUPPORT;
+    private static final Type TYPE_TOKEN = new TypeToken<Map<String, Collection<String>>>() {}.getType();
+
+    private final VectorDrawableRenderer renderer = new VectorDrawableRenderer();
+    private File outputResDirectory;
+    private File generatedResDirectory;
+    private File mergedResDirectory;
+    private Collection<Density> densitiesToGenerate;
+
+    @Override
+    protected boolean isIncremental() {
+        return true;
+    }
+
+    @Override
+    protected void doIncrementalTaskAction(Map<File, FileStatus> changedInputs) {
+        try {
+            File stateFile = getStateFile();
+            if (!stateFile.exists()) {
+                doFullTaskAction();
+            }
+
+            SetMultimap<String, String> state = readState(stateFile);
+
+            for (Map.Entry<File, FileStatus> entry : changedInputs.entrySet()) {
+                switch (entry.getValue()) {
+                    case NEW:
+                    case CHANGED:
+                        handleFile(entry.getKey(), state);
+                        break;
+                    case REMOVED:
+                        for (String path : state.get(entry.getKey().getAbsolutePath())) {
+                            File file = new File(path);
+                            System.out.println("deleting " + path);
+                            file.delete();
+                        }
+                        state.removeAll(entry.getKey());
+                        break;
+                    default:
+                        throw new RuntimeException("Unsupported operation " + entry.getValue());
+                }
+            }
+
+            saveState(state);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doFullTaskAction() {
+        SetMultimap<String, String> state = HashMultimap.create();
+        emptyFolder(getOutputResDirectory());
+        emptyFolder(getGeneratedResDirectory());
+
+        try {
+            for (File resourceFile : getProject().fileTree(getMergedResDirectory())) {
+                handleFile(resourceFile, state);
+            }
+            saveState(state);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @NonNull
+    private static SetMultimap<String, String> readState(@NonNull File stateFile) throws IOException {
+        String stateString = Files.toString(stateFile, Charsets.UTF_8);
+        Map<String, Collection<String>> stateMap = new Gson().fromJson(stateString, TYPE_TOKEN);
+
+        SetMultimap<String, String> state = HashMultimap.create();
+        for (Map.Entry<String, Collection<String>> entry : stateMap.entrySet()) {
+            state.putAll(entry.getKey(), entry.getValue());
+        }
+        return state;
+    }
+
+    private void saveState(@NonNull Multimap<String, String> state) throws IOException {
+        File stateFile = getStateFile();
+        Files.write(new Gson().toJson(state.asMap(), TYPE_TOKEN), stateFile, Charsets.UTF_8);
+    }
+
+    @NonNull
+    private File getStateFile() {
+        return new File(getIncrementalFolder(), "state.json");
+    }
+
+
+    private void handleFile(@NonNull File resourceFile, @NonNull SetMultimap<String, String> state)
+            throws IOException {
+        if (renderer.isVectorDrawable(resourceFile)) {
+            Collection<File> generatedFiles = renderer.createPngFiles(
+                    resourceFile,
+                    getGeneratedResDirectory(),
+                    getDensitiesToGenerate());
+
+            for (File generatedFile : generatedFiles) {
+                copyFile(generatedFile, resourceFile, getGeneratedResDirectory(), state);
+            }
+        } else {
+            copyFile(resourceFile, resourceFile, getMergedResDirectory(), state);
+        }
+    }
+
+    private void copyFile(
+            @NonNull File resourceFile,
+            @NonNull File input,
+            @NonNull File resDir,
+            @NonNull SetMultimap<String, String> createdFiles) throws IOException {
+        checkNotNull(resDir);
+        String relativePath =
+                resDir.toURI().relativize(resourceFile.toURI()).getPath();
+
+        File finalFile = new File(getOutputResDirectory(), relativePath);
+        Files.createParentDirs(finalFile);
+        Files.copy(resourceFile, finalFile);
+        createdFiles.put(input.getAbsolutePath(), finalFile.getAbsolutePath());
+    }
+
+    @OutputDirectory
+    public File getGeneratedResDirectory() {
+        return generatedResDirectory;
+    }
+
+    public void setGeneratedResDirectory(File generatedResDirectory) {
+        this.generatedResDirectory = generatedResDirectory;
+    }
+
+    @InputDirectory
+    public File getMergedResDirectory() {
+        return mergedResDirectory;
+    }
+
+    public void setMergedResDirectory(File mergedResDirectory) {
+        this.mergedResDirectory = mergedResDirectory;
+    }
+
+    /**
+     * res directory where the generated PNGs should be put.
+     */
+    @OutputDirectory
+    public File getOutputResDirectory() {
+        return outputResDirectory;
+    }
+
+    public void setOutputResDirectory(File outputResDirectory) {
+        this.outputResDirectory = outputResDirectory;
+    }
+
+    @Input
+    public Collection<Density> getDensitiesToGenerate() {
+        return densitiesToGenerate;
+    }
+
+    public void setDensitiesToGenerate(
+            Collection<Density> densitiesToGenerate) {
+        this.densitiesToGenerate = densitiesToGenerate;
+    }
+
+}

JetBrains/adt-tools-base

build-system/gradle-core/src/main/groovy/com/android/build/gradle/tasks/PreprocessResourcesTask.java
@@ -1,0 +1,215 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.build.gradle.tasks;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import com.android.annotations.NonNull;
+import com.android.build.gradle.internal.tasks.IncrementalTask;
+import com.android.builder.png.VectorDrawableRenderer;
+import com.android.ide.common.res2.FileStatus;
+import com.android.resources.Density;
+import com.google.common.base.Charsets;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.SetMultimap;
+import com.google.common.io.Files;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.InputDirectory;
+import org.gradle.api.tasks.OutputDirectory;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * Generates PNGs from Android vector drawable files.
+ */
+public class PreprocessResourcesTask extends IncrementalTask {
+    public static final int MIN_SDK = VectorDrawableRenderer.MIN_SDK_WITH_VECTOR_SUPPORT;
+    private static final Type TYPE_TOKEN = new TypeToken<Map<String, Collection<String>>>() {}.getType();
+
+    private final VectorDrawableRenderer renderer = new VectorDrawableRenderer();
+    private File outputResDirectory;
+    private File generatedResDirectory;
+    private File mergedResDirectory;
+    private Collection<Density> densitiesToGenerate;
+
+    @Override
+    protected boolean isIncremental() {
+        return true;
+    }
+
+    @Override
+    protected void doIncrementalTaskAction(Map<File, FileStatus> changedInputs) {
+        try {
+            File stateFile = getStateFile();
+            if (!stateFile.exists()) {
+                doFullTaskAction();
+            }
+
+            SetMultimap<String, String> state = readState(stateFile);
+
+            for (Map.Entry<File, FileStatus> entry : changedInputs.entrySet()) {
+                switch (entry.getValue()) {
+                    case NEW:
+                    case CHANGED:
+                        handleFile(entry.getKey(), state);
+                        break;
+                    case REMOVED:
+                        for (String path : state.get(entry.getKey().getAbsolutePath())) {
+                            File file = new File(path);
+                            file.delete();
+                        }
+                        state.removeAll(entry.getKey());
+                        break;
+                    default:
+                        throw new RuntimeException("Unsupported operation " + entry.getValue());
+                }
+            }
+
+            saveState(state);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doFullTaskAction() {
+        SetMultimap<String, String> state = HashMultimap.create();
+        emptyFolder(getOutputResDirectory());
+        emptyFolder(getGeneratedResDirectory());
+
+        try {
+            for (File resourceFile : getProject().fileTree(getMergedResDirectory())) {
+                handleFile(resourceFile, state);
+            }
+            saveState(state);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @NonNull
+    private static SetMultimap<String, String> readState(@NonNull File stateFile) throws IOException {
+        String stateString = Files.toString(stateFile, Charsets.UTF_8);
+        Map<String, Collection<String>> stateMap = new Gson().fromJson(stateString, TYPE_TOKEN);
+
+        SetMultimap<String, String> state = HashMultimap.create();
+        for (Map.Entry<String, Collection<String>> entry : stateMap.entrySet()) {
+            state.putAll(entry.getKey(), entry.getValue());
+        }
+        return state;
+    }
+
+    private void saveState(@NonNull Multimap<String, String> state) throws IOException {
+        File stateFile = getStateFile();
+        Files.write(new Gson().toJson(state.asMap(), TYPE_TOKEN), stateFile, Charsets.UTF_8);
+    }
+
+    @NonNull
+    private File getStateFile() {
+        return new File(getIncrementalFolder(), "state.json");
+    }
+
+
+    private void handleFile(@NonNull File resourceFile, @NonNull SetMultimap<String, String> state)
+            throws IOException {
+        if (renderer.isVectorDrawable(resourceFile)) {
+            Collection<File> generatedFiles = renderer.createPngFiles(
+                    resourceFile,
+                    getGeneratedResDirectory(),
+                    getDensitiesToGenerate());
+
+            for (File generatedFile : generatedFiles) {
+                copyFile(generatedFile, resourceFile, getGeneratedResDirectory(), state);
+            }
+        } else {
+            copyFile(resourceFile, resourceFile, getMergedResDirectory(), state);
+        }
+    }
+
+    private void copyFile(
+            @NonNull File fileToUse,
+            @NonNull File originalFile,
+            @NonNull File resDir,
+            @NonNull SetMultimap<String, String> state) throws IOException {
+        checkNotNull(resDir);
+        String relativePath =
+                resDir.toURI().relativize(fileToUse.toURI()).getPath();
+
+        File finalFile = new File(getOutputResDirectory(), relativePath);
+        Files.createParentDirs(finalFile);
+        Files.copy(fileToUse, finalFile);
+        state.put(originalFile.getAbsolutePath(), finalFile.getAbsolutePath());
+    }
+
+    /**
+     * Directory in which to put generated files. They will be then copied to the final destination
+     * if this would not overwrite anything.
+     *
+     * @see #getOutputResDirectory()
+     */
+    @OutputDirectory
+    public File getGeneratedResDirectory() {
+        return generatedResDirectory;
+    }
+
+    public void setGeneratedResDirectory(File generatedResDirectory) {
+        this.generatedResDirectory = generatedResDirectory;
+    }
+
+    /**
+     * "Input" resource directory, with all resources for the current variant merged.
+     */
+    @InputDirectory
+    public File getMergedResDirectory() {
+        return mergedResDirectory;
+    }
+
+    public void setMergedResDirectory(File mergedResDirectory) {
+        this.mergedResDirectory = mergedResDirectory;
+    }
+
+    /**
+     * Resources directory that will be passed to aapt.
+     */
+    @OutputDirectory
+    public File getOutputResDirectory() {
+        return outputResDirectory;
+    }
+
+    public void setOutputResDirectory(File outputResDirectory) {
+        this.outputResDirectory = outputResDirectory;
+    }
+
+    @Input
+    public Collection<Density> getDensitiesToGenerate() {
+        return densitiesToGenerate;
+    }
+
+    public void setDensitiesToGenerate(
+            Collection<Density> densitiesToGenerate) {
+        this.densitiesToGenerate = densitiesToGenerate;
+    }
+
+}