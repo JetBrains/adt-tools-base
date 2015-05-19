725d6c464548c01ca5cd946dbda7a4e2501fd416
Alex Ruiz <alruiz@google.com>
2015-05-18 18:09:56 -0700

    Updated version of embedded Gradle dist to 2.4. (CL 2 of 3)
    Updated "latest" Gradle version to 2.4
    
    Change-Id: If7eead165374bdbc898966e2838ab1832fea52b2

--- common/src/main/java/com/android/SdkConstants.java
+++ common/src/main/java/com/android/SdkConstants.java
@@ -1375,7 +1375,7 @@ public final class SdkConstants {
 
     public static final String GRADLE_PLUGIN_NAME = "com.android.tools.build:gradle:";
     public static final String GRADLE_MINIMUM_VERSION = "2.2.1";
-    public static final String GRADLE_LATEST_VERSION = "2.2.1";
+    public static final String GRADLE_LATEST_VERSION = "2.4";
     public static final String GRADLE_PLUGIN_MINIMUM_VERSION = "1.0.0";
     public static final String GRADLE_PLUGIN_RECOMMENDED_VERSION = "1.2.3";
     public static final String GRADLE_PLUGIN_LATEST_VERSION = GRADLE_PLUGIN_RECOMMENDED_VERSION;
