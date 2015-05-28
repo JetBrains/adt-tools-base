29a95f81649429683cfcb38274dd91c4cf3e5017
Chris Warrington <cmw@google.com>
2015-05-28 00:53:02 -0700

    Convert Jack and Jill tasks to Java.
    Change-Id: Ibf8774a88fcfc9799caa47a979b3b084d4c266c0

--- build-system/gradle-core/src/main/groovy/com/android/build/gradle/tasks/JackTask.java
+++ build-system/gradle-core/src/main/groovy/com/android/build/gradle/tasks/JackTask.java
@@ -14,27 +14,35 @@
  * limitations under the License.
  */
 
-package com.android.build.gradle.tasks
+package com.android.build.gradle.tasks;
 
-import com.android.annotations.NonNull
-import com.android.build.gradle.internal.tasks.AbstractAndroidCompile
-import com.android.build.gradle.internal.tasks.FileSupplier
-import com.android.builder.core.AndroidBuilder
-import com.android.builder.tasks.Job
-import com.android.builder.tasks.JobContext
-import com.android.sdklib.BuildToolInfo
-import com.android.sdklib.repository.FullRevision
-import com.google.common.base.Charsets
-import com.google.common.io.Files
-import org.gradle.api.Task
-import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.InputFile
-import org.gradle.api.tasks.InputFiles
-import org.gradle.api.tasks.Optional
-import org.gradle.api.tasks.OutputFile
-import org.gradle.api.tasks.ParallelizableTask
-import org.gradle.api.tasks.TaskAction
-import proguard.ParseException
+import com.android.annotations.NonNull;
+import com.android.build.gradle.internal.tasks.AbstractAndroidCompile;
+import com.android.build.gradle.internal.tasks.FileSupplier;
+import com.android.builder.core.AndroidBuilder;
+import com.android.builder.tasks.Job;
+import com.android.builder.tasks.JobContext;
+import com.android.builder.tasks.Task;
+import com.android.ide.common.process.ProcessException;
+import com.android.sdklib.BuildToolInfo;
+import com.android.sdklib.repository.FullRevision;
+import com.google.common.base.Charsets;
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.collect.Iterables;
+import com.google.common.io.Files;
+
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.InputFile;
+import org.gradle.api.tasks.InputFiles;
+import org.gradle.api.tasks.Optional;
+import org.gradle.api.tasks.OutputFile;
+import org.gradle.api.tasks.ParallelizableTask;
+import org.gradle.api.tasks.TaskAction;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
 
 /**
  * Jack task.
@@ -43,66 +51,47 @@ import proguard.ParseException
 public class JackTask extends AbstractAndroidCompile
         implements FileSupplier, BinaryFileProviderTask {
 
-    final static FullRevision JACK_MIN_REV = new FullRevision(21, 1, 0)
+    public static final FullRevision JACK_MIN_REV = new FullRevision(21, 1, 0);
 
-    AndroidBuilder androidBuilder
+    private AndroidBuilder androidBuilder;
 
-    boolean isVerbose
+    private boolean isVerbose;
+    private boolean isDebugLog;
 
-    boolean isDebugLog
+    private Collection<File> packagedLibraries;
+    private Collection<File> proguardFiles;
+    private Collection<File> jarJarRuleFiles;
 
-    @InputFile
-    File getJackExe() {
-        new File(androidBuilder.targetInfo.buildTools.getPath(BuildToolInfo.PathId.JACK))
-    }
+    private boolean debug;
 
-    @InputFiles
-    Collection<File> packagedLibraries
+    private File tempFolder;
+    private File jackFile;
 
-    @InputFiles
-    @Optional
-    Collection<File> proguardFiles
+    private File mappingFile;
 
-    @InputFiles
-    @Optional
-    Collection<File> jarJarRuleFiles
+    private boolean multiDexEnabled;
 
-    @Input
-    boolean debug
+    private int minSdkVersion;
 
-    File tempFolder
+    private String javaMaxHeapSize;
 
-    @OutputFile
-    File jackFile
+    private File incrementalDir;
 
-    @OutputFile
-    @Optional
-    File mappingFile
-
-    @Input
-    boolean multiDexEnabled
-
-    @Input
-    int minSdkVersion
-
-    @Input
-    @Optional
-    String javaMaxHeapSize
-
-    @Input
-    @Optional
-    File incrementalDir
-
+    @Override
     @TaskAction
     public void compile() {
-        final Job<Void> job = new Job<Void>(getName(),
-                new com.android.builder.tasks.Task<Void>() {
-                    @Override
-                    public void run(@NonNull Job<Void> job,
-                            @NonNull JobContext<Void> context) throws IOException {
-                        JackTask.this.doMinification();
-                    }
-                });
+        final Job<Void> job = new Job<Void>(getName(), new Task<Void>() {
+            @Override
+            public void run(@NonNull Job<Void> job, @NonNull JobContext<Void> context)
+                    throws IOException {
+                try {
+                    JackTask.this.doMinification();
+                } catch (ProcessException e) {
+                    throw new IOException(e);
+                }
+            }
+
+        });
         try {
             SimpleWorkQueue.push(job);
 
@@ -112,23 +101,24 @@ public class JackTask extends AbstractAndroidCompile
             Thread.currentThread().interrupt();
             throw new RuntimeException(e);
         }
+
     }
 
-    void doMinification() {
+    private void doMinification() throws ProcessException, IOException {
 
         if (System.getenv("USE_JACK_API") == null ||
                 !androidBuilder.convertByteCodeUsingJackApis(
-                getDestinationDir(),
-                getJackFile(),
-                getClasspath().files,
-                getPackagedLibraries(),
-                getSource().files,
-                getProguardFiles(),
-                getMappingFile(),
-                getJarJarRuleFiles(),
-                getIncrementalDir(),
-                isMultiDexEnabled(),
-                getMinSdkVersion())) {
+                        getDestinationDir(),
+                        getJackFile(),
+                        getClasspath().getFiles(),
+                        getPackagedLibraries(),
+                        getSource().getFiles(),
+                        getProguardFiles(),
+                        getMappingFile(),
+                        getJarJarRuleFiles(),
+                        getIncrementalDir(),
+                        isMultiDexEnabled(),
+                        getMinSdkVersion())) {
 
             // no incremental support through command line so far.
             androidBuilder.convertByteCodeWithJack(
@@ -143,61 +133,194 @@ public class JackTask extends AbstractAndroidCompile
                     isMultiDexEnabled(),
                     getMinSdkVersion(),
                     isDebugLog,
-                    getJavaMaxHeapSize())
+                    getJavaMaxHeapSize());
         }
+
     }
 
-    private File computeEcjOptionFile() {
-        File folder = getTempFolder()
-        folder.mkdirs()
+    private File computeEcjOptionFile() throws IOException {
+        File folder = getTempFolder();
+        //noinspection ResultOfMethodCallIgnored
+        folder.mkdirs();
         File file = new File(folder, "ecj-options.txt");
 
-        StringBuffer sb = new StringBuffer()
+        StringBuilder sb = new StringBuilder();
 
-        for (File sourceFile : getSource().files) {
-            sb.append(sourceFile.absolutePath).append('\n')
+        for (File sourceFile : getSource().getFiles()) {
+            sb.append(sourceFile.getAbsolutePath()).append("\n");
         }
 
-        file.getParentFile().mkdirs()
+        //noinspection ResultOfMethodCallIgnored
+        file.getParentFile().mkdirs();
 
-        Files.write(sb.toString(), file, Charsets.UTF_8)
+        Files.write(sb.toString(), file, Charsets.UTF_8);
 
-        return file
+        return file;
     }
 
     private String computeBootClasspath() {
-        StringBuilder sb = new StringBuilder()
+        return Joiner.on(':').join(
+                Iterables.transform(getClasspath().getFiles(), GET_ABSOLUTE_PATH));
+    }
 
-        boolean first = true;
-        for (File file : getClasspath().files) {
-            if (!first) {
-                sb.append(':')
-            } else {
-                first = false
-            }
-            sb.append(file.getAbsolutePath())
+    private static final Function<File, String> GET_ABSOLUTE_PATH = new Function<File, String>() {
+        @Override
+        public String apply(File file) {
+            return file.getAbsolutePath();
         }
+    };
 
-        return sb.toString()
+
+    @InputFile
+    public File getJackExe() {
+        return new File(
+                androidBuilder.getTargetInfo().getBuildTools().getPath(BuildToolInfo.PathId.JACK));
+    }
+
+    public AndroidBuilder getAndroidBuilder() {
+        return androidBuilder;
+    }
+
+    public void setAndroidBuilder(AndroidBuilder androidBuilder) {
+        this.androidBuilder = androidBuilder;
+    }
+
+    public boolean getIsVerbose() {
+        return isVerbose;
+    }
+
+    public void setIsVerbose(boolean isVerbose) {
+        this.isVerbose = isVerbose;
+    }
+
+    public boolean getIsDebugLog() {
+        return isDebugLog;
+    }
+
+    public void setIsDebugLog(boolean isDebugLog) {
+        this.isDebugLog = isDebugLog;
+    }
+
+    @InputFiles
+    public Collection<File> getPackagedLibraries() {
+        return packagedLibraries;
+    }
+
+    public void setPackagedLibraries(Collection<File> packagedLibraries) {
+        this.packagedLibraries = packagedLibraries;
+    }
+
+    @InputFiles
+    @Optional
+    public Collection<File> getProguardFiles() {
+        return proguardFiles;
+    }
+
+    public void setProguardFiles(Collection<File> proguardFiles) {
+        this.proguardFiles = proguardFiles;
+    }
+
+    @InputFiles
+    @Optional
+    public Collection<File> getJarJarRuleFiles() {
+        return jarJarRuleFiles;
+    }
+
+    public void setJarJarRuleFiles(Collection<File> jarJarRuleFiles) {
+        this.jarJarRuleFiles = jarJarRuleFiles;
+    }
+
+    @Input
+    public boolean getDebug() {
+        return debug;
+    }
+
+    public void setDebug(boolean debug) {
+        this.debug = debug;
+    }
+
+    public File getTempFolder() {
+        return tempFolder;
+    }
+
+    public void setTempFolder(File tempFolder) {
+        this.tempFolder = tempFolder;
+    }
+
+    @OutputFile
+    public File getJackFile() {
+        return jackFile;
+    }
+
+    public void setJackFile(File jackFile) {
+        this.jackFile = jackFile;
+    }
+
+    @OutputFile
+    @Optional
+    public File getMappingFile() {
+        return mappingFile;
+    }
+
+    public void setMappingFile(File mappingFile) {
+        this.mappingFile = mappingFile;
+    }
+
+    @Input
+    public boolean isMultiDexEnabled() {
+        return multiDexEnabled;
+    }
+
+    public void setMultiDexEnabled(boolean multiDexEnabled) {
+        this.multiDexEnabled = multiDexEnabled;
+    }
+
+    @Input
+    public int getMinSdkVersion() {
+        return minSdkVersion;
+    }
+
+    public void setMinSdkVersion(int minSdkVersion) {
+        this.minSdkVersion = minSdkVersion;
+    }
+
+    @Input
+    @Optional
+    public String getJavaMaxHeapSize() {
+        return javaMaxHeapSize;
+    }
+
+    public void setJavaMaxHeapSize(String javaMaxHeapSize) {
+        this.javaMaxHeapSize = javaMaxHeapSize;
+    }
+
+    @Input
+    @Optional
+    public File getIncrementalDir() {
+        return incrementalDir;
+    }
+
+    public void setIncrementalDir(File incrementalDir) {
+        this.incrementalDir = incrementalDir;
     }
 
     @Override
     @NonNull
-    BinaryFileProviderTask.Artifact getArtifact() {
+    public BinaryFileProviderTask.Artifact getArtifact() {
         return new BinaryFileProviderTask.Artifact(
                 BinaryFileProviderTask.BinaryArtifactType.JACK,
-                getJackFile())
+                getJackFile());
     }
 
     // ----- FileSupplierTask ----
     @NonNull
     @Override
-    Task getTask() {
-        return this
+    public org.gradle.api.Task getTask() {
+        return this;
     }
 
     @Override
-    File get() {
-        return getMappingFile()
+    public File get() {
+        return getMappingFile();
     }
 }
--- build-system/gradle-core/src/main/groovy/com/android/build/gradle/tasks/JillTask.java
+++ build-system/gradle-core/src/main/groovy/com/android/build/gradle/tasks/JillTask.java
@@ -13,172 +13,215 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.build.gradle.tasks
-import com.android.SdkConstants
-import com.android.annotations.NonNull
-import com.android.build.gradle.internal.tasks.BaseTask
-import com.android.builder.core.AndroidBuilder
-import com.android.builder.core.DexOptions
-import com.android.ide.common.internal.WaitableExecutor
-import com.android.sdklib.repository.FullRevision
-import com.google.common.base.Charsets
-import com.google.common.collect.Lists
-import com.google.common.collect.Sets
-import com.google.common.hash.HashCode
-import com.google.common.hash.HashFunction
-import com.google.common.hash.Hashing
-import com.google.common.io.Files
-import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.InputFiles
-import org.gradle.api.tasks.Nested
-import org.gradle.api.tasks.OutputDirectory
-import org.gradle.api.tasks.ParallelizableTask
-import org.gradle.api.tasks.TaskAction
-import org.gradle.api.tasks.incremental.IncrementalTaskInputs
 
-import java.util.concurrent.Callable
+package com.android.build.gradle.tasks;
 
-import static com.android.build.gradle.tasks.JackTask.JACK_MIN_REV
+import com.android.SdkConstants;
+import com.android.annotations.NonNull;
+import com.android.build.gradle.internal.dsl.DexOptions;
+import com.android.build.gradle.internal.tasks.BaseTask;
+import com.android.builder.core.AndroidBuilder;
+import com.android.ide.common.internal.LoggedErrorException;
+import com.android.ide.common.internal.WaitableExecutor;
+import com.android.sdklib.repository.FullRevision;
+import com.google.common.base.Charsets;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import com.google.common.hash.HashCode;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
+import com.google.common.io.Files;
+
+import org.gradle.api.Action;
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.InputFiles;
+import org.gradle.api.tasks.Nested;
+import org.gradle.api.tasks.OutputDirectory;
+import org.gradle.api.tasks.ParallelizableTask;
+import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.tasks.incremental.IncrementalTaskInputs;
+import org.gradle.api.tasks.incremental.InputFileDetails;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Callable;
 
 @ParallelizableTask
 public class JillTask extends BaseTask {
 
-    // ----- PUBLIC TASK API -----
+    private Collection<File> inputLibs;
 
-    // ----- PRIVATE TASK API -----
-    @Input
-    String getBuildToolsVersion() {
-        getBuildTools().getRevision()
-    }
+    private File outputFolder;
 
-    @SuppressWarnings("GroovyUnusedDeclaration")
-    @InputFiles
-    Collection<File> inputLibs
-
-    @OutputDirectory
-    File outputFolder
-
-    @Nested
-    com.android.build.gradle.internal.dsl.DexOptions dexOptions
+    private DexOptions dexOptions;
 
     @TaskAction
-    void taskAction(IncrementalTaskInputs taskInputs) {
-        FullRevision revision = getBuilder().targetInfo.buildTools.revision
-        if (revision.compareTo(JACK_MIN_REV) < 0) {
-            throw new RuntimeException("Jack requires Build Tools ${JACK_MIN_REV.toString()} or later")
+    public void taskAction(IncrementalTaskInputs taskInputs)
+            throws LoggedErrorException, InterruptedException {
+        FullRevision revision = getBuilder().getTargetInfo().getBuildTools().getRevision();
+        if (revision.compareTo(JackTask.JACK_MIN_REV) < 0) {
+            throw new RuntimeException(
+                    "Jack requires Build Tools " + JackTask.JACK_MIN_REV.toString()
+                            + " or later");
         }
 
-        final File outFolder = getOutputFolder()
+        final File outFolder = getOutputFolder();
 
         // if we are not in incremental mode, then outOfDate will contain
         // all th files, but first we need to delete the previous output
         if (!taskInputs.isIncremental()) {
-            emptyFolder(outFolder)
+            emptyFolder(outFolder);
         }
 
-        final Set<String> hashs = Sets.newHashSet()
-        final WaitableExecutor<Void> executor = new WaitableExecutor<Void>()
-        final List<File> inputFileDetails = Lists.newArrayList()
+        final Set<String> hashs = Sets.newHashSet();
+        final WaitableExecutor<Void> executor = new WaitableExecutor<Void>();
+        final List<File> inputFileDetails = Lists.newArrayList();
 
-        final AndroidBuilder builder = getBuilder()
+        final AndroidBuilder builder = getBuilder();
 
-        taskInputs.outOfDate { final change ->
-            inputFileDetails.add(change.file)
-        }
+        taskInputs.outOfDate(new Action<InputFileDetails>() {
+            @Override
+            public void execute(InputFileDetails change) {
+                inputFileDetails.add(change.getFile());
+            }
+        });
 
         for (final File file : inputFileDetails) {
-            Callable<Void> action = new JillCallable(file, hashs, outFolder, builder)
-            executor.execute(action)
+            Callable<Void> action = new JillCallable(this, file, hashs, outFolder, builder);
+            executor.execute(action);
         }
 
-        taskInputs.removed { change ->
-            //noinspection GroovyAssignabilityCheck
-            File jackFile = getJackFileName(outFolder, change.file)
-            jackFile.delete()
-        }
+        taskInputs.removed(new Action<InputFileDetails>() {
+            @Override
+            public void execute(InputFileDetails change) {
+                File jackFile = getJackFileName(outFolder, ((InputFileDetails) change).getFile());
+                //noinspection ResultOfMethodCallIgnored
+                jackFile.delete();
+            }
+        });
 
-        executor.waitForTasksWithQuickFail(false)
+        executor.waitForTasksWithQuickFail(false);
+    }
+
+    @Input
+    public String getBuildToolsVersion() {
+        return getBuildTools().getRevision().toString();
+    }
+
+    @InputFiles
+    public Collection<File> getInputLibs() {
+        return inputLibs;
+    }
+
+    public void setInputLibs(Collection<File> inputLibs) {
+        this.inputLibs = inputLibs;
+    }
+
+    @OutputDirectory
+    public File getOutputFolder() {
+        return outputFolder;
+    }
+
+    public void setOutputFolder(File outputFolder) {
+        this.outputFolder = outputFolder;
+    }
+
+    @Nested
+    public DexOptions getDexOptions() {
+        return dexOptions;
+    }
+
+    public void setDexOptions(DexOptions dexOptions) {
+        this.dexOptions = dexOptions;
     }
 
     private final class JillCallable implements Callable<Void> {
-        @NonNull
-        private final File fileToProcess
-        @NonNull
-        private final Set<String> hashs
-        @NonNull
-        private final DexOptions options = getDexOptions()
-        @NonNull
-        final File outFolder
-        @NonNull
-        private final AndroidBuilder builder
 
-        private JillCallable(
-                @NonNull File file,
-                @NonNull Set<String> hashs,
-                @NonNull File outFolder,
-                @NonNull AndroidBuilder builder) {
-            this.fileToProcess = file
-            this.hashs = hashs
-            this.outFolder = outFolder
-            this.builder = builder
+        @NonNull
+        private final File fileToProcess;
+
+        @NonNull
+        private final Set<String> hashs;
+
+        @NonNull
+        private final com.android.builder.core.DexOptions options = getDexOptions();
+
+        @NonNull
+        private final File outFolder;
+
+        @NonNull
+        private final AndroidBuilder builder;
+
+        private JillCallable(JillTask enclosing, @NonNull File file, @NonNull Set<String> hashs,
+                @NonNull File outFolder, @NonNull AndroidBuilder builder) {
+            this.fileToProcess = file;
+            this.hashs = hashs;
+            this.outFolder = outFolder;
+            this.builder = builder;
         }
 
         @Override
-        Void call() throws Exception {
+        public Void call() throws Exception {
             // TODO remove once we can properly add a library as a dependency of its test.
-            String hash = getFileHash(fileToProcess)
+            String hash = getFileHash(fileToProcess);
 
             synchronized (hashs) {
                 if (hashs.contains(hash)) {
-                    return null
+                    return null;
                 }
 
-                hashs.add(hash)
+                hashs.add(hash);
             }
 
             //noinspection GroovyAssignabilityCheck
-            File jackFile = getJackFileName(outFolder, fileToProcess)
+            File jackFile = getJackFileName(outFolder, fileToProcess);
             //noinspection GroovyAssignabilityCheck
-            builder.convertLibraryToJack(fileToProcess, jackFile, options)
+            builder.convertLibraryToJack(fileToProcess, jackFile, options);
 
-            return null
+            return null;
+        }
+
+        @NonNull
+        public final File getOutFolder() {
+            return outFolder;
         }
     }
 
     /**
      * Returns the hash of a file.
+     *
      * @param file the file to hash
-     * @return
      */
-    private static String getFileHash(@NonNull File file) {
-        HashCode hashCode = Files.hash(file, Hashing.sha1())
-        return hashCode.toString()
+    private static String getFileHash(@NonNull File file) throws IOException {
+        HashCode hashCode = Files.hash(file, Hashing.sha1());
+        return hashCode.toString();
     }
 
     /**
-     * Returns a unique File for the converted library, even
-     * if there are 2 libraries with the same file names (but different
-     * paths)
+     * Returns a unique File for the converted library, even if there are 2 libraries with the same
+     * file names (but different paths)
      *
      * @param outFolder the output folder.
      * @param inputFile the library
-     * @return
      */
     @NonNull
-    static File getJackFileName(@NonNull File outFolder, @NonNull File inputFile) {
+    public static File getJackFileName(@NonNull File outFolder, @NonNull File inputFile) {
         // get the filename
-        String name = inputFile.getName()
+        String name = inputFile.getName();
         // remove the extension
-        int pos = name.lastIndexOf('.')
+        int pos = name.lastIndexOf('.');
         if (pos != -1) {
-            name = name.substring(0, pos)
+            name = name.substring(0, pos);
         }
 
         // add a hash of the original file path.
-        String input = inputFile.getAbsolutePath()
-        HashFunction hashFunction = Hashing.sha1()
-        HashCode hashCode = hashFunction.hashString(input, Charsets.UTF_16LE)
+        String input = inputFile.getAbsolutePath();
+        HashFunction hashFunction = Hashing.sha1();
+        HashCode hashCode = hashFunction.hashString(input, Charsets.UTF_16LE);
 
-        return new File(outFolder, name + "-" + hashCode.toString() + SdkConstants.DOT_JAR)
+        return new File(outFolder, name + "-" + hashCode.toString() + SdkConstants.DOT_JAR);
     }
 }
