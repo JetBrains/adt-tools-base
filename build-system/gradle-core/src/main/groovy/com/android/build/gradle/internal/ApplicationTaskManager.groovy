754e573e722710422ddaf37c1bfe912817ce5896
Chris Warrington <cmw@google.com>
2015-05-27 21:05:12 -0700

    Convert ApplicationTaskManager to Java.
    Change-Id: I68cce1b2478dad99db94e0332bbe5c80fe769f75

--- build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/ApplicationTaskManager.groovy
+++ build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/ApplicationTaskManager.groovy
@@ -14,45 +14,52 @@
  * limitations under the License.
  */
 
-package com.android.build.gradle.internal
+package com.android.build.gradle.internal;
 
-import com.android.annotations.NonNull
-import com.android.build.gradle.AndroidConfig
-import com.android.build.gradle.internal.profile.SpanRecorders
-import com.android.build.gradle.internal.scope.AndroidTask
-import com.android.build.gradle.internal.scope.VariantScope
-import com.android.build.gradle.internal.variant.ApplicationVariantData
-import com.android.build.gradle.internal.variant.BaseVariantData
-import com.android.build.gradle.internal.variant.BaseVariantOutputData
-import com.android.builder.core.AndroidBuilder
-import com.android.builder.profile.ExecutionType
-import org.gradle.api.Project
-import org.gradle.api.artifacts.Configuration
-import org.gradle.api.tasks.compile.JavaCompile
-import org.gradle.tooling.provider.model.ToolingModelBuilderRegistry
+import com.android.annotations.NonNull;
+import com.android.build.gradle.AndroidConfig;
+import com.android.build.gradle.internal.scope.AndroidTask;
+import com.android.build.gradle.internal.scope.VariantScope;
+import com.android.build.gradle.internal.variant.ApplicationVariantData;
+import com.android.build.gradle.internal.variant.BaseVariantData;
+import com.android.build.gradle.internal.variant.BaseVariantOutputData;
+import com.android.builder.core.AndroidBuilder;
+import com.android.builder.profile.ExecutionType;
+import com.android.builder.profile.Recorder;
+import com.android.builder.profile.ThreadRecorder;
+
+import org.gradle.api.Project;
+import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.tasks.compile.JavaCompile;
+import org.gradle.tooling.provider.model.ToolingModelBuilderRegistry;
+
+import java.io.File;
+import java.util.List;
+import java.util.Set;
 
 /**
  * TaskManager for creating tasks in an Android application project.
  */
-class ApplicationTaskManager extends TaskManager {
-    public ApplicationTaskManager (
+public class ApplicationTaskManager extends TaskManager {
+
+    public ApplicationTaskManager(
             Project project,
             AndroidBuilder androidBuilder,
             AndroidConfig extension,
             SdkHandler sdkHandler,
             DependencyManager dependencyManager,
             ToolingModelBuilderRegistry toolingRegistry) {
-        super(project, androidBuilder, extension, sdkHandler, dependencyManager, toolingRegistry)
+        super(project, androidBuilder, extension, sdkHandler, dependencyManager, toolingRegistry);
     }
 
     @Override
-    public void  createTasksForVariantData(
-            @NonNull TaskFactory tasks,
-            @NonNull BaseVariantData<? extends BaseVariantOutputData> variantData) {
+    public void createTasksForVariantData(
+            @NonNull final TaskFactory tasks,
+            @NonNull final BaseVariantData<? extends BaseVariantOutputData> variantData) {
         assert variantData instanceof ApplicationVariantData;
-        ApplicationVariantData appVariantData = (ApplicationVariantData) variantData;
+        final ApplicationVariantData appVariantData = (ApplicationVariantData) variantData;
 
-        VariantScope variantScope = variantData.getScope()
+        final VariantScope variantScope = variantData.getScope();
 
         createAnchorTasks(tasks, variantScope);
         createCheckManifestTask(tasks, variantScope);
@@ -60,96 +67,168 @@ class ApplicationTaskManager extends TaskManager {
         handleMicroApp(tasks, variantScope);
 
         // Add a task to process the manifest(s)
-        SpanRecorders.record(ExecutionType.APP_TASK_MANAGER_CREATE_MERGE_MANIFEST_TASK) {
-            createMergeAppManifestsTask(tasks, variantScope)
-        }
+        ThreadRecorder.get().record(ExecutionType.APP_TASK_MANAGER_CREATE_MERGE_MANIFEST_TASK,
+                new Recorder.Block<Void>() {
+                    @Override
+                    public Void call() {
+                        createMergeAppManifestsTask(tasks, variantScope);
+                        return null;
+                    }
+                });
 
         // Add a task to create the res values
-        SpanRecorders.record(ExecutionType.APP_TASK_MANAGER_CREATE_GENERATE_RES_VALUES_TASK) {
-            createGenerateResValuesTask(tasks, variantScope);
-        }
+        ThreadRecorder.get().record(ExecutionType.APP_TASK_MANAGER_CREATE_GENERATE_RES_VALUES_TASK,
+                new Recorder.Block<Void>() {
+                    @Override
+                    public Void call() {
+                        createGenerateResValuesTask(tasks, variantScope);
+                        return null;
+                    }
+                });
 
         // Add a task to compile renderscript files.
-        SpanRecorders.record(ExecutionType.APP_TASK_MANAGER_CREATE_CREATE_RENDERSCRIPT_TASK) {
-            createRenderscriptTask(tasks, variantScope);
-        }
+        ThreadRecorder.get().record(ExecutionType.APP_TASK_MANAGER_CREATE_CREATE_RENDERSCRIPT_TASK,
+                new Recorder.Block<Void>() {
+                    @Override
+                    public Void call() {
+                        createRenderscriptTask(tasks, variantScope);
+                        return null;
+                    }
+                });
 
         // Add a task to merge the resource folders
-        SpanRecorders.record(ExecutionType.APP_TASK_MANAGER_CREATE_MERGE_RESOURCES_TASK) {
-            createMergeResourcesTask(tasks, variantScope);
-        }
+        ThreadRecorder.get().record(ExecutionType.APP_TASK_MANAGER_CREATE_MERGE_RESOURCES_TASK,
+                new Recorder.Block<Void>() {
+                    @Override
+                    public Void call() {
+                        createMergeResourcesTask(tasks, variantScope);
+                        return null;
+                    }
+                });
 
         // Add a task to merge the asset folders
-        SpanRecorders.record(ExecutionType.APP_TASK_MANAGER_CREATE_MERGE_ASSETS_TASK) {
-            createMergeAssetsTask(tasks, variantScope);
-        }
+        ThreadRecorder.get().record(ExecutionType.APP_TASK_MANAGER_CREATE_MERGE_ASSETS_TASK,
+                new Recorder.Block<Void>() {
+                    @Override
+                    public Void call() {
+                        createMergeAssetsTask(tasks, variantScope);
+                        return null;
+                    }
+                });
 
         // Add a task to create the BuildConfig class
-        SpanRecorders.record(ExecutionType.APP_TASK_MANAGER_CREATE_BUILD_CONFIG_TASK) {
-            createBuildConfigTask(tasks, variantScope);
-        }
+        ThreadRecorder.get().record(ExecutionType.APP_TASK_MANAGER_CREATE_BUILD_CONFIG_TASK,
+                new Recorder.Block<Void>() {
+                    @Override
+                    public Void call() {
+                        createBuildConfigTask(tasks, variantScope);
+                        return null;
+                    }
+                });
 
-        SpanRecorders.record(ExecutionType.APP_TASK_MANAGER_CREATE_PREPROCESS_RESOURCES_TASK) {
-            createPreprocessResourcesTask(tasks, variantScope)
-        }
+        ThreadRecorder.get().record(ExecutionType.APP_TASK_MANAGER_CREATE_PREPROCESS_RESOURCES_TASK,
+                new Recorder.Block<Void>() {
+                    @Override
+                    public Void call() {
+                        createPreprocessResourcesTask(tasks, variantScope);
+                        return null;
+                    }
+                });
 
-        SpanRecorders.record(ExecutionType.APP_TASK_MANAGER_CREATE_PROCESS_RES_TASK) {
-            // Add a task to process the Android Resources and generate source files
-            createProcessResTask(tasks, variantScope, true /*generateResourcePackage*/);
+        ThreadRecorder.get().record(ExecutionType.APP_TASK_MANAGER_CREATE_PROCESS_RES_TASK,
+                new Recorder.Block<Void>() {
+                    @Override
+                    public Void call() {
+                        // Add a task to process the Android Resources and generate source files
+                        createProcessResTask(tasks, variantScope, true /*generateResourcePackage*/);
 
-            // Add a task to process the java resources
-            createProcessJavaResTask(tasks, variantScope);
-        }
+                        // Add a task to process the java resources
+                        createProcessJavaResTask(tasks, variantScope);
+                        return null;
+                    }
+                });
 
-        SpanRecorders.record(ExecutionType.APP_TASK_MANAGER_CREATE_AIDL_TASK) {
-            createAidlTask(tasks, variantScope);
-        }
+        ThreadRecorder.get().record(ExecutionType.APP_TASK_MANAGER_CREATE_AIDL_TASK,
+                new Recorder.Block<Void>() {
+                    @Override
+                    public Void call() {
+                        createAidlTask(tasks, variantScope);
+                        return null;
+                    }
+                });
 
         // Add a compile task
-        SpanRecorders.record(ExecutionType.APP_TASK_MANAGER_CREATE_COMPILE_TASK) {
-            AndroidTask<JavaCompile> javacTask = createJavacTask(tasks, variantScope);
+        ThreadRecorder.get().record(ExecutionType.APP_TASK_MANAGER_CREATE_COMPILE_TASK,
+                new Recorder.Block<Void>() {
+                    @Override
+                    public Void call() {
+                        AndroidTask<JavaCompile> javacTask = createJavacTask(tasks, variantScope);
 
-            if (variantData.getVariantConfiguration().getUseJack()) {
-                createJackTask(appVariantData, null /*testedVariant*/);
-            } else {
-                setJavaCompilerTask(javacTask, tasks, variantScope)
-                createJarTask(tasks, variantScope);
-                createPostCompilationTasks(tasks, variantScope);
-            }
-        }
+                        if (variantData.getVariantConfiguration().getUseJack()) {
+                            createJackTask(appVariantData, null  /*testedVariant*/);
+                        } else {
+                            setJavaCompilerTask(javacTask, tasks, variantScope);
+                            createJarTask(tasks, variantScope);
+                            createPostCompilationTasks(tasks, variantScope);
+                        }
+                        return null;
+                    }
+                });
 
         // Add NDK tasks
         if (isNdkTaskNeeded) {
-            SpanRecorders.record(ExecutionType.APP_TASK_MANAGER_CREATE_NDK_TASK) {
-                createNdkTasks(variantScope);
-            }
+            ThreadRecorder.get().record(ExecutionType.APP_TASK_MANAGER_CREATE_NDK_TASK,
+                    new Recorder.Block<Void>() {
+                        @Override
+                        public Void call() {
+                            createNdkTasks(variantScope);
+                            return null;
+                        }
+                    });
         } else {
             if (variantData.compileTask != null) {
-                variantData.compileTask.dependsOn(getNdkBuildable(variantData))
+                variantData.compileTask.dependsOn(getNdkBuildable(variantData));
             } else {
-                variantScope.compileTask.dependsOn(tasks, getNdkBuildable(variantData))
+                variantScope.getCompileTask().dependsOn(tasks, getNdkBuildable(variantData));
             }
         }
-        variantScope.setNdkBuildable(getNdkBuildable(variantData))
+        variantScope.setNdkBuildable(getNdkBuildable(variantData));
 
-        if (variantData.getSplitHandlingPolicy() ==
-                BaseVariantData.SplitHandlingPolicy.RELEASE_21_AND_AFTER_POLICY) {
-            if (extension.getBuildToolsRevision().getMajor() < 21) {
-                throw new RuntimeException("Pure splits can only be used with buildtools 21 and later")
+        if (variantData.getSplitHandlingPolicy().equals(
+                BaseVariantData.SplitHandlingPolicy.RELEASE_21_AND_AFTER_POLICY)) {
+            if (getExtension().getBuildToolsRevision().getMajor() < 21) {
+                throw new RuntimeException("Pure splits can only be used with buildtools 21 and later");
             }
-            SpanRecorders.record(ExecutionType.APP_TASK_MANAGER_CREATE_SPLIT_TASK) {
-                createSplitResourcesTasks(variantScope);
-                createSplitAbiTasks(variantScope);
-            }
+
+            ThreadRecorder.get().record(ExecutionType.APP_TASK_MANAGER_CREATE_SPLIT_TASK,
+                    new Recorder.Block<Void>() {
+                        @Override
+                        public Void call() {
+                            createSplitResourcesTasks(variantScope);
+                            createSplitAbiTasks(variantScope);
+                            return null;
+                        }
+                    });
         }
-        SpanRecorders.record(ExecutionType.APP_TASK_MANAGER_CREATE_PACKAGING_TASK) {
-            createPackagingTask(tasks, variantScope, true /*publishApk*/);
-        }
+
+        ThreadRecorder.get().record(ExecutionType.APP_TASK_MANAGER_CREATE_PACKAGING_TASK,
+                new Recorder.Block<Void>() {
+                    @Override
+                    public Void call() {
+                        createPackagingTask(tasks, variantScope, true /*publishApk*/);
+                        return null;
+                    }
+                });
 
         // create the lint tasks.
-        SpanRecorders.record(ExecutionType.APP_TASK_MANAGER_CREATE_LINT_TASK) {
-            createLintTasks(tasks, variantScope);
-        }
+        ThreadRecorder.get().record(ExecutionType.APP_TASK_MANAGER_CREATE_LINT_TASK,
+                new Recorder.Block<Void>() {
+                    @Override
+                    public Void call() {
+                        createLintTasks(tasks, variantScope);
+                        return null;
+                    }
+                });
     }
 
     /**
@@ -158,7 +237,7 @@ class ApplicationTaskManager extends TaskManager {
     private void handleMicroApp(
             @NonNull TaskFactory tasks,
             @NonNull VariantScope scope) {
-        BaseVariantData<? extends BaseVariantOutputData> variantData = scope.variantData
+        BaseVariantData<? extends BaseVariantOutputData> variantData = scope.getVariantData();
         if (variantData.getVariantConfiguration().getBuildType().isEmbedMicroApp()) {
             // get all possible configurations for the variant. We'll take the highest priority
             // of them that have a file.
