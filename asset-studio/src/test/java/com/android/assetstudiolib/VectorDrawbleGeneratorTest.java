e122323e1fdbf34d37927ff093d131ce6a85e0f3
ztenghui <ztenghui@google.com>
2015-06-03 14:23:56 -0700

    New approach for png generation
    Change-Id: I30447787dbb80a87beeaaa5077ac24e9f9502b21

--- asset-studio/src/test/java/com/android/assetstudiolib/VectorDrawbleGeneratorTest.java
+++ asset-studio/src/test/java/com/android/assetstudiolib/VectorDrawbleGeneratorTest.java
@@ -43,7 +43,8 @@ public class VectorDrawbleGeneratorTest extends GeneratorTest {
             assertTrue("Failure: Exception in Svg2Vector.parseSvgToXml!", false);
         }
 
-        BufferedImage image = VdPreview.getPreviewFromVectorXml(24, outStream.toString(), null);
+        final VdPreview.Size imageSize = VdPreview.Size.createSizeFromWidth(24);
+        BufferedImage image = VdPreview.getPreviewFromVectorXml(imageSize, outStream.toString(), null);
 
         String path = grandParentDir + imageName;
         InputStream is = GeneratorTest.class.getResourceAsStream(path);
