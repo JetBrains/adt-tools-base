a4134351a30c358685fc191ed052ff05c556e687
Chris Warrington <cmw@google.com>
2015-06-24 16:00:42 -0700

    Convert Gradle(Java)ProcessExecutor to java.
    The possible ProcessException that the processOutputHandler can throw is
    now caught and stored in the process result, to match the
    DefaultProcessExecutor.
    
    Change-Id: Id796b676f13f62a6c243546822aa8bda6190974b

--- build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/process/GradleJavaProcessExecutor.java
+++ build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/process/GradleJavaProcessExecutor.java
@@ -14,30 +14,34 @@
  * limitations under the License.
  */
 
-package com.android.build.gradle.internal.process
-import com.android.annotations.NonNull
-import com.android.ide.common.process.JavaProcessExecutor
-import com.android.ide.common.process.JavaProcessInfo
-import com.android.ide.common.process.ProcessOutput
-import com.android.ide.common.process.ProcessOutputHandler
-import com.android.ide.common.process.ProcessResult
-import groovy.transform.CompileStatic
-import org.gradle.api.Project
-import org.gradle.process.ExecResult
-import org.gradle.process.JavaExecSpec
+package com.android.build.gradle.internal.process;
+
+import com.android.annotations.NonNull;
+import com.android.ide.common.process.JavaProcessExecutor;
+import com.android.ide.common.process.JavaProcessInfo;
+import com.android.ide.common.process.ProcessException;
+import com.android.ide.common.process.ProcessOutput;
+import com.android.ide.common.process.ProcessOutputHandler;
+import com.android.ide.common.process.ProcessResult;
+
+import org.gradle.api.Action;
+import org.gradle.api.Project;
+import org.gradle.process.ExecResult;
+import org.gradle.process.JavaExecSpec;
+
+import java.io.File;
 
 /**
  * Implementation of JavaProcessExecutor that uses Gradle's mechanism to execute external java
  * processes.
  */
-@CompileStatic
 public class GradleJavaProcessExecutor implements JavaProcessExecutor {
 
     @NonNull
     private final Project project;
 
     public GradleJavaProcessExecutor(@NonNull Project project) {
-        this.project = project
+        this.project = project;
     }
 
     @NonNull
@@ -45,30 +49,45 @@ public class GradleJavaProcessExecutor implements JavaProcessExecutor {
     public ProcessResult execute(
             @NonNull JavaProcessInfo javaProcessInfo,
             @NonNull ProcessOutputHandler processOutputHandler) {
-        ProcessOutput output = processOutputHandler.createOutput()
+        ProcessOutput output = processOutputHandler.createOutput();
 
-        ExecResult result = project.javaexec(getJavaExecClosure(javaProcessInfo, output))
+        ExecResult result = project.javaexec(new ExecAction(javaProcessInfo, output));
 
-        processOutputHandler.handleOutput(output);
+        try {
+            processOutputHandler.handleOutput(output);
+        } catch (ProcessException e) {
+            return new OutputHandlerFailedGradleProcessResult(e);
+        }
 
-        return new GradleProcessResult(result)
+        return new GradleProcessResult(result);
     }
 
-    @NonNull
-    private static Closure getJavaExecClosure(
-            @NonNull final JavaProcessInfo javaProcessInfo,
-            @NonNull final ProcessOutput processOutput) {
-        return { JavaExecSpec javaExecSpec ->
-            javaExecSpec.classpath(new File(javaProcessInfo.getClasspath()))
-            javaExecSpec.setMain(javaProcessInfo.getMainClass())
-            javaExecSpec.args(javaProcessInfo.getArgs())
-            javaExecSpec.jvmArgs(javaProcessInfo.getJvmArgs())
-            javaExecSpec.environment(javaProcessInfo.getEnvironment())
-            javaExecSpec.setStandardOutput(processOutput.getStandardOutput())
-            javaExecSpec.setErrorOutput(processOutput.getErrorOutput())
+    private static class ExecAction implements Action<JavaExecSpec> {
+
+        @NonNull
+        private final JavaProcessInfo javaProcessInfo;
+
+        @NonNull
+        private final ProcessOutput processOutput;
+
+        private ExecAction(@NonNull JavaProcessInfo javaProcessInfo,
+                @NonNull ProcessOutput processOutput) {
+            this.javaProcessInfo = javaProcessInfo;
+            this.processOutput = processOutput;
+        }
+
+        @Override
+        public void execute(JavaExecSpec javaExecSpec) {
+            javaExecSpec.classpath(new File(javaProcessInfo.getClasspath()));
+            javaExecSpec.setMain(javaProcessInfo.getMainClass());
+            javaExecSpec.args(javaProcessInfo.getArgs());
+            javaExecSpec.jvmArgs(javaProcessInfo.getJvmArgs());
+            javaExecSpec.environment(javaProcessInfo.getEnvironment());
+            javaExecSpec.setStandardOutput(processOutput.getStandardOutput());
+            javaExecSpec.setErrorOutput(processOutput.getErrorOutput());
 
             // we want the caller to be able to do its own thing.
-            javaExecSpec.setIgnoreExitValue(true)
+            javaExecSpec.setIgnoreExitValue(true);
         }
     }
 }
--- build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/process/GradleProcessExecutor.java
+++ build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/process/GradleProcessExecutor.java
@@ -14,29 +14,31 @@
  * limitations under the License.
  */
 
-package com.android.build.gradle.internal.process
-import com.android.annotations.NonNull
-import com.android.ide.common.process.ProcessExecutor
-import com.android.ide.common.process.ProcessInfo
-import com.android.ide.common.process.ProcessOutput
-import com.android.ide.common.process.ProcessOutputHandler
-import com.android.ide.common.process.ProcessResult
-import groovy.transform.CompileStatic
-import org.gradle.api.Project
-import org.gradle.process.ExecResult
-import org.gradle.process.ExecSpec
+package com.android.build.gradle.internal.process;
+
+import com.android.annotations.NonNull;
+import com.android.ide.common.process.ProcessException;
+import com.android.ide.common.process.ProcessExecutor;
+import com.android.ide.common.process.ProcessInfo;
+import com.android.ide.common.process.ProcessOutput;
+import com.android.ide.common.process.ProcessOutputHandler;
+import com.android.ide.common.process.ProcessResult;
+
+import org.gradle.api.Action;
+import org.gradle.api.Project;
+import org.gradle.process.ExecResult;
+import org.gradle.process.ExecSpec;
 
 /**
  * Implementation of ProcessExecutor that uses Gradle's mechanism to execute external processes.
  */
-@CompileStatic
 public class GradleProcessExecutor implements ProcessExecutor {
 
     @NonNull
     private final Project project;
 
     public GradleProcessExecutor(@NonNull Project project) {
-        this.project = project
+        this.project = project;
     }
 
     @NonNull
@@ -44,29 +46,43 @@ public class GradleProcessExecutor implements ProcessExecutor {
     public ProcessResult execute(
             @NonNull ProcessInfo processInfo,
             @NonNull ProcessOutputHandler processOutputHandler) {
-        ProcessOutput output = processOutputHandler.createOutput()
+        ProcessOutput output = processOutputHandler.createOutput();
 
-        ExecResult result = project.exec(getExecClosure(processInfo, output))
+        final ExecResult result = project.exec(new ExecAction(processInfo, output));
 
-        processOutputHandler.handleOutput(output);
+        try {
+            processOutputHandler.handleOutput(output);
+        } catch (final ProcessException e) {
+            return new OutputHandlerFailedGradleProcessResult(e);
+        }
 
-        return new GradleProcessResult(result)
+        return new GradleProcessResult(result);
     }
 
-    @NonNull
-    private static Closure getExecClosure(
-            @NonNull final ProcessInfo processInfo,
-            @NonNull final ProcessOutput processOutput) {
-        return { ExecSpec execSpec ->
+    private static class ExecAction implements Action<ExecSpec> {
 
-            execSpec.setExecutable(processInfo.executable)
-            execSpec.args(processInfo.getArgs())
-            execSpec.environment(processInfo.getEnvironment())
-            execSpec.setStandardOutput(processOutput.getStandardOutput())
-            execSpec.setErrorOutput(processOutput.getErrorOutput())
+        @NonNull
+        private final ProcessInfo processInfo;
+
+        @NonNull
+        private final ProcessOutput processOutput;
+
+        ExecAction(@NonNull final ProcessInfo processInfo,
+                @NonNull final ProcessOutput processOutput) {
+            this.processInfo = processInfo;
+            this.processOutput = processOutput;
+        }
+
+        @Override
+        public void execute(ExecSpec execSpec) {
+            execSpec.setExecutable(processInfo.getExecutable());
+            execSpec.args(processInfo.getArgs());
+            execSpec.environment(processInfo.getEnvironment());
+            execSpec.setStandardOutput(processOutput.getStandardOutput());
+            execSpec.setErrorOutput(processOutput.getErrorOutput());
 
             // we want the caller to be able to do its own thing.
-            execSpec.setIgnoreExitValue(true)
+            execSpec.setIgnoreExitValue(true);
         }
     }
 }
