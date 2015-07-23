993ba9b7c8bf0fb00b9b5263e02e749212c99c00
Raymond Chiu <chiur@google.com>
2015-07-23 11:46:51 -0700

    Convert PackageSplitAbi task to java
    Change-Id: I75516bf5165248a4acba8dfc2d2c2362cb9b5b24

--- build-system/gradle-core/src/main/groovy/com/android/build/gradle/tasks/PackageSplitAbi.java
+++ build-system/gradle-core/src/main/groovy/com/android/build/gradle/tasks/PackageSplitAbi.java
@@ -14,81 +14,89 @@
  * limitations under the License.
  */
 
-package com.android.build.gradle.tasks
+package com.android.build.gradle.tasks;
 
-import com.android.annotations.NonNull
-import com.android.annotations.Nullable
-import com.android.build.FilterData
-import com.android.build.OutputFile
-import com.android.build.gradle.api.ApkOutputFile
-import com.android.build.gradle.internal.dsl.PackagingOptions
-import com.android.build.gradle.internal.model.FilterDataImpl
-import com.android.builder.model.SigningConfig
-import com.android.builder.signing.SignedJarBuilder
-import com.google.common.base.Joiner
-import com.google.common.collect.ImmutableList
-import com.google.common.collect.ImmutableSet
-import com.google.common.util.concurrent.Callables
-import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.InputFiles
-import org.gradle.api.tasks.Nested
-import org.gradle.api.tasks.Optional
-import org.gradle.api.tasks.ParallelizableTask
-import org.gradle.api.tasks.OutputFiles
-import org.gradle.api.tasks.TaskAction
+import com.android.annotations.NonNull;
+import com.android.annotations.Nullable;
+import com.android.build.FilterData;
+import com.android.build.OutputFile;
+import com.android.build.gradle.api.ApkOutputFile;
+import com.android.build.gradle.internal.dsl.PackagingOptions;
+import com.android.build.gradle.internal.model.FilterDataImpl;
+import com.android.builder.model.SigningConfig;
+import com.android.builder.packaging.DuplicateFileException;
+import com.android.builder.packaging.PackagerException;
+import com.android.builder.packaging.SigningException;
+import com.android.builder.signing.SignedJarBuilder;
+import com.android.ide.common.signing.KeytoolException;
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Lists;
+import com.google.common.util.concurrent.Callables;
 
-import java.util.regex.Matcher
-import java.util.regex.Pattern
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.InputFiles;
+import org.gradle.api.tasks.Nested;
+import org.gradle.api.tasks.Optional;
+import org.gradle.api.tasks.OutputFiles;
+import org.gradle.api.tasks.ParallelizableTask;
+import org.gradle.api.tasks.TaskAction;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Package a abi dimension specific split APK
  */
 @ParallelizableTask
-class PackageSplitAbi extends SplitRelatedTask {
+public class PackageSplitAbi extends SplitRelatedTask {
 
-    ImmutableList<ApkOutputFile> outputFiles;
+    private ImmutableList<ApkOutputFile> outputFiles;
 
-    @InputFiles
-    Collection<File> inputFiles
+    private Collection<File> inputFiles;
 
-    File outputDirectory
+    private File outputDirectory;
 
-    @Input
-    Set<String> splits
+    private Set<String> splits;
 
-    @Input
-    String outputBaseName
+    private String outputBaseName;
 
-    @Input
-    boolean jniDebuggable
+    private boolean jniDebuggable;
 
-    @Nested @Optional
-    SigningConfig signingConfig
+    private SigningConfig signingConfig;
 
-    @Nested
-    PackagingOptions packagingOptions
+    private PackagingOptions packagingOptions;
 
-    @Input
-    Collection<File> jniFolders;
+    private Collection<File> jniFolders;
 
-    File mergingFolder
+    private File mergingFolder;
+
+    private SignedJarBuilder.IZipEntryFilter packagingOptionsFilter;
 
     @OutputFiles
     public List<File> getOutputFiles() {
-        return getOutputSplitFiles()*.getOutputFile();
+        ImmutableList.Builder<File> builder = ImmutableList.builder();
+        for (ApkOutputFile apk : getOutputSplitFiles()) {
+            builder.add(apk.getOutputFile());
+        }
+        return builder.build();
     }
 
     @Override
     @Nullable
-    File getApkMetadataFile() {
+    public File getApkMetadataFile() {
         return null;
     }
 
-    SignedJarBuilder.IZipEntryFilter packagingOptionsFilter;
-
-
+    @Override
     @NonNull
-    public synchronized  ImmutableList<ApkOutputFile> getOutputSplitFiles() {
+    public synchronized ImmutableList<ApkOutputFile> getOutputSplitFiles() {
 
         if (outputFiles == null) {
             ImmutableList.Builder<ApkOutputFile> builder = ImmutableList.builder();
@@ -96,12 +104,12 @@ class PackageSplitAbi extends SplitRelatedTask {
                 String apkName = getApkName(split);
                 ApkOutputFile apkOutput = new ApkOutputFile(
                         OutputFile.OutputType.SPLIT,
-                        ImmutableList.<FilterData> of(
-                                FilterDataImpl.build(OutputFile.ABI, apkName)),
-                        Callables.returning(new File(outputDirectory, apkName)))
-                builder.add(apkOutput)
+                        ImmutableList.of(FilterDataImpl.build(OutputFile.ABI, apkName)),
+                        Callables.returning(new File(outputDirectory, apkName)));
+                builder.add(apkOutput);
             }
-            outputFiles = builder.build()
+
+            outputFiles = builder.build();
         }
         return outputFiles;
     }
@@ -116,54 +124,146 @@ class PackageSplitAbi extends SplitRelatedTask {
     }
 
     @TaskAction
-    protected void doFullTaskAction() {
+    protected void doFullTaskAction() throws FileNotFoundException, SigningException,
+            KeytoolException, DuplicateFileException, PackagerException {
 
         // resources- and .ap_ should be shared in a setting somewhere. see BasePlugin:1206
-        final Pattern pattern = Pattern.compile(
-                "resources-${getOutputBaseName()}-(.*).ap_")
-        List<String> unprocessedSplits = new ArrayList(splits);
+        final Pattern pattern = Pattern.compile("resources-" + getOutputBaseName() + "-(.*).ap_");
+        List<String> unprocessedSplits = Lists.newArrayList(splits);
         for (File file : inputFiles) {
             Matcher matcher = pattern.matcher(file.getName());
             if (matcher.matches() && isAbiSplit(file.getName())) {
-                String apkName = getApkName(matcher.group(1))
+                String apkName = getApkName(matcher.group(1));
 
                 File outFile = new File(getOutputDirectory(), apkName);
                 getBuilder().packageApk(
-                        file.absolutePath,
+                        file.getAbsolutePath(),
                         null, /* dexFolder */
-                        null, /* dexedLibraries */
-                        ImmutableList.of(),
+                        ImmutableList.<File>of(), /* dexedLibraries */
+                        ImmutableList.<File>of(),
                         null, /* getJavaResourceDir */
                         getJniFolders(),
                         getMergingFolder(),
                         ImmutableSet.of(matcher.group(1)),
-                        getJniDebuggable(),
+                        isJniDebuggable(),
                         getSigningConfig(),
                         getPackagingOptions(),
                         getPackagingOptionsFilter(),
-                        outFile.absolutePath)
+                        outFile.getAbsolutePath());
                 unprocessedSplits.remove(matcher.group(1));
             }
         }
         if (!unprocessedSplits.isEmpty()) {
-            String message = String.format("Could not find resource package for %1$s",
-                    Joiner.on(',').join(unprocessedSplits));
-            logger.error(message);
+            String message = "Could not find resource package for "
+                    + Joiner.on(',').join(unprocessedSplits);
+            getLogger().error(message);
             throw new IllegalStateException(message);
         }
     }
 
     @Override
-    List<FilterData> getSplitsData() {
-        ImmutableList.Builder<FilterData> filterDataBuilder = ImmutableList.builder()
-        addAllFilterData(filterDataBuilder, splits, OutputFile.FilterType.ABI);
-        return filterDataBuilder.build()
+    public List<FilterData> getSplitsData() {
+        ImmutableList.Builder<FilterData> filterDataBuilder = ImmutableList.builder();
+        SplitRelatedTask.addAllFilterData(filterDataBuilder, splits, OutputFile.FilterType.ABI);
+        return filterDataBuilder.build();
     }
 
-    private String getApkName(String split) {
-        String apkName = "${project.archivesBaseName}-${getOutputBaseName()}_${split}"
-        return apkName + (getSigningConfig() == null
-                ? "-unsigned.apk"
-                : "-unaligned.apk")
+    private String getApkName(final String split) {
+        String archivesBaseName = (String)getProject().getProperties().get("archivesBaseName");
+        String apkName = archivesBaseName + "-" + getOutputBaseName() + "_" + split;
+        return apkName + (getSigningConfig() == null ? "-unsigned.apk" : "-unaligned.apk");
     }
+
+    public void setOutputFiles(ImmutableList<ApkOutputFile> outputFiles) {
+        this.outputFiles = outputFiles;
+    }
+
+    @InputFiles
+    public Collection<File> getInputFiles() {
+        return inputFiles;
+    }
+
+    public void setInputFiles(Collection<File> inputFiles) {
+        this.inputFiles = inputFiles;
+    }
+
+    public File getOutputDirectory() {
+        return outputDirectory;
+    }
+
+    public void setOutputDirectory(File outputDirectory) {
+        this.outputDirectory = outputDirectory;
+    }
+
+    @Input
+    public Set<String> getSplits() {
+        return splits;
+    }
+
+    public void setSplits(Set<String> splits) {
+        this.splits = splits;
+    }
+
+    @Input
+    public String getOutputBaseName() {
+        return outputBaseName;
+    }
+
+    public void setOutputBaseName(String outputBaseName) {
+        this.outputBaseName = outputBaseName;
+    }
+
+    @Input
+    public boolean isJniDebuggable() {
+        return jniDebuggable;
+    }
+
+    public void setJniDebuggable(boolean jniDebuggable) {
+        this.jniDebuggable = jniDebuggable;
+    }
+
+    @Nested
+    @Optional
+    public SigningConfig getSigningConfig() {
+        return signingConfig;
+    }
+
+    public void setSigningConfig(SigningConfig signingConfig) {
+        this.signingConfig = signingConfig;
+    }
+
+    @Nested
+    public PackagingOptions getPackagingOptions() {
+        return packagingOptions;
+    }
+
+    public void setPackagingOptions(PackagingOptions packagingOptions) {
+        this.packagingOptions = packagingOptions;
+    }
+
+    @Input
+    public Collection<File> getJniFolders() {
+        return jniFolders;
+    }
+
+    public void setJniFolders(Collection<File> jniFolders) {
+        this.jniFolders = jniFolders;
+    }
+
+    public File getMergingFolder() {
+        return mergingFolder;
+    }
+
+    public void setMergingFolder(File mergingFolder) {
+        this.mergingFolder = mergingFolder;
+    }
+
+    public SignedJarBuilder.IZipEntryFilter getPackagingOptionsFilter() {
+        return packagingOptionsFilter;
+    }
+
+    public void setPackagingOptionsFilter(SignedJarBuilder.IZipEntryFilter packagingOptionsFilter) {
+        this.packagingOptionsFilter = packagingOptionsFilter;
+    }
+
 }
