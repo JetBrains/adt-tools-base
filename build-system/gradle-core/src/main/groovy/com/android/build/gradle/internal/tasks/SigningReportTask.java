5c440524bc7dab3764aa1513abf22de4dec8cd25
Chris Warrington <cmw@google.com>
2015-07-27 16:51:34 +0100

    Convert SigningReportTask to Java.
    Change-Id: I329c64759a8b0c165797007de15ec0873eafba23

--- build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/tasks/SigningReportTask.java
+++ build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/tasks/SigningReportTask.java
@@ -14,157 +14,161 @@
  * limitations under the License.
  */
 
-package com.android.build.gradle.internal.tasks
+package com.android.build.gradle.internal.tasks;
 
-import com.android.build.gradle.internal.variant.BaseVariantData
-import com.android.builder.model.SigningConfig
-import com.android.ide.common.signing.CertificateInfo
-import com.android.ide.common.signing.KeystoreHelper
-import com.android.ide.common.signing.KeytoolException
-import com.google.common.collect.Maps
-import org.gradle.api.DefaultTask
-import org.gradle.api.tasks.TaskAction
-import org.gradle.logging.StyledTextOutput
-import org.gradle.logging.StyledTextOutputFactory
+import com.android.annotations.NonNull;
+import com.android.build.gradle.internal.variant.BaseVariantData;
+import com.android.builder.model.SigningConfig;
+import com.android.ide.common.signing.CertificateInfo;
+import com.android.ide.common.signing.KeystoreHelper;
+import com.android.ide.common.signing.KeytoolException;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 
-import java.security.MessageDigest
-import java.security.NoSuchAlgorithmException
-import java.security.cert.Certificate
-import java.security.cert.CertificateEncodingException
-import java.text.DateFormat
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
+import org.gradle.logging.StyledTextOutput;
+import org.gradle.logging.StyledTextOutputFactory;
 
-import static org.gradle.logging.StyledTextOutput.Style.Description
-import static org.gradle.logging.StyledTextOutput.Style.Failure
-import static org.gradle.logging.StyledTextOutput.Style.Identifier
-import static org.gradle.logging.StyledTextOutput.Style.Normal
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateEncodingException;
+import java.text.DateFormat;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+
+import static org.gradle.logging.StyledTextOutput.Style.Description;
+import static org.gradle.logging.StyledTextOutput.Style.Failure;
+import static org.gradle.logging.StyledTextOutput.Style.Identifier;
+import static org.gradle.logging.StyledTextOutput.Style.Normal;
 
 /**
  * Report tasks displaying the signing information for all variants.
  */
-class SigningReportTask extends DefaultTask {
+public class SigningReportTask extends DefaultTask {
 
-    private Set<BaseVariantData> variants = [];
+    private Set<BaseVariantData> variants = Sets.newHashSet();
 
     @TaskAction
     public void generate() throws IOException {
 
         StyledTextOutput textOutput = getServices().get(
-                StyledTextOutputFactory.class).create(getClass())
+                StyledTextOutputFactory.class).create(getClass());
 
-        Map<SigningConfig, SigningInfo> cache = Maps.newHashMap()
+        Map<SigningConfig, SigningInfo> cache = Maps.newHashMap();
 
         for (BaseVariantData variant : variants) {
-            textOutput.withStyle(Identifier).text("Variant: ")
-            textOutput.withStyle(Description).text(variant.name)
-            textOutput.println()
+            textOutput.withStyle(Identifier).text("Variant: ");
+            textOutput.withStyle(Description).text(variant.getName());
+            textOutput.println();
 
             // get the data
-            com.android.build.gradle.internal.dsl.SigningConfig signingConfig = (com.android.build.gradle.internal.dsl.SigningConfig) variant.variantConfiguration.signingConfig
+            SigningConfig signingConfig = variant.getVariantConfiguration().getSigningConfig();
             if (signingConfig == null) {
-                textOutput.withStyle(Identifier).text("Config: ")
-                textOutput.withStyle(Normal).text("none")
-                textOutput.println()
+                textOutput.withStyle(Identifier).text("Config: ");
+                textOutput.withStyle(Normal).text("none");
+                textOutput.println();
             } else {
-                SigningInfo signingInfo = getSigningInfo(signingConfig, cache)
+                SigningInfo signingInfo = getSigningInfo(signingConfig, cache);
 
 
-                textOutput.withStyle(Identifier).text("Config: ")
-                textOutput.withStyle(Description).text(signingConfig.name)
-                textOutput.println()
+                textOutput.withStyle(Identifier).text("Config: ");
+                textOutput.withStyle(Description).text(signingConfig.getName());
+                textOutput.println();
 
-                textOutput.withStyle(Identifier).text("Store: ")
-                textOutput.withStyle(Description).text(signingConfig.getStoreFile())
-                textOutput.println()
+                textOutput.withStyle(Identifier).text("Store: ");
+                textOutput.withStyle(Description).text(signingConfig.getStoreFile());
+                textOutput.println();
 
-                textOutput.withStyle(Identifier).text("Alias: ")
-                textOutput.withStyle(Description).text(signingConfig.getKeyAlias())
-                textOutput.println()
+                textOutput.withStyle(Identifier).text("Alias: ");
+                textOutput.withStyle(Description).text(signingConfig.getKeyAlias());
+                textOutput.println();
 
                 if (signingInfo.isValid()) {
                     if (signingInfo.error != null) {
-                        textOutput.withStyle(Identifier).text("Error: ")
-                        textOutput.withStyle(Failure).text(signingInfo.error)
-                        textOutput.println()
+                        textOutput.withStyle(Identifier).text("Error: ");
+                        textOutput.withStyle(Failure).text(signingInfo.error);
+                        textOutput.println();
                     } else {
-                        textOutput.withStyle(Identifier).text("MD5: ")
-                        textOutput.withStyle(Description).text(signingInfo.md5)
-                        textOutput.println()
+                        textOutput.withStyle(Identifier).text("MD5: ");
+                        textOutput.withStyle(Description).text(signingInfo.md5);
+                        textOutput.println();
 
-                        textOutput.withStyle(Identifier).text("SHA1: ")
-                        textOutput.withStyle(Description).text(signingInfo.sha1)
-                        textOutput.println()
+                        textOutput.withStyle(Identifier).text("SHA1: ");
+                        textOutput.withStyle(Description).text(signingInfo.sha1);
+                        textOutput.println();
 
-                        textOutput.withStyle(Identifier).text("Valid until: ")
-                        DateFormat df = DateFormat.getDateInstance(DateFormat.FULL)
-                        textOutput.withStyle(Description).text(df.format(signingInfo.notAfter))
-                        textOutput.println()
+                        textOutput.withStyle(Identifier).text("Valid until: ");
+                        DateFormat df = DateFormat.getDateInstance(DateFormat.FULL);
+                        textOutput.withStyle(Description).text(df.format(signingInfo.notAfter));
+                        textOutput.println();
                     }
                 }
             }
 
-            textOutput.withStyle(Normal).text("----------")
-            textOutput.println()
+            textOutput.withStyle(Normal).text("----------");
+            textOutput.println();
         }
     }
 
     /**
      * Sets the configurations to generate the report for.
-     *
-     * @param configurations The configuration. Must not be null.
      */
-    public void setVariants(Collection<? extends BaseVariantData> variants) {
+    public void setVariants(@NonNull Collection<? extends BaseVariantData> variants) {
         this.variants.addAll(variants);
     }
 
-    private static SigningInfo getSigningInfo(SigningConfig signingConfig,
-                                       Map<SigningConfig, SigningInfo> cache) {
-        SigningInfo signingInfo = cache.get(signingConfig)
+    private static SigningInfo getSigningInfo(
+            @NonNull SigningConfig signingConfig,
+            @NonNull Map<SigningConfig, SigningInfo> cache) {
+        SigningInfo signingInfo = cache.get(signingConfig);
 
         if (signingInfo == null) {
-            signingInfo = new SigningInfo()
+            signingInfo = new SigningInfo();
 
             if (signingConfig.isSigningReady()) {
                 try {
                     CertificateInfo certificateInfo = KeystoreHelper.getCertificateInfo(
                             signingConfig.getStoreType(), signingConfig.getStoreFile(),
                             signingConfig.getStorePassword(), signingConfig.getKeyPassword(),
-                            signingConfig.getKeyAlias())
+                            signingConfig.getKeyAlias());
                     if (certificateInfo != null) {
-                        signingInfo.md5 = getFingerprint(certificateInfo.certificate, "MD5")
-                        signingInfo.sha1 = getFingerprint(certificateInfo.certificate, "SHA1")
-                        signingInfo.notAfter = certificateInfo.certificate.notAfter
-                    } else {
-
+                        signingInfo.md5 = getFingerprint(certificateInfo.getCertificate(), "MD5");
+                        signingInfo.sha1 = getFingerprint(certificateInfo.getCertificate(), "SHA1");
+                        signingInfo.notAfter = certificateInfo.getCertificate().getNotAfter();
                     }
                 } catch (KeytoolException e) {
-                    signingInfo.error = e.getMessage()
+                    signingInfo.error = e.getMessage();
                 } catch (FileNotFoundException e) {
-                    signingInfo.error = "Missing keystore"
+                    signingInfo.error = "Missing keystore";
                 }
             }
 
-            cache.put(signingConfig, signingInfo)
+            cache.put(signingConfig, signingInfo);
         }
 
-        return signingInfo
+        return signingInfo;
     }
 
-    private final static class SigningInfo {
-        String md5
-        String sha1
-        Date notAfter
-        String error
+    private static final class SigningInfo {
+        String md5;
+        String sha1;
+        Date notAfter;
+        String error;
 
         boolean isValid() {
-            return md5 != null || error != null
+            return md5 != null || error != null;
         }
     }
 
     /**
      * Returns the {@link Certificate} fingerprint as returned by <code>keytool</code>.
-     *
-     * @param certificate
-     * @param hashAlgorithm
      */
     public static String getFingerprint(Certificate cert, String hashAlgorithm) {
         if (cert == null) {
