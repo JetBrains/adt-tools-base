tadfisher/platform_tools_base

Rewrite AAPT messages using new blame log.
Change-Id: Ie88bc1d7b74de79a863ee0d6ed789c7ba4665ee0

build-system/gradle-core/src/main/groovy/com/android/build/gradle/tasks/ProcessAndroidResources.java
@@ -31,6 +31,8 @@ import com.android.builder.core.AaptPackageProcessBuilder;
 import com.android.builder.core.AndroidBuilder;
 import com.android.builder.core.VariantType;
 import com.android.builder.dependency.LibraryDependency;
+import com.android.ide.common.blame.MergingLog;
+import com.android.ide.common.blame.MergingLogRewriter;
 import com.android.ide.common.blame.ParsingProcessOutputHandler;
 import com.android.ide.common.blame.parser.ToolOutputParser;
 import com.android.ide.common.blame.parser.aapt.AaptOutputParser;
@@ -53,7 +55,6 @@ import org.gradle.api.tasks.ParallelizableTask;
 import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -96,6 +97,7 @@ public class ProcessAndroidResources extends IncrementalTask {
 
     private AaptOptions aaptOptions;
 
+    private File mergeBlameLogFolder;
 
     @Override
     protected void doFullTaskAction() throws IOException {
@@ -138,9 +140,12 @@ public class ProcessAndroidResources extends IncrementalTask {
 
         @NonNull
         AndroidBuilder builder = getBuilder();
+
+        MergingLog mergingLog = new MergingLog(getMergeBlameLogFolder());
+
         ProcessOutputHandler processOutputHandler = new ParsingProcessOutputHandler(
                 new ToolOutputParser(new AaptOutputParser(), getILogger()),
-                builder.getErrorHandler());
+                new MergingLogRewriter(mergingLog, builder.getErrorHandler()));
         try {
             builder.processResources(
                     aaptPackageCommandBuilder,
@@ -316,6 +321,8 @@ public class ProcessAndroidResources extends IncrementalTask {
                     });
 
 
+            processResources.setMergeBlameLogFolder(
+                    scope.getVariantScope().getResourceBlameLogFolder());
         }
 
         @NonNull
@@ -499,5 +506,14 @@ public class ProcessAndroidResources extends IncrementalTask {
 
     public void setAaptOptions(AaptOptions aaptOptions) {
         this.aaptOptions = aaptOptions;
+    }
+
+    @Input
+    public File getMergeBlameLogFolder() {
+        return mergeBlameLogFolder;
+    }
+
+    public void setMergeBlameLogFolder(File mergeBlameLogFolder) {
+        this.mergeBlameLogFolder = mergeBlameLogFolder;
     }
 }

JetBrains/adt-tools-base

build-system/gradle-core/src/main/groovy/com/android/build/gradle/tasks/ProcessAndroidResources.java
@@ -31,6 +31,8 @@ import com.android.builder.core.AaptPackageProcessBuilder;
 import com.android.builder.core.AndroidBuilder;
 import com.android.builder.core.VariantType;
 import com.android.builder.dependency.LibraryDependency;
+import com.android.ide.common.blame.MergingLog;
+import com.android.ide.common.blame.MergingLogRewriter;
 import com.android.ide.common.blame.ParsingProcessOutputHandler;
 import com.android.ide.common.blame.parser.ToolOutputParser;
 import com.android.ide.common.blame.parser.aapt.AaptOutputParser;
@@ -95,6 +97,7 @@ public class ProcessAndroidResources extends IncrementalTask {
 
     private AaptOptions aaptOptions;
 
+    private File mergeBlameLogFolder;
 
     @Override
     protected void doFullTaskAction() throws IOException {
@@ -137,9 +140,12 @@ public class ProcessAndroidResources extends IncrementalTask {
 
         @NonNull
         AndroidBuilder builder = getBuilder();
+
+        MergingLog mergingLog = new MergingLog(getMergeBlameLogFolder());
+
         ProcessOutputHandler processOutputHandler = new ParsingProcessOutputHandler(
                 new ToolOutputParser(new AaptOutputParser(), getILogger()),
-                builder.getErrorReporter());
+                new MergingLogRewriter(mergingLog, builder.getErrorReporter()));
         try {
             builder.processResources(
                     aaptPackageCommandBuilder,
@@ -316,6 +322,8 @@ public class ProcessAndroidResources extends IncrementalTask {
                     });
 
 
+            processResources.setMergeBlameLogFolder(
+                    scope.getVariantScope().getResourceBlameLogDir());
         }
 
         @NonNull
@@ -499,5 +507,14 @@ public class ProcessAndroidResources extends IncrementalTask {
 
     public void setAaptOptions(AaptOptions aaptOptions) {
         this.aaptOptions = aaptOptions;
+    }
+
+    @Input
+    public File getMergeBlameLogFolder() {
+        return mergeBlameLogFolder;
+    }
+
+    public void setMergeBlameLogFolder(File mergeBlameLogFolder) {
+        this.mergeBlameLogFolder = mergeBlameLogFolder;
     }
 }