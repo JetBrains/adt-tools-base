6b34a27be5d437bc34604244a5b9bbe2f68340a4
Michal Bendowski <bendowski@google.com>
2015-10-05 15:12:11 +0100

    Use the same compile SDK in (most) integration tests
    There are two special cases that need an older SDK:
      - Local unit test support, which needs to run on the same bytecode
        version as our CI server.
      - RenderScript-related tests, that use some deprecated classes.
      - One test that depends on Android HTTP libs.
    
    Change-Id: If117d8ca172ca7a4bbd75694ef3f77a5309f4931

--- build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/application/GenFolderApi2Test.groovy
+++ build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/application/GenFolderApi2Test.groovy
@@ -91,6 +91,6 @@ class GenFolderApi2Test {
         // ATTENTION Author and Reviewers - please make sure required changes to the build file
         // are backwards compatible before updating this test.
         assertThat(FileUtils.sha1(project.file("build.gradle")))
-                .isEqualTo("93b7507ce31a087a4efa4cae66474ad320e25b6c")
+                .isEqualTo("8e3bc3c6c9074b49d27c32595e7c541dc2f18309")
     }
 }
