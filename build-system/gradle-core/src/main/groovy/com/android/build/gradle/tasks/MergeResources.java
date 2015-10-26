19bc99aea324bd5fa6b680260fe37beb3dcc6446
Chris Warrington <cmw@google.com>
2015-10-26 12:01:57 -0700

    Fix library res clashing output folders.
    Libraries use the MergeResources task twice, once for compilation (with
    dependencies), where blame is useful, and once for packaging (without
    dependencies) where it is not. In any case two tasks outputting to the
    same directory is bad, so not storing the blame for one fixes that.
    
    Change-Id: I969d9e0f8fd626880d02ce74bbf0df84375db977

--- build-system/gradle-core/src/main/groovy/com/android/build/gradle/tasks/MergeResources.java
+++ build-system/gradle-core/src/main/groovy/com/android/build/gradle/tasks/MergeResources.java
@@ -429,8 +429,11 @@ public class MergeResources extends IncrementalTask {
             mergeResourcesTask.setVariantName(scope.getVariantConfiguration().getFullName());
             mergeResourcesTask.setIncrementalFolder(scope.getIncrementalDir(getName()));
 
-            mergeResourcesTask.setBlameLogFolder(scope.getResourceBlameLogDir());
-
+            // Libraries use this task twice, once for compilation (with dependencies),
+            // where blame is useful, and once for packaging where it is not.
+            if (includeDependencies) {
+                mergeResourcesTask.setBlameLogFolder(scope.getResourceBlameLogDir());
+            }
             mergeResourcesTask.setProcess9Patch(process9Patch);
             mergeResourcesTask.setCrunchPng(extension.getAaptOptions().getCruncherEnabled());
 
