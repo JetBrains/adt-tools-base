150c7b37677873281517cd8458e3cee04b247ca4
Tor Norbye <tnorbye@google.com>
2015-12-19 08:53:37 -0800

    Update bundled and recommended Gradle version to 2.10
    Updated by running
     $ gradle wrapper --gradle-version 2.10
    in a Gradle project and then copying the downloaded artifacts.
    
    Change-Id: I9a7387b6efdd7c9cee350878a114788a9185b9a7

--- common/src/main/java/com/android/SdkConstants.java
+++ common/src/main/java/com/android/SdkConstants.java
@@ -1447,7 +1447,7 @@ public final class SdkConstants {
 
     public static final String GRADLE_PLUGIN_NAME = "com.android.tools.build:gradle:";
     public static final String GRADLE_MINIMUM_VERSION = "2.2.1";
-    public static final String GRADLE_LATEST_VERSION = "2.8";
+    public static final String GRADLE_LATEST_VERSION = "2.10";
     public static final String GRADLE_PLUGIN_MINIMUM_VERSION = "1.0.0";
     public static final String GRADLE_PLUGIN_RECOMMENDED_VERSION = "2.0.0-alpha3";
     public static final String GRADLE_PLUGIN_LATEST_VERSION = GRADLE_PLUGIN_RECOMMENDED_VERSION;
