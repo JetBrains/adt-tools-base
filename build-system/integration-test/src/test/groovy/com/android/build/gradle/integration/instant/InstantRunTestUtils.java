b455d22cd029eab36ffd7ee7e434e716c5a11006
Chris Warrington <cmw@google.com>
2016-01-07 14:07:15 -0800

    Instant Run: Connected smoke test.
    This added test method simulates a first build of the project, deploys
    and runs the app and pings the server.
    
    Change-Id: Ie25638f00f97b09174e494310b8a232f5e8f97c1

--- build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/instant/InstantRunTestUtils.java
+++ build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/instant/InstantRunTestUtils.java
@@ -16,16 +16,31 @@
 
 package com.android.build.gradle.integration.instant;
 
+import static com.android.build.gradle.integration.common.utils.DeviceHelper.DEFAULT_ADB_TIMEOUT_MSEC;
+import static com.google.common.truth.Truth.assertThat;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+
 import com.android.annotations.NonNull;
 import com.android.build.gradle.OptionalCompilationStep;
+import com.android.build.gradle.integration.common.utils.DeviceHelper;
 import com.android.build.gradle.internal.incremental.InstantRunBuildContext;
 import com.android.builder.model.AndroidProject;
 import com.android.builder.model.InstantRun;
 import com.android.builder.model.Variant;
+import com.android.builder.testing.api.DeviceException;
+import com.android.ddmlib.CollectingOutputReceiver;
+import com.android.ddmlib.IDevice;
+import com.android.ddmlib.IShellOutputReceiver;
+import com.android.ddmlib.InstallException;
+import com.android.sdklib.AndroidVersion;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 
+import java.io.File;
 import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 final class InstantRunTestUtils {
 
@@ -56,7 +71,19 @@ final class InstantRunTestUtils {
     @NonNull
     static List<String> getInstantRunArgs(int apiLevel,
             @NonNull OptionalCompilationStep... flags) {
-        String version = String.format("-Pandroid.injected.build.api=%d", apiLevel);
+        return getInstantRunArgs(new AndroidVersion(apiLevel, null), flags);
+    }
+
+    static List<String> getInstantRunArgs(@NonNull IDevice device,
+            @NonNull OptionalCompilationStep... flags) {
+        return getInstantRunArgs(device.getVersion(), flags);
+    }
+
+    @NonNull
+    static List<String> getInstantRunArgs(@NonNull AndroidVersion androidVersion,
+            @NonNull OptionalCompilationStep... flags) {
+        String version =
+                String.format("-Pandroid.injected.build.api=%s", androidVersion.getApiString());
         return ImmutableList.of(buildOptionalCompilationStepsProperty(flags), version);
     }
 
@@ -70,5 +97,50 @@ final class InstantRunTestUtils {
             builder.append(',').append(step);
         }
         return builder.toString();
+    }
+
+    static void doInstall(
+            @NonNull IDevice device,
+            @NonNull List<InstantRunBuildContext.Artifact> artifacts) throws DeviceException,
+            InstallException {
+        List<File> apkFiles = Lists.newArrayList();
+        for (InstantRunBuildContext.Artifact artifact : artifacts) {
+            if (artifact.getType() == InstantRunBuildContext.FileType.SPLIT) {
+                apkFiles.add(artifact.getLocation());
+            }
+            if (artifact.getType() == InstantRunBuildContext.FileType.MAIN) {
+                apkFiles.add(0, artifact.getLocation());
+            }
+        }
+
+        if (device.getVersion().isGreaterOrEqualThan(21)) {
+            device.installPackages(apkFiles, true /*reinstall*/, ImmutableList.<String>of(),
+                    DEFAULT_ADB_TIMEOUT_MSEC, MILLISECONDS);
+
+        } else {
+            assertThat(apkFiles).hasSize(1);
+            device.installPackage(
+                    Iterables.getOnlyElement(apkFiles).getAbsolutePath(), true /*reinstall*/);
+        }
+    }
+
+    static void runApp(IDevice device, String target) throws Exception {
+        IShellOutputReceiver receiver = new CollectingOutputReceiver();
+        String command = "am start" +
+                " -n " + target +
+                " -a android.intent.action.MAIN" +
+                " -c android.intent.category.LAUNCHER";
+        device.executeShellCommand(
+                command, receiver, DEFAULT_ADB_TIMEOUT_MSEC, MILLISECONDS);
+    }
+
+    static void unlockDevice(@NonNull IDevice device) throws Exception {
+        IShellOutputReceiver receiver = new CollectingOutputReceiver();
+        device.executeShellCommand(
+                "input keyevent KEYCODE_WAKEUP", receiver,
+                DEFAULT_ADB_TIMEOUT_MSEC, MILLISECONDS);
+        device.executeShellCommand(
+                "wm dismiss-keyguard", receiver,
+                DEFAULT_ADB_TIMEOUT_MSEC, MILLISECONDS);
     }
 }
