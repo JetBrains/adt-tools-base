f89c9bf8af8a5e3b33c116eb0b73c26ff2bf0449
David Herman <davidherman@google.com>
2016-01-26 10:51:19 -0800

    New profiler support jar
    Change-Id: I1839a6088d055e99f2cf0a468e5bfa24facbce3f

---
+++ profiler/integration-tests/ProfilerApp/app/src/main/java/com/android/profilerapp/MyApplication.java
@@ -0,0 +1,12 @@
+package com.android.profilerapp;
+
+import android.app.Application;
+import android.util.Log;
+
+// This class exists as a placeholder that will get instrumented by the Profiler Plugin
+public final class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
