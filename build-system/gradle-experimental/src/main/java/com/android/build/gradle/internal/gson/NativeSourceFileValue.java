24761ec1a56a73c59375bb27ad988e2492094acd
Jomo Fisher <jomof@google.com>
2016-03-02 18:17:37 +0000

    Revert "Add GNU Make parser support."
    This reverts commit 6ca726795c38f808a0e0b61371777971e1778a05.
    
    Change-Id: Ic83f24225b1f26ba818f26ea1fde9b12f89cd438

--- build-system/gradle-experimental/src/main/java/com/android/build/gradle/internal/gson/NativeSourceFileValue.java
+++ build-system/gradle-experimental/src/main/java/com/android/build/gradle/internal/gson/NativeSourceFileValue.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 The Android Open Source Project
+ * Copyright (C) 2015 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,21 +14,32 @@
  * limitations under the License.
  */
 
-package com.android.build.gradle.external.gson;
+package com.android.build.gradle.internal.gson;
 
+import com.android.annotations.NonNull;
 import com.android.annotations.Nullable;
+import com.android.build.gradle.managed.NativeSourceFile;
 
 import java.io.File;
 import java.util.List;
 
 /**
- * Value type to be used with Gson.
+ * Value type for {@link NativeSourceFile} to be used with Gson.
  */
 public class NativeSourceFileValue {
     @Nullable
-    public File src;
+    File src;
     @Nullable
-    public List<String> flags;
+    List<String> flags;
     @Nullable
-    public File workingDirectory;
+    File workingDirectory;
+
+    void copyTo(@NonNull NativeSourceFile file) {
+        file.setSrc(src);
+        if (flags != null) {
+            file.getFlags().clear();
+            file.getFlags().addAll(flags);
+        }
+        file.setWorkingDirectory(workingDirectory);
+    }
 }
