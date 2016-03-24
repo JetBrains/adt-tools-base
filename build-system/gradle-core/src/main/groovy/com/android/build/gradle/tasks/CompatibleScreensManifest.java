3d0a4bd4e3f4b96347d2d533007edd630bd001b9
Chris Warrington <cmw@google.com>
2016-03-24 12:51:06 -0700

    Convert CompatibleScreensManifest to java.
    Change-Id: Id49b9d80277fb1db5696a32d7f8b3fdbf1a47f21

--- build-system/gradle-core/src/main/groovy/com/android/build/gradle/tasks/CompatibleScreensManifest.java
+++ build-system/gradle-core/src/main/groovy/com/android/build/gradle/tasks/CompatibleScreensManifest.java
@@ -14,59 +14,92 @@
  * limitations under the License.
  */
 
-package com.android.build.gradle.tasks
+package com.android.build.gradle.tasks;
 
-import com.android.annotations.NonNull
-import com.android.build.gradle.internal.scope.ConventionMappingHelper
-import com.android.build.gradle.internal.scope.TaskConfigAction
-import com.android.build.gradle.internal.scope.VariantOutputScope
-import com.android.build.gradle.internal.tasks.DefaultAndroidTask
-import com.android.resources.Density
-import com.google.common.base.Charsets
-import com.google.common.io.Files
-import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.OutputFile
-import org.gradle.api.tasks.ParallelizableTask
-import org.gradle.api.tasks.TaskAction
+import com.android.annotations.NonNull;
+import com.android.build.gradle.internal.scope.ConventionMappingHelper;
+import com.android.build.gradle.internal.scope.TaskConfigAction;
+import com.android.build.gradle.internal.scope.VariantOutputScope;
+import com.android.build.gradle.internal.tasks.DefaultAndroidTask;
+import com.android.resources.Density;
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.OutputFile;
+import org.gradle.api.tasks.ParallelizableTask;
+import org.gradle.api.tasks.TaskAction;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Set;
+import java.util.concurrent.Callable;
+
+import groovy.lang.Closure;
 
 /**
  * Task to generate a manifest snippet that just contains a compatible-screens
  * node with the given density and the given list of screen sizes.
  */
 @ParallelizableTask
-class CompatibleScreensManifest extends DefaultAndroidTask {
+public class CompatibleScreensManifest extends DefaultAndroidTask {
+
+    private String screenDensity;
+
+    private Set<String> screenSizes;
+
+    private File manifestFile;
 
     @Input
-    String screenDensity
+    public String getScreenDensity() {
+        return screenDensity;
+    }
+
+    public void setScreenDensity(String screenDensity) {
+        this.screenDensity = screenDensity;
+    }
 
     @Input
-    Set<String> screenSizes
+    public Set<String> getScreenSizes() {
+        return screenSizes;
+    }
+
+    public void setScreenSizes(Set<String> screenSizes) {
+        this.screenSizes = screenSizes;
+    }
 
     @OutputFile
-    File manifestFile
+    public File getManifestFile() {
+        return manifestFile;
+    }
+
+    public void setManifestFile(File manifestFile) {
+        this.manifestFile = manifestFile;
+    }
 
     @TaskAction
-    void generate() {
+    public void generate() throws IOException {
         StringBuilder content = new StringBuilder(
                 "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
                 "<manifest xmlns:android=\"http://schemas.android.com/apk/res/android\"\n" +
                 "    package=\"\">\n" +
                 "\n" +
-                "    <compatible-screens>\n")
+                "    <compatible-screens>\n");
 
-        String density = getScreenDensity()
+        String density = getScreenDensity();
 
         // convert unsupported values to numbers.
         density = convert(density, Density.XXHIGH, Density.XXXHIGH);
 
         for (String size : getScreenSizes()) {
             content.append(
-                "        <screen android:screenSize=\"$size\" android:screenDensity=\"$density\" />\n")
+                    "        <screen android:screenSize=\"").append(size).append("\" "
+                    + "android:screenDensity=\"").append(density).append("\" />\n");
         }
 
         content.append(
                 "    </compatible-screens>\n" +
-                "</manifest>")
+                "</manifest>");
 
         Files.write(content.toString(), getManifestFile(), Charsets.UTF_8);
     }
@@ -74,7 +107,7 @@ class CompatibleScreensManifest extends DefaultAndroidTask {
     private static String convert(@NonNull String density, @NonNull Density... densitiesToConvert) {
         for (Density densityToConvert : densitiesToConvert) {
             if (densityToConvert.getResourceValue().equals(density)) {
-                return Integer.toString(densityToConvert.dpiValue);
+                return Integer.toString(densityToConvert.getDpiValue());
             }
         }
 
@@ -84,40 +117,38 @@ class CompatibleScreensManifest extends DefaultAndroidTask {
     public static class ConfigAction implements TaskConfigAction<CompatibleScreensManifest> {
 
         @NonNull
-        VariantOutputScope scope
+        private final VariantOutputScope scope;
         @NonNull
-        Set<String> screenSizes
+        private final Set<String> screenSizes;
 
-        ConfigAction(
+        public ConfigAction(
                 @NonNull VariantOutputScope scope,
                 @NonNull Set<String> screenSizes) {
-            this.scope = scope
-            this.screenSizes = screenSizes
+            this.scope = scope;
+            this.screenSizes = screenSizes;
         }
 
         @NonNull
         @Override
-        String getName() {
-            return scope.getTaskName("create", "CompatibleScreenManifest")
+        public String getName() {
+            return scope.getTaskName("create", "CompatibleScreenManifest");
         }
 
         @NonNull
         @Override
-        Class<CompatibleScreensManifest> getType() {
-            return CompatibleScreensManifest.class
+        public Class<CompatibleScreensManifest> getType() {
+            return CompatibleScreensManifest.class;
         }
 
         @Override
-        void execute(@NonNull CompatibleScreensManifest csmTask) {
-            csmTask.setVariantName(scope.getVariantScope().getVariantConfiguration().getFullName())
+        public void execute(@NonNull CompatibleScreensManifest csmTask) {
+            csmTask.setVariantName(scope.getVariantScope().getVariantConfiguration().getFullName());
 
-            csmTask.screenDensity = scope.variantOutputData.getMainOutputFile().getFilter(
-                    com.android.build.OutputFile.DENSITY)
-            csmTask.screenSizes = screenSizes
+            csmTask.setScreenDensity(scope.getVariantOutputData().getMainOutputFile()
+                    .getFilter(com.android.build.OutputFile.DENSITY));
+            csmTask.setScreenSizes(screenSizes);
 
-            ConventionMappingHelper.map(csmTask, "manifestFile") {
-                scope.getCompatibleScreensManifestFile()
-            }
+            csmTask.setManifestFile(scope.getCompatibleScreensManifestFile());
         }
     }
 }
