3991dee110895f2b60c651815acbed4ce5f63c73
Amaury Medeiros <amaurym@google.com>
2016-04-14 19:05:44 +0100

    Create test application for profilers
    Differently than VisualTests, which should contain tabs for different
    types of charts used by profilers, ProfilersMockPrototypes contains
    tabs correspondent to each one of the profilers (CPU, Memory, etc). The
    profilers UI should be implemented according to the mocks.
    
    UI should be implemented according to the mocks.
    
    https://folio.googleplex.com/android-studio-ux/11%20Android%20Monitor/v11
    
    Change-Id: Ieb6562ea1fc8b0be6c317ec2030a742fba3fb9aa

---
+++ chartlib/src/test/java/com/android/tools/chartlib/visual/CPUProfilerVisualTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tools.chartlib.visual;
+
+import com.android.tools.chartlib.Animatable;
+import com.android.tools.chartlib.AnimatedComponent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JPanel;
+
+public class CPUProfilerVisualTest extends VisualTest {
+
+    private static final String CPU_PROFILER_NAME = "CPU Profiler";
+
+    public CPUProfilerVisualTest() {
+        // TODO: implement constructor
+    }
+
+    @Override
+    protected void registerComponents(List<AnimatedComponent> components) {
+        // TODO: register components
+    }
+
+    @Override
+    public String getName() {
+        return CPU_PROFILER_NAME;
+    }
+
+    @Override
+    protected JPanel create() {
+        // TODO: implement CPU profiler panel
+        return new JPanel();
+    }
+
+    @Override
+    protected List<Animatable> createComponentsList() {
+        // TODO: create components list
+        return new ArrayList<>();
+    }
+}
