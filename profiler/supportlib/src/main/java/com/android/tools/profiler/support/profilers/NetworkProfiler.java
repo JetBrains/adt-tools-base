2f5a01d2e6bc685cc006432ca4fcac4633ff20e1
estebandlc <estebandlc@google.com>
2016-05-05 14:22:13 -0700

    Changes ProfilerService initialization
    This CL refactors the way the ProfilerService is initialized
    instead of assuming there is an Application class, this plugin
    looks for all known entry points (for now only Application and
    Activity) and initializes our Service there.
    
    This change also separates the transform specific behaviour
    and creates a class to allow simple class transformations.
    
    Network and Initialization are now separate MethodVisitors.
    
    Deleted ProfilerServer and moved the ProfilerComponent hooks
    to the Service.
    
    Change-Id: I15c14d03e89c840e88cc80435d8b394657a3034b

--- profiler/supportlib/src/main/java/com/android/tools/profiler/support/profilers/NetworkProfiler.java
+++ profiler/supportlib/src/main/java/com/android/tools/profiler/support/profilers/NetworkProfiler.java
@@ -13,73 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.android.tools.profiler.support.profilers;
 
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import com.android.tools.profiler.support.profilerserver.MessageHeader;
-
-import android.net.TrafficStats;
-import android.os.Process;
-import com.android.tools.profiler.support.profilerserver.ProfilerServer;
-
-import java.nio.ByteBuffer;
-
-public class NetworkProfiler extends AbstractProfilerComponent {
-
-    private final int myUid = Process.myUid();
-    private ConnectivityManager mConnectivityManager;
-
-    @Override
-    public byte getComponentId() {
-        return ProfilerRegistry.NETWORKING;
-    }
-
-    @Override
-    public void initialize() {
-        mConnectivityManager = (ConnectivityManager) ProfilerServer.getInstance().getContext()
-            .getSystemService(Context.CONNECTIVITY_SERVICE);
-    }
-
-    @Override
-    public void onClientConnection() {
-
-    }
-
-    @Override
-    public void onClientDisconnection() {
-
-    }
-
-    @Override
-    public String configure(byte flags) {
-        return null;
-    }
-
-    @Override
-    public int receiveMessage(long frameStartTime, MessageHeader header, ByteBuffer input,
-            ByteBuffer output) {
-        return RESPONSE_OK;
-    }
-
-    @Override
-    public int update(long frameStartTime, ByteBuffer output) {
-        // TODO: Change time to a customized world clock time when the world clock is ready.
-        long time = System.currentTimeMillis();
-        long txBytes = TrafficStats.getUidTxBytes(myUid);
-        long rxBytes = TrafficStats.getUidRxBytes(myUid);
-        MessageHeader.writeToBuffer(output, 39, (short) 0, (short) 0, (byte) 0x7f, ProfilerRegistry.NETWORKING, (short) 0);
-        output.putLong(time);
-        output.putLong(txBytes);
-        output.putLong(rxBytes);
-
-        NetworkInfo networkInfo = mConnectivityManager.getActiveNetworkInfo();
-        short networkType = (short) (networkInfo != null ? networkInfo.getType() : -1);
-        byte highPowerState = (byte) (mConnectivityManager.isDefaultNetworkActive() ? 0 : 1);
-        output.putShort(networkType);
-        output.put(highPowerState);
-
-        return UPDATE_DONE;
-    }
+public class NetworkProfiler implements ProfilerComponent {
+    //TODO: Bootstrap the connection tracker and interface with JNI
 }
