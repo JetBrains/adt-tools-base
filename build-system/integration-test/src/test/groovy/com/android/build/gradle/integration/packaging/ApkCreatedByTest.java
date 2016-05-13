0b20b98984fda0835ca2b214418a73b27a348592
Paulo Casanova <pasc@google.com>
2016-05-13 14:17:15 +0100

    Added test to ensure Created-By is in MANIFEST.MF.
    Statistics rely on Created By starting with Android Gradle.
    This test ensures we won't break this.
    
    Change-Id: I3b437d48ca87f3a5a3c2df634d9a0b6e9c0b210a

---
+++ build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/packaging/ApkCreatedByTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.build.gradle.integration.packaging;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import com.android.build.gradle.integration.common.fixture.GradleTestProject;
+import com.android.build.gradle.integration.common.fixture.app.HelloWorldApp;
+import com.android.builder.internal.packaging.zip.StoredEntry;
+import com.android.builder.internal.packaging.zip.ZFile;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ApkCreatedByTest {
+    @Rule
+    public GradleTestProject project = GradleTestProject.builder()
+            .fromTestApp(HelloWorldApp.forPlugin("com.android.application"))
+            .create();
+
+    @Test
+    public void checkCreatedByInApk() throws Exception {
+        project.execute("assembleDebug");
+
+        File apk = project.getApk("debug");
+        assertTrue(apk.isFile());
+
+        try (ZFile zf = new ZFile(apk)) {
+            StoredEntry manifestEntry = zf.get("META-INF/MANIFEST.MF");
+            assertNotNull(manifestEntry);
+
+            Properties props = new Properties();
+
+            try (InputStream is = manifestEntry.open()) {
+                props.load(is);
+            }
+
+            /*
+             * This is required to keep track of gradle builds.
+             */
+            assertTrue(props.getProperty("Created-By").startsWith("Android Gradle"));
+        }
+    }
+}
