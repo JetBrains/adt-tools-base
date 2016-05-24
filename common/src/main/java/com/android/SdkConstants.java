b56477b7a83c8ed47fb1615b9ce653d8f32d0bfb
Yigit Boyar <yboyar@google.com>
2016-05-24 09:24:02 -0700

    Add Data Binding's ViewStubProxy class
    Change-Id: I0227ac3c9fdbcc9d5539fb063c09e8cb8c177bdc

--- common/src/main/java/com/android/SdkConstants.java
+++ common/src/main/java/com/android/SdkConstants.java
@@ -1785,6 +1785,7 @@ public final class SdkConstants {
         TAG_IMPORT, TAG_LAYOUT, TAG_DATA};
     public static final String[] ATTRS_DATA_BINDING = new String[]{ATTR_NAME,
         ATTR_TYPE, ATTR_CLASS, ATTR_ALIAS};
+    public static final String DATA_BINDING_VIEW_STUB_PROXY = "android.databinding.ViewStubProxy";
 
     /** Name of keep attribute in XML */
     public static final String ATTR_KEEP = "keep";
