edf0f2991049b4a60410b437fa8f6484725d201f
Xavier Ducrohet <xav@google.com>
2016-05-24 10:08:23 -0700

    Add $SDK/extras/m2repository as a maven repo.
    This is where the recombined repo is located.
    
    Change-Id: Ic620a4ad6c8698397834f398976fccff54872800

--- build-system/builder/src/main/java/com/android/builder/sdk/DefaultSdkLoader.java
+++ build-system/builder/src/main/java/com/android/builder/sdk/DefaultSdkLoader.java
@@ -327,7 +327,7 @@ public class DefaultSdkLoader implements SdkLoader {
 
     @NonNull
     public ImmutableList<File> computeRepositories() {
-        List<File> repositories = Lists.newArrayListWithExpectedSize(2);
+        List<File> repositories = Lists.newArrayListWithExpectedSize(3);
 
         File androidRepo = new File(mSdkLocation, FD_EXTRAS + File.separator + "android"
                 + File.separator + FD_M2_REPOSITORY);
@@ -340,6 +340,12 @@ public class DefaultSdkLoader implements SdkLoader {
         if (googleRepo.isDirectory()) {
             repositories.add(googleRepo);
         }
+
+        File m2Repo = new File(mSdkLocation, FD_EXTRAS + File.separator + FD_M2_REPOSITORY);
+        if (m2Repo.isDirectory()) {
+            repositories.add(m2Repo);
+        }
+
         return ImmutableList.copyOf(repositories);
     }
 
