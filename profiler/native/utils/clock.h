f3e02b3104c00158cc03c1a48300e12e60b7bb8d
David Herman <davidherman@google.com>
2016-06-04 10:05:25 -0700

    Profiler time utility classes
    This CL adds two new classes - Clock, which will be our cross-
    platform utility class for getting the current time, and
    Stopwatch, which lets you measure the elapsed time between two
    moments.
    
    Change-Id: I43eb9bc3bd96c752bd2cf5ab48695be0ea0c128c

---
+++ profiler/native/utils/clock.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef CLOCK_H_
+#define CLOCK_H_
+
+#include <cstdint>
+
+namespace profiler {
+namespace utils {
+
+// A mockable clock class for getting the current epoch time, in nanoseconds.
+// Example:
+//  SteadyClock clock;
+//  Log(clock.GetCurrentTime())
+//
+// Note: If you are more interested in the amount of time an operation took,
+// rather than absolute time, use Stopwatch instead.
+class Clock {
+ public:
+  // Returns a monotonically increasing value. This value is meant for
+  // comparing two relative times, as the time represented by time=0 is not
+  // explicitly defined.
+  virtual uint64_t GetCurrentTime() = 0;
+  virtual ~Clock() = default;
+};
+
+// A Clock implementation based on clock_gettime(CLOCK_MONOTONIC).
+//
+// Note: we choose to rely on our own class instead of <chrono> because our most
+// important use-case is profiling on Android, and this approach lets us use an
+// API which:
+// - has satisfactory precision, granularity, and reliability
+// - is also accessible from Java via System.nanoTime.
+// - is used by the Linux kernel to timestamp events (like in perfs)
+class SteadyClock final : public Clock {
+ public:
+  virtual uint64_t GetCurrentTime() override;
+};
+
+}
+}
+
+#endif //CLOCK_H_
