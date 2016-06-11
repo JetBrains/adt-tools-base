38147f1564397c0042d2806638a688f8e97206f3
estebandlc <estebandlc@google.com>
2016-06-10 21:48:27 -0700

    Move missing flag to the right place.
    Change-Id: Ie30ca4c01b94b20f3e01ec36b3893c7a06547a94

--- profiler/native/cmake/linux-host.cmake
+++ profiler/native/cmake/linux-host.cmake
@@ -11,5 +11,5 @@ set(CMAKE_CXX_COMPILER "${PREBUILTS}/clang/linux-x86/host/3.6/bin/clang++" CACHE
 set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} ${TARGET}" CACHE STRING "ASM Flags")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${COMMON_FLAGS}" CACHE STRING "C flags")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${COMMON_FLAGS} -std=c++11 -stdlib=libstdc++ -I${STL}/x86_64-linux/include/c++/4.8/ -I${STL}/x86_64-linux/include/c++/4.8/x86_64-linux/" CACHE STRING "CXX flags")
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS}" CACHE STRING "Executable Library linker flags")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -L${STL}/x86_64-linux/lib64" CACHE STRING "Executable Library linker flags")
 set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -L${STL}/x86_64-linux/lib64" CACHE STRING "Shared Library linker flags")
