aaa6af88584cc8d94b6ed380b768116df79bbaeb
Jerome Dochez <jedo@google.com>
2016-06-16 16:44:42 -0700

    fixed separator issue on Windows
    Change-Id: I02544598597719df7bcb2f284e94f4e985b959e6
    (cherry picked from commit 4ca5a62157bd7f993358d072dfbba784b1f2243e)

--- build-system/gradle-core/src/test/java/com/android/build/gradle/internal/transforms/DexTransformTest.java
+++ build-system/gradle-core/src/test/java/com/android/build/gradle/internal/transforms/DexTransformTest.java
@@ -19,6 +19,7 @@ package com.android.build.gradle.internal.transforms;
 import static org.junit.Assert.assertEquals;
 
 import com.android.repository.Revision;
+import com.android.utils.FileUtils;
 
 import org.junit.Test;
 
@@ -44,7 +45,7 @@ public class DexTransformTest {
         buildToolsRevision = Revision.parseRevision("23.0");
         key = DexTransform.getKey(inputFile, buildToolsRevision, false, true, false);
 
-        assertEquals("com.android.support/design/23.3.0/jars/classes.jar"
+        assertEquals(FileUtils.join("com.android.support", "design", "23.3.0", "jars", "classes.jar")
                 + "_build=23.0_jumbo=false_multidex=true_optimize=false", key);
 
         inputFile = new File("/Users/foo/MyApplication/app/build/intermediates/exploded-aar/"
@@ -52,8 +53,9 @@ public class DexTransformTest {
         buildToolsRevision = Revision.parseRevision("23");
         key = DexTransform.getKey(inputFile, buildToolsRevision, true, true, true);
 
-        assertEquals("com.android.support/support-v4/23.3.0/jars/libs/internal_impl-23.3.0.jar"
-                + "_build=23_jumbo=true_multidex=true_optimize=true", key);
+        assertEquals(FileUtils.join("com.android.support", "support-v4", "23.3.0", "jars", "libs",
+                "internal_impl-23.3.0.jar") + "_build=23_jumbo=true_multidex=true_optimize=true",
+                key);
 
         inputFile = new File("/Users/foo/MyApplication/app/build/intermediates/pre-dexed/debug/"
                 + "debug_283caf89dd7987cc2f3b325eb70525d5b717a7b5.jar");
