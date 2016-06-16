4e8683d48427d0c2b22bb8fa5c10afb5eddd688e
Tor Norbye <tnorbye@google.com>
2016-06-16 08:45:58 -0700

    213263: Update ApiDetectorTest for final API 24
    Change-Id: I1ed2228925b22908148394bcd63d5c6b1c18864d

--- lint/libs/lint-tests/src/test/java/com/android/tools/lint/checks/ApiDetectorTest.java
+++ lint/libs/lint-tests/src/test/java/com/android/tools/lint/checks/ApiDetectorTest.java
@@ -1214,16 +1214,10 @@ public class ApiDetectorTest extends AbstractCheckTest {
         // Type annotations are not supported
         //noinspection ClassNameDiffersFromFileName
         assertEquals(""
-                + "src/test/pkg/MyAnnotation.java:9: Error: Type annotations are not supported in Android: TYPE_PARAMETER [NewApi]\n"
-                + "@Target({METHOD, PARAMETER, FIELD, LOCAL_VARIABLE, TYPE_PARAMETER, TYPE_USE})\n"
-                + "                                                   ~~~~~~~~~~~~~~\n"
-                + "src/test/pkg/MyAnnotation.java:9: Error: Type annotations are not supported in Android: TYPE_USE [NewApi]\n"
-                + "@Target({METHOD, PARAMETER, FIELD, LOCAL_VARIABLE, TYPE_PARAMETER, TYPE_USE})\n"
-                + "                                                                   ~~~~~~~~\n"
                 + "src/test/pkg/MyAnnotation2.java:9: Error: Type annotations are not supported in Android: TYPE_PARAMETER [NewApi]\n"
                 + "@Target(TYPE_PARAMETER)\n"
                 + "        ~~~~~~~~~~~~~~\n"
-                + "3 errors, 0 warnings\n",
+                + "1 errors, 0 warnings\n",
                 lintProject(
                         manifest().minSdk(15),
                         java("src/test/pkg/MyAnnotation.java", ""
