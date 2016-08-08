dc3c4f9ce31a9dd353ddcb3acfcae457bf945b56
Nicolas Roard <nicolasroard@google.com>
2016-08-08 15:59:20 -0700

    Update to ConstraintLayout alpha 6
    Change-Id: Ie48a9ea1705652174d3e75809bd8ece4e1f35125
    (cherry picked from commit a36b79d76d1d6668b81325ac5f7222bd739328b5)

--- common/src/main/java/com/android/SdkConstants.java
+++ common/src/main/java/com/android/SdkConstants.java
@@ -655,7 +655,7 @@ public final class SdkConstants {
     public static final String CONSTRAINT_LAYOUT_LIB_ARTIFACT_ID = "constraint-layout";
     public static final String CONSTRAINT_LAYOUT_LIB_ARTIFACT = CONSTRAINT_LAYOUT_LIB_GROUP_ID + ":" + CONSTRAINT_LAYOUT_LIB_ARTIFACT_ID;
     /** Latest known version of the ConstraintLayout library (as a string) */
-    public static final String LATEST_CONSTRAINT_LAYOUT_VERSION = "1.0.0-alpha5";
+    public static final String LATEST_CONSTRAINT_LAYOUT_VERSION = "1.0.0-alpha6";
 
     /** Returns the appropriate name for the 'android' command, which is 'android.exe' for
      * Windows and 'android' for all other platforms. */
