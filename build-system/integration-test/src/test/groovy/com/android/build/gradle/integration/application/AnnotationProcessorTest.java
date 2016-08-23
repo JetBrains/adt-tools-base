ba2797cc944ee88d9cbb14d0252f68a1d833d3a4
Jerome Dochez <jedo@google.com>
2016-08-23 12:58:37 -0700

    fix missing import.
    Change-Id: I572e9068d59f0464561ea39788732ab6fac493bd

--- build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/application/AnnotationProcessorTest.java
+++ build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/application/AnnotationProcessorTest.java
@@ -30,6 +30,7 @@ import com.android.build.gradle.integration.common.fixture.app.MultiModuleTestPr
 import com.android.build.gradle.integration.common.fixture.app.TestSourceFile;
 import com.android.build.gradle.integration.common.runner.FilterableParameterized;
 import com.android.build.gradle.integration.common.utils.ModelHelper;
+import com.android.build.gradle.integration.common.utils.TestFileUtils;
 import com.android.builder.model.AndroidProject;
 import com.google.common.base.Charsets;
 import com.google.common.collect.ImmutableMap;
