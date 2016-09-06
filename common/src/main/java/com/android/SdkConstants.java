95cc8cf9968a9ed2a18117d959c174098c92f1f8
Tor Norbye <tnorbye@google.com>
2016-09-06 12:19:18 -0700

    Update recommended Gradle plugin to 2.2 RC2
    Change-Id: If811aba6326df2fc20ca5a80dc74ccf977380b16

--- common/src/main/java/com/android/SdkConstants.java
+++ common/src/main/java/com/android/SdkConstants.java
@@ -1769,8 +1769,8 @@ public final class SdkConstants {
     public static final String GRADLE_LATEST_VERSION = GRADLE_MINIMUM_VERSION;
     public static final String GRADLE_PLUGIN_MINIMUM_VERSION = "1.0.0";
     // Always update GRADLE_PLUGIN_RECOMMENDED_VERSION and GRADLE_EXPERIMENTAL_PLUGIN_RECOMMENDED_VERSION together.
-    public static final String GRADLE_PLUGIN_RECOMMENDED_VERSION = "2.2.0-rc1";
-    public static final String GRADLE_EXPERIMENTAL_PLUGIN_RECOMMENDED_VERSION = "0.8.0-beta3";
+    public static final String GRADLE_PLUGIN_RECOMMENDED_VERSION = "2.2.0-rc2";
+    public static final String GRADLE_EXPERIMENTAL_PLUGIN_RECOMMENDED_VERSION = "0.8.0-rc2";
     public static final String GRADLE_PLUGIN_LATEST_VERSION = GRADLE_PLUGIN_RECOMMENDED_VERSION;
     public static final String GRADLE_EXPERIMENTAL_PLUGIN_LATEST_VERSION = GRADLE_EXPERIMENTAL_PLUGIN_RECOMMENDED_VERSION;
     public static final String GRADLE_COMPILE_CONFIGURATION = "compile";
